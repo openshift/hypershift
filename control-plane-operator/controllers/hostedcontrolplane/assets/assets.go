69ffe4fc76b367ceccfb3b3f6ed35f5c916d9b01
Dan Mace <ironcladlou@gmail.com>
2021-02-19 14:07:47 -0500

    control-plane-operator: replace bindata tool with embed package

---
+++ control-plane-operator/controllers/hostedcontrolplane/assets/assets.go
@@ -0,0 +1,45 @@
+package assets
+
+import "embed"
+
+//go:embed apiserver-haproxy/*
+//go:embed cluster-bootstrap/*
+//go:embed cluster-version-operator/*
+//go:embed common/*
+//go:embed etcd/*
+//go:embed hosted-cluster-config-operator/*
+//go:embed ignition-configs/*
+//go:embed kube-apiserver/*
+//go:embed kube-controller-manager/*
+//go:embed kube-scheduler/*
+//go:embed machine-config-server/*
+//go:embed oauth-apiserver/*
+//go:embed oauth-openshift/*
+//go:embed openshift-apiserver/*
+//go:embed openshift-controller-manager/*
+//go:embed openvpn/*
+//go:embed registry/*
+//go:embed roks-metrics/*
+//go:embed router-proxy/*
+//go:embed user-manifests-bootstrapper/*
+var content embed.FS
+
+func AssetDir(name string) ([]string, error) {
+	entries, err := content.ReadDir(name)
+	if err != nil {
+		panic(err)
+	}
+	var files []string
+	for _, entry := range entries {
+		files = append(files, entry.Name())
+	}
+	return files, nil
+}
+
+func MustAsset(name string) []byte {
+	b, err := content.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
