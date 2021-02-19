a4e105d9aa7805efaed575e329b6e5c6761853c3
Cesar Wong <cewong@redhat.com>
2021-02-19 16:52:50 -0500

    Infra CLI: support creating a worker instance profile

--- cmd/create/infra/infra.go
+++ cmd/create/infra/infra.go
@@ -10,6 +10,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go/service/iam/iamiface"
 	"github.com/spf13/cobra"
 )
 
@@ -98,46 +100,46 @@ func (o *CreateInfraOptions) CreateInfra() (*CreateInfraOutput, error) {
 	if err != nil {
 		return nil, err
 	}
-	result.Zone, err = o.firstZone(client)
+	result.Zone, err = o.firstZone(client.EC2)
 	if err != nil {
 		return nil, err
 	}
-	result.VPCID, err = o.createVPC(client)
+	result.VPCID, err = o.createVPC(client.EC2)
 	if err != nil {
 		return nil, err
 	}
-	if err = o.CreateDHCPOptions(client, result.VPCID); err != nil {
+	if err = o.CreateDHCPOptions(client.EC2, result.VPCID); err != nil {
 		return nil, err
 	}
-	result.PrivateSubnetID, err = o.CreatePrivateSubnet(client, result.VPCID, result.Zone)
+	result.PrivateSubnetID, err = o.CreatePrivateSubnet(client.EC2, result.VPCID, result.Zone)
 	if err != nil {
 		return nil, err
 	}
-	result.PublicSubnetID, err = o.CreatePublicSubnet(client, result.VPCID, result.Zone)
+	result.PublicSubnetID, err = o.CreatePublicSubnet(client.EC2, result.VPCID, result.Zone)
 	if err != nil {
 		return nil, err
 	}
-	igwID, err := o.CreateInternetGateway(client, result.VPCID)
+	igwID, err := o.CreateInternetGateway(client.EC2, result.VPCID)
 	if err != nil {
 		return nil, err
 	}
-	natGatewayID, err := o.CreateNATGateway(client, result.PublicSubnetID, result.Zone)
+	natGatewayID, err := o.CreateNATGateway(client.EC2, result.PublicSubnetID, result.Zone)
 	if err != nil {
 		return nil, err
 	}
-	result.SecurityGroupID, err = o.CreateWorkerSecurityGroup(client, result.VPCID)
+	result.SecurityGroupID, err = o.CreateWorkerSecurityGroup(client.EC2, result.VPCID)
 	if err != nil {
 		return nil, err
 	}
-	privateRouteTable, err := o.CreatePrivateRouteTable(client, result.VPCID, natGatewayID, result.PrivateSubnetID, result.Zone)
+	privateRouteTable, err := o.CreatePrivateRouteTable(client.EC2, result.VPCID, natGatewayID, result.PrivateSubnetID, result.Zone)
 	if err != nil {
 		return nil, err
 	}
-	publicRouteTable, err := o.CreatePublicRouteTable(client, result.VPCID, igwID, result.PublicSubnetID, result.Zone)
+	publicRouteTable, err := o.CreatePublicRouteTable(client.EC2, result.VPCID, igwID, result.PublicSubnetID, result.Zone)
 	if err != nil {
 		return nil, err
 	}
-	err = o.CreateVPCS3Endpoint(client, result.VPCID, privateRouteTable, publicRouteTable)
+	err = o.CreateVPCS3Endpoint(client.EC2, result.VPCID, privateRouteTable, publicRouteTable)
 	if err != nil {
 		return nil, err
 	}
@@ -633,7 +635,92 @@ func (o *CreateInfraOptions) CreatePublicRouteTable(client ec2iface.EC2API, vpcI
 	return tableID, nil
 }
 
-func (o *CreateInfraOptions) AWSClient() (ec2iface.EC2API, error) {
+func (o *CreateInfraOptions) CreateWorkerInstanceProfile(client iamiface.IAMAPI, profileName string) error {
+	const (
+		assumeRolePolicy = `{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}`
+		workerPolicy = `{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions"
+      ],
+      "Resource": "*"
+    }
+  ]
+}`
+	)
+	roleName := fmt.Sprintf("%s-worker-role", o.InfraID)
+	_, err := client.CreateRole(&iam.CreateRoleInput{
+		AssumeRolePolicyDocument: aws.String(assumeRolePolicy),
+		Path:                     aws.String("/"),
+		RoleName:                 aws.String(roleName),
+		Tags:                     iamTags(o.InfraID, roleName),
+	})
+	if err != nil {
+		return fmt.Errorf("cannot create worker role: %w", err)
+	}
+	_, err = client.CreateInstanceProfile(&iam.CreateInstanceProfileInput{
+		InstanceProfileName: aws.String(profileName),
+		Path:                aws.String("/"),
+	})
+	if err != nil {
+		return fmt.Errorf("cannot create worker instance profile: %w", err)
+	}
+	_, err = client.AddRoleToInstanceProfile(&iam.AddRoleToInstanceProfileInput{
+		InstanceProfileName: aws.String(profileName),
+		RoleName:            aws.String(roleName),
+	})
+	if err != nil {
+		return fmt.Errorf("cannot add role to worker instance profile: %w", err)
+	}
+	_, err = client.PutRolePolicy(&iam.PutRolePolicyInput{
+		PolicyName:     aws.String(fmt.Sprintf("%s-worker-policy", o.InfraID)),
+		PolicyDocument: aws.String(workerPolicy),
+		RoleName:       aws.String(roleName),
+	})
+	if err != nil {
+		return fmt.Errorf("cannot create worker policy: %w", err)
+	}
+	return nil
+}
+
+func iamTags(infraID, name string) []*iam.Tag {
+	tags := []*iam.Tag{
+		{
+			Key:   aws.String(fmt.Sprintf("kubernetes.io/cluster/%s", infraID)),
+			Value: aws.String("owned"),
+		},
+	}
+	if len(name) > 0 {
+		tags = append(tags, &iam.Tag{
+			Key:   aws.String("Name"),
+			Value: aws.String(name),
+		})
+	}
+	return tags
+}
+
+type AWSClient struct {
+	EC2 ec2iface.EC2API
+	IAM iamiface.IAMAPI
+}
+
+func (o *CreateInfraOptions) AWSClient() (*AWSClient, error) {
 	awsConfig := &aws.Config{
 		Region: aws.String(o.Region),
 	}
@@ -642,7 +729,10 @@ func (o *CreateInfraOptions) AWSClient() (ec2iface.EC2API, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to create client session: %w", err)
 	}
-	return ec2.New(s), nil
+	return &AWSClient{
+		EC2: ec2.New(s),
+		IAM: iam.New(s),
+	}, nil
 }
 
 func ec2Tags(infraID, name string) []*ec2.Tag {
