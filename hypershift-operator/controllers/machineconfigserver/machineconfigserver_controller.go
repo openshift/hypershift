a798e10c489b158ed45ef09139c41efc158bf2ae
Dan Mace <ironcladlou@gmail.com>
2021-03-23 10:18:02 -0400

    Refactor machine config server controller for consistency
    This is a refactor to make the controller more consistent with the
    hosted cluster controller. This commit doesn't change any behavior.

--- hypershift-operator/controllers/machineconfigserver/machineconfigserver_controller.go
+++ hypershift-operator/controllers/machineconfigserver/machineconfigserver_controller.go
@@ -2,16 +2,24 @@ package machineconfigserver
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"time"
 
 	"github.com/blang/semver"
 	"github.com/go-logr/logr"
+	routev1 "github.com/openshift/api/route/v1"
 	hyperv1 "github.com/openshift/hypershift/api/v1alpha1"
 	"github.com/openshift/hypershift/control-plane-operator/releaseinfo"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/client-go/util/workqueue"
+	k8sutilspointer "k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -75,37 +83,17 @@ func (r *MachineConfigServerReconciler) Reconcile(ctx context.Context, req ctrl.
 	r.Log = r.Log.WithValues("releaseImage", mcs.Spec.ReleaseImage, "version", releaseImage.Version())
 
 	// Generate mcs manifests for the given release
-	mcsServiceAccount := ServiceAccount{
-		Name:      mcs.GetName(),
-		Namespace: mcs.GetNamespace(),
-	}.Build()
+	mcsServiceAccount := MachineConfigServerServiceAccount(mcs.Namespace, mcs.Name)
 
-	mcsRoleBinding := RoleBinding{
-		Name:           mcs.GetName(),
-		ServiceAccount: mcsServiceAccount,
-	}.Build()
+	mcsRoleBinding := MachineConfigServerRoleBinding(mcs.Namespace, mcs.Name)
 
-	mcsService := Service{
-		Name:      mcs.GetName(),
-		Namespace: mcs.GetNamespace(),
-	}.Build()
+	mcsService := MachineConfigServerService(mcs.Namespace, mcs.Name)
 
-	mcsDeployment := Deployment{
-		Name:           mcs.GetName(),
-		Namespace:      mcs.GetNamespace(),
-		ServiceAccount: mcsServiceAccount,
-		Images:         releaseImage.ComponentImages(),
-	}.Build()
+	mcsDeployment := MachineConfigServerDeployment(mcs.Namespace, mcs.Name)
 
-	ignitionRoute := IgnitionRoute{
-		Name:      mcs.GetName(),
-		Namespace: mcs.GetNamespace(),
-	}.Build()
+	ignitionRoute := MachineConfigServerIgnitionRoute(mcs.Namespace, mcs.Name)
 
-	userDataSecret := Userdata{
-		Name:      mcs.GetName(),
-		Namespace: mcs.GetNamespace(),
-	}.Build()
+	userDataSecret := MachineConfigServerUserDataSecret(mcs.Namespace, mcs.Name)
 
 	// Return early if deleted
 	if !mcs.DeletionTimestamp.IsZero() {
@@ -155,32 +143,64 @@ func (r *MachineConfigServerReconciler) Reconcile(ctx context.Context, req ctrl.
 		return ctrl.Result{}, err
 	}
 
-	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, mcsRoleBinding, NoopReconcile)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, mcsDeployment, func() error {
-		// Regenerate deployment for latest release
-		mcsDeployment = Deployment{
-			Name:           mcs.GetName(),
-			Namespace:      mcs.GetNamespace(),
-			ServiceAccount: mcsServiceAccount,
-			Images:         releaseImage.ComponentImages(),
-		}.Build()
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, mcsRoleBinding, func() error {
+		mcsRoleBinding.RoleRef = rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "edit",
+		}
+		mcsRoleBinding.Subjects = []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      mcsServiceAccount.Name,
+				Namespace: mcsServiceAccount.Namespace,
+			},
+		}
 		return nil
 	})
 	if err != nil {
 		return ctrl.Result{}, err
 	}
 
-	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, mcsService, NoopReconcile)
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, mcsDeployment, func() error {
+		return reconcileMCSDeployment(mcsDeployment, mcs, mcsServiceAccount, releaseImage.ComponentImages())
+	})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, mcsService, func() error {
+		mcsService.Spec = corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       80,
+					TargetPort: intstr.FromInt(8080),
+					NodePort:   0,
+				},
+			},
+			Selector: map[string]string{
+				"app": fmt.Sprintf("machine-config-server-%s", mcs.Name),
+			},
+			Type: corev1.ServiceTypeClusterIP,
+		}
+		return nil
+	})
 	if err != nil {
 		return ctrl.Result{}, err
 	}
 
 	r.Log.Info("Creating ignition provider route")
-	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, ignitionRoute, NoopReconcile)
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, ignitionRoute, func() error {
+		ignitionRoute.Spec = routev1.RouteSpec{
+			To: routev1.RouteTargetReference{
+				Kind: "Service",
+				Name: fmt.Sprintf("machine-config-server-%s", mcs.Name),
+			},
+		}
+		return nil
+	})
 	if err != nil {
 		return ctrl.Result{}, err
 	}
@@ -197,14 +217,32 @@ func (r *MachineConfigServerReconciler) Reconcile(ctx context.Context, req ctrl.
 	if err != nil {
 		return ctrl.Result{}, nil
 	}
-	userDataSecret = Userdata{
-		Name:                 mcs.GetName(),
-		Namespace:            mcs.GetNamespace(),
-		IgnitionProviderAddr: ignitionRoute.Spec.Host,
-		Version:              semversion,
-	}.Build()
-	if err := r.Create(ctx, userDataSecret); err != nil && !apierrors.IsAlreadyExists(err) {
-		return ctrl.Result{}, fmt.Errorf("failed to generate user data secret: %w", err)
+	userDataSecret = MachineConfigServerUserDataSecret(mcs.Namespace, mcs.Name)
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, userDataSecret, func() error {
+		// For now, only create and never update this secret
+		if !userDataSecret.CreationTimestamp.IsZero() {
+			return nil
+		}
+		disableTemplatingValue := []byte(base64.StdEncoding.EncodeToString([]byte("true")))
+		var userDataValue []byte
+
+		// Clear any version modifiers for this comparison
+		semversion.Pre = nil
+		semversion.Build = nil
+		if semversion.GTE(semver.MustParse("4.6.0")) {
+			userDataValue = []byte(fmt.Sprintf(`{"ignition":{"config":{"merge":[{"source":"http://%s/config/master","verification":{}}]},"security":{},"timeouts":{},"version":"3.1.0"},"networkd":{},"passwd":{},"storage":{},"systemd":{}}`, ignitionRoute.Spec.Host))
+		} else {
+			userDataValue = []byte(fmt.Sprintf(`{"ignition":{"config":{"append":[{"source":"http://%s/config/master","verification":{}}]},"security":{},"timeouts":{},"version":"2.2.0"},"networkd":{},"passwd":{},"storage":{},"systemd":{}}`, ignitionRoute.Spec.Host))
+		}
+
+		userDataSecret.Data = map[string][]byte{
+			"disableTemplating": disableTemplatingValue,
+			"value":             userDataValue,
+		}
+		return nil
+	})
+	if err != nil {
+		return ctrl.Result{}, err
 	}
 
 	mcs.Status.Version = releaseImage.Version()
@@ -214,4 +252,244 @@ func (r *MachineConfigServerReconciler) Reconcile(ctx context.Context, req ctrl.
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func reconcileMCSDeployment(deployment *appsv1.Deployment, mcs *hyperv1.MachineConfigServer, sa *corev1.ServiceAccount, images map[string]string) error {
+	bootstrapArgs := fmt.Sprintf(`
+mkdir -p /mcc-manifests/bootstrap/manifests
+mkdir -p /mcc-manifests/manifests
+exec machine-config-operator bootstrap \
+--root-ca=/assets/manifests/root-ca.crt \
+--kube-ca=/assets/manifests/combined-ca.crt \
+--machine-config-operator-image=%s \
+--machine-config-oscontent-image=%s \
+--infra-image=%s \
+--keepalived-image=%s \
+--coredns-image=%s \
+--mdns-publisher-image=%s \
+--haproxy-image=%s \
+--baremetal-runtimecfg-image=%s \
+--infra-config-file=/assets/manifests/cluster-infrastructure-02-config.yaml \
+--network-config-file=/assets/manifests/cluster-network-02-config.yaml \
+--proxy-config-file=/assets/manifests/cluster-proxy-01-config.yaml \
+--config-file=/assets/manifests/install-config.yaml \
+--dns-config-file=/assets/manifests/cluster-dns-02-config.yaml \
+--dest-dir=/mcc-manifests \
+--pull-secret=/assets/manifests/pull-secret.yaml
+
+# Use our own version of configpools that swap master and workers
+mv /mcc-manifests/bootstrap/manifests /mcc-manifests/bootstrap/manifests.tmp
+mkdir /mcc-manifests/bootstrap/manifests
+cp /mcc-manifests/bootstrap/manifests.tmp/* /mcc-manifests/bootstrap/manifests/
+cp /assets/manifests/*.machineconfigpool.yaml /mcc-manifests/bootstrap/manifests/`,
+		images["machine-config-operator"],
+		images["machine-os-content"],
+		images["pod"],
+		images["keepalived-ipfailover"],
+		images["coredns"],
+		images["mdns-publisher"],
+		images["haproxy-router"],
+		images["baremetal-runtimecfg"],
+	)
+
+	customMachineConfigArg := `
+cat <<"EOF" > "./copy-ignition-config.sh"
+#!/bin/bash
+name="${1}"
+oc get cm ${name} -n "${NAMESPACE}" -o jsonpath='{ .data.data }' > "/mcc-manifests/bootstrap/manifests/${name/#ignition-config-//}.yaml"
+EOF
+chmod +x ./copy-ignition-config.sh
+oc get cm -l ignition-config="true" -n "${NAMESPACE}" --no-headers | awk '{ print $1 }' | xargs -n1 ./copy-ignition-config.sh`
+
+	deployment.Spec = appsv1.DeploymentSpec{
+		Replicas: k8sutilspointer.Int32Ptr(1),
+		Selector: &metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"app": fmt.Sprintf("machine-config-server-%s", mcs.Name),
+			},
+		},
+		Template: corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{
+					"app": fmt.Sprintf("machine-config-server-%s", mcs.Name),
+				},
+			},
+			Spec: corev1.PodSpec{
+				ServiceAccountName:            sa.Name,
+				TerminationGracePeriodSeconds: k8sutilspointer.Int64Ptr(10),
+				Tolerations: []corev1.Toleration{
+					{
+						Key:      "multi-az-worker",
+						Operator: "Equal",
+						Value:    "true",
+						Effect:   "NoSchedule",
+					},
+				},
+				InitContainers: []corev1.Container{
+					{
+						Image: images["machine-config-operator"],
+						Name:  "machine-config-operator-bootstrap",
+						Command: []string{
+							"/bin/bash",
+						},
+						Args: []string{
+							"-c",
+							bootstrapArgs,
+						},
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      "mcc-manifests",
+								MountPath: "/mcc-manifests",
+							},
+							{
+								Name:      "config",
+								MountPath: "/assets/manifests",
+							},
+						},
+					},
+					{
+						Image:           images["cli"],
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						Name:            "inject-custom-machine-configs",
+						Env: []corev1.EnvVar{
+							{
+								Name: "NAMESPACE",
+								ValueFrom: &corev1.EnvVarSource{
+									FieldRef: &corev1.ObjectFieldSelector{
+										FieldPath: "metadata.namespace",
+									},
+								},
+							},
+						},
+						WorkingDir: "/tmp",
+						Command: []string{
+							"/usr/bin/bash",
+						},
+						Args: []string{
+							"-c",
+							customMachineConfigArg,
+						},
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      "mcc-manifests",
+								MountPath: "/mcc-manifests",
+							},
+						},
+					},
+					{
+						Image:           images["machine-config-operator"],
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						Name:            "machine-config-controller-bootstrap",
+						Command: []string{
+							"/usr/bin/machine-config-controller",
+						},
+						Args: []string{
+							"bootstrap",
+							"--manifest-dir=/mcc-manifests/bootstrap/manifests",
+							"--pull-secret=/mcc-manifests/bootstrap/manifests/machineconfigcontroller-pull-secret",
+							"--dest-dir=/mcs-manifests",
+						},
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      "mcc-manifests",
+								MountPath: "/mcc-manifests",
+							},
+							{
+								Name:      "mcs-manifests",
+								MountPath: "/mcs-manifests",
+							},
+						},
+					},
+				},
+				Containers: []corev1.Container{
+					{
+						Image:           images["machine-config-operator"],
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						Name:            "machine-config-server",
+						Command: []string{
+							"/usr/bin/machine-config-server",
+						},
+						Args: []string{
+							"bootstrap",
+							"--bootstrap-kubeconfig=/etc/openshift/kubeconfig",
+							"--secure-port=8443",
+							"--insecure-port=8080",
+						},
+						Ports: []corev1.ContainerPort{
+							{
+								Name:          "http",
+								ContainerPort: 8080,
+								Protocol:      corev1.ProtocolTCP,
+							},
+							{
+								Name:          "https",
+								ContainerPort: 8443,
+								Protocol:      corev1.ProtocolTCP,
+							},
+						},
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      "kubeconfig",
+								ReadOnly:  true,
+								MountPath: "/etc/openshift",
+							},
+							{
+								Name:      "mcs-manifests",
+								MountPath: "/etc/mcs/bootstrap",
+							},
+							{
+								Name:      "mcc-manifests",
+								MountPath: "/etc/mcc/bootstrap",
+							},
+							{
+								Name:      "mcs-tls",
+								MountPath: "/etc/ssl/mcs",
+							},
+						},
+					},
+				},
+				Volumes: []corev1.Volume{
+					{
+						Name: "kubeconfig",
+						VolumeSource: corev1.VolumeSource{
+							Secret: &corev1.SecretVolumeSource{
+								SecretName: "machine-config-server-kubeconfig",
+							},
+						},
+					},
+					{
+						Name: "mcs-tls",
+						VolumeSource: corev1.VolumeSource{
+							Secret: &corev1.SecretVolumeSource{
+								SecretName: "machine-config-server",
+							},
+						},
+					},
+					{
+						Name: "mcs-manifests",
+						VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{},
+						},
+					},
+					{
+						Name: "mcc-manifests",
+						VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{},
+						},
+					},
+					{
+						Name: "config",
+						VolumeSource: corev1.VolumeSource{
+							ConfigMap: &corev1.ConfigMapVolumeSource{
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: "machine-config-server",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return nil
 }
--- hypershift-operator/controllers/machineconfigserver/manifests.go
+++ hypershift-operator/controllers/machineconfigserver/manifests.go
@@ -1,405 +1,65 @@
 package machineconfigserver
 
 import (
-	"encoding/base64"
 	"fmt"
 
-	"github.com/blang/semver"
 	routev1 "github.com/openshift/api/route/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
-	k8sutilspointer "k8s.io/utils/pointer"
 )
 
-type Deployment struct {
-	Name           string
-	Namespace      string
-	ServiceAccount *corev1.ServiceAccount
-	Images         map[string]string
-}
-
-func (d Deployment) mcoBootstrapArg() string {
-	return fmt.Sprintf(`
-mkdir -p /mcc-manifests/bootstrap/manifests
-mkdir -p /mcc-manifests/manifests
-exec machine-config-operator bootstrap \
---root-ca=/assets/manifests/root-ca.crt \
---kube-ca=/assets/manifests/combined-ca.crt \
---machine-config-operator-image=%s \
---machine-config-oscontent-image=%s \
---infra-image=%s \
---keepalived-image=%s \
---coredns-image=%s \
---mdns-publisher-image=%s \
---haproxy-image=%s \
---baremetal-runtimecfg-image=%s \
---infra-config-file=/assets/manifests/cluster-infrastructure-02-config.yaml \
---network-config-file=/assets/manifests/cluster-network-02-config.yaml \
---proxy-config-file=/assets/manifests/cluster-proxy-01-config.yaml \
---config-file=/assets/manifests/install-config.yaml \
---dns-config-file=/assets/manifests/cluster-dns-02-config.yaml \
---dest-dir=/mcc-manifests \
---pull-secret=/assets/manifests/pull-secret.yaml
-
-# Use our own version of configpools that swap master and workers
-mv /mcc-manifests/bootstrap/manifests /mcc-manifests/bootstrap/manifests.tmp
-mkdir /mcc-manifests/bootstrap/manifests
-cp /mcc-manifests/bootstrap/manifests.tmp/* /mcc-manifests/bootstrap/manifests/
-cp /assets/manifests/*.machineconfigpool.yaml /mcc-manifests/bootstrap/manifests/`,
-		d.Images["machine-config-operator"],
-		d.Images["machine-os-content"],
-		d.Images["pod"],
-		d.Images["keepalived-ipfailover"],
-		d.Images["coredns"],
-		d.Images["mdns-publisher"],
-		d.Images["haproxy-router"],
-		d.Images["baremetal-runtimecfg"],
-	)
-}
-
-var customMachineConfigArg = `
-cat <<"EOF" > "./copy-ignition-config.sh"
-#!/bin/bash
-name="${1}"
-oc get cm ${name} -n "${NAMESPACE}" -o jsonpath='{ .data.data }' > "/mcc-manifests/bootstrap/manifests/${name/#ignition-config-//}.yaml"
-EOF
-chmod +x ./copy-ignition-config.sh
-oc get cm -l ignition-config="true" -n "${NAMESPACE}" --no-headers | awk '{ print $1 }' | xargs -n1 ./copy-ignition-config.sh`
-
-func (o Deployment) Build() *appsv1.Deployment {
+func MachineConfigServerDeployment(machineConfigServerNamespace, machineConfigServerName string) *appsv1.Deployment {
 	return &appsv1.Deployment{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Deployment",
-			APIVersion: appsv1.SchemeGroupVersion.String(),
-		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("machine-config-server-%s", o.Name),
-			Namespace: o.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: k8sutilspointer.Int32Ptr(1),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": fmt.Sprintf("machine-config-server-%s", o.Name),
-				},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": fmt.Sprintf("machine-config-server-%s", o.Name),
-					},
-				},
-				Spec: corev1.PodSpec{
-					ServiceAccountName:            o.ServiceAccount.Name,
-					TerminationGracePeriodSeconds: k8sutilspointer.Int64Ptr(10),
-					Tolerations: []corev1.Toleration{
-						{
-							Key:      "multi-az-worker",
-							Operator: "Equal",
-							Value:    "true",
-							Effect:   "NoSchedule",
-						},
-					},
-					InitContainers: []corev1.Container{
-						{
-							Image: o.Images["machine-config-operator"],
-							Name:  "machine-config-operator-bootstrap",
-							Command: []string{
-								"/bin/bash",
-							},
-							Args: []string{
-								"-c",
-								o.mcoBootstrapArg(),
-							},
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "mcc-manifests",
-									MountPath: "/mcc-manifests",
-								},
-								{
-									Name:      "config",
-									MountPath: "/assets/manifests",
-								},
-							},
-						},
-						{
-							Image:           o.Images["cli"],
-							ImagePullPolicy: corev1.PullIfNotPresent,
-							Name:            "inject-custom-machine-configs",
-							Env: []corev1.EnvVar{
-								{
-									Name: "NAMESPACE",
-									ValueFrom: &corev1.EnvVarSource{
-										FieldRef: &corev1.ObjectFieldSelector{
-											FieldPath: "metadata.namespace",
-										},
-									},
-								},
-							},
-							WorkingDir: "/tmp",
-							Command: []string{
-								"/usr/bin/bash",
-							},
-							Args: []string{
-								"-c",
-								customMachineConfigArg,
-							},
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "mcc-manifests",
-									MountPath: "/mcc-manifests",
-								},
-							},
-						},
-						{
-							Image:           o.Images["machine-config-operator"],
-							ImagePullPolicy: corev1.PullIfNotPresent,
-							Name:            "machine-config-controller-bootstrap",
-							Command: []string{
-								"/usr/bin/machine-config-controller",
-							},
-							Args: []string{
-								"bootstrap",
-								"--manifest-dir=/mcc-manifests/bootstrap/manifests",
-								"--pull-secret=/mcc-manifests/bootstrap/manifests/machineconfigcontroller-pull-secret",
-								"--dest-dir=/mcs-manifests",
-							},
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "mcc-manifests",
-									MountPath: "/mcc-manifests",
-								},
-								{
-									Name:      "mcs-manifests",
-									MountPath: "/mcs-manifests",
-								},
-							},
-						},
-					},
-					Containers: []corev1.Container{
-						{
-							Image:           o.Images["machine-config-operator"],
-							ImagePullPolicy: corev1.PullIfNotPresent,
-							Name:            "machine-config-server",
-							Command: []string{
-								"/usr/bin/machine-config-server",
-							},
-							Args: []string{
-								"bootstrap",
-								"--bootstrap-kubeconfig=/etc/openshift/kubeconfig",
-								"--secure-port=8443",
-								"--insecure-port=8080",
-							},
-							Ports: []corev1.ContainerPort{
-								{
-									Name:          "http",
-									ContainerPort: 8080,
-									Protocol:      corev1.ProtocolTCP,
-								},
-								{
-									Name:          "https",
-									ContainerPort: 8443,
-									Protocol:      corev1.ProtocolTCP,
-								},
-							},
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "kubeconfig",
-									ReadOnly:  true,
-									MountPath: "/etc/openshift",
-								},
-								{
-									Name:      "mcs-manifests",
-									MountPath: "/etc/mcs/bootstrap",
-								},
-								{
-									Name:      "mcc-manifests",
-									MountPath: "/etc/mcc/bootstrap",
-								},
-								{
-									Name:      "mcs-tls",
-									MountPath: "/etc/ssl/mcs",
-								},
-							},
-						},
-					},
-					Volumes: []corev1.Volume{
-						{
-							Name: "kubeconfig",
-							VolumeSource: corev1.VolumeSource{
-								Secret: &corev1.SecretVolumeSource{
-									SecretName: "machine-config-server-kubeconfig",
-								},
-							},
-						},
-						{
-							Name: "mcs-tls",
-							VolumeSource: corev1.VolumeSource{
-								Secret: &corev1.SecretVolumeSource{
-									SecretName: "machine-config-server",
-								},
-							},
-						},
-						{
-							Name: "mcs-manifests",
-							VolumeSource: corev1.VolumeSource{
-								EmptyDir: &corev1.EmptyDirVolumeSource{},
-							},
-						},
-						{
-							Name: "mcc-manifests",
-							VolumeSource: corev1.VolumeSource{
-								EmptyDir: &corev1.EmptyDirVolumeSource{},
-							},
-						},
-						{
-							Name: "config",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: "machine-config-server",
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			Namespace: machineConfigServerNamespace,
+			Name:      fmt.Sprintf("machine-config-server-%s", machineConfigServerName),
 		},
 	}
 }
 
-type ServiceAccount struct {
-	Name      string
-	Namespace string
-}
-
-func (o ServiceAccount) Build() *corev1.ServiceAccount {
+func MachineConfigServerServiceAccount(machineConfigServerNamespace, machineConfigServerName string) *corev1.ServiceAccount {
 	return &corev1.ServiceAccount{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "ServiceAccount",
-			APIVersion: corev1.SchemeGroupVersion.String(),
-		},
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace: o.Namespace,
-			Name:      fmt.Sprintf("machine-config-server-%s", o.Name),
+			Namespace: machineConfigServerNamespace,
+			Name:      fmt.Sprintf("machine-config-server-%s", machineConfigServerName),
 		},
 	}
 }
 
-type RoleBinding struct {
-	Name           string
-	ServiceAccount *corev1.ServiceAccount
-}
-
-func (o RoleBinding) Build() *rbacv1.RoleBinding {
+func MachineConfigServerRoleBinding(machineConfigServerNamespace, machineConfigServerName string) *rbacv1.RoleBinding {
 	return &rbacv1.RoleBinding{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "RoleBinding",
-			APIVersion: rbacv1.SchemeGroupVersion.String(),
-		},
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace: o.ServiceAccount.Namespace,
-			Name:      fmt.Sprintf("machine-config-server-%s", o.Name),
-		},
-		RoleRef: rbacv1.RoleRef{
-			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "ClusterRole",
-			Name:     "edit",
-		},
-		Subjects: []rbacv1.Subject{
-			{
-				Kind:      "ServiceAccount",
-				Name:      o.ServiceAccount.Name,
-				Namespace: o.ServiceAccount.Namespace,
-			},
+			Namespace: machineConfigServerNamespace,
+			Name:      fmt.Sprintf("machine-config-server-%s", machineConfigServerName),
 		},
 	}
 }
 
-type Service struct {
-	Name      string
-	Namespace string
-}
-
-func (o Service) Build() *corev1.Service {
+func MachineConfigServerService(machineConfigServerNamespace, machineConfigServerName string) *corev1.Service {
 	return &corev1.Service{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Service",
-			APIVersion: corev1.SchemeGroupVersion.String(),
-		},
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace: o.Namespace,
-			Name:      fmt.Sprintf("machine-config-server-%s", o.Name),
-		},
-		Spec: corev1.ServiceSpec{
-			Ports: []corev1.ServicePort{
-				{
-					Name:       "http",
-					Protocol:   corev1.ProtocolTCP,
-					Port:       80,
-					TargetPort: intstr.FromInt(8080),
-					NodePort:   0,
-				},
-			},
-			Selector: map[string]string{
-				"app": fmt.Sprintf("machine-config-server-%s", o.Name),
-			},
-			Type: corev1.ServiceTypeClusterIP,
+			Namespace: machineConfigServerNamespace,
+			Name:      fmt.Sprintf("machine-config-server-%s", machineConfigServerName),
 		},
 	}
 }
 
-type IgnitionRoute struct {
-	Name      string
-	Namespace string
-}
-
-func (o IgnitionRoute) Build() *routev1.Route {
+func MachineConfigServerIgnitionRoute(machineConfigServerNamespace, machineConfigServerName string) *routev1.Route {
 	return &routev1.Route{
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace: o.Namespace,
-			Name:      fmt.Sprintf("ignition-provider-%s", o.Name),
-		},
-		Spec: routev1.RouteSpec{
-			To: routev1.RouteTargetReference{
-				Kind: "Service",
-				Name: fmt.Sprintf("machine-config-server-%s", o.Name),
-			},
+			Namespace: machineConfigServerNamespace,
+			Name:      fmt.Sprintf("ignition-provider-%s", machineConfigServerName),
 		},
 	}
 }
 
-type Userdata struct {
-	Name                 string
-	Namespace            string
-	Version              semver.Version
-	IgnitionProviderAddr string
-}
-
-func (o Userdata) Build() *corev1.Secret {
-	secret := &corev1.Secret{}
-	secret.Name = fmt.Sprintf("user-data-%s", o.Name)
-	secret.Namespace = o.Namespace
-
-	disableTemplatingValue := []byte(base64.StdEncoding.EncodeToString([]byte("true")))
-	var userDataValue []byte
-
-	// Clear any version modifiers for this comparison
-	o.Version.Pre = nil
-	o.Version.Build = nil
-	if o.Version.GTE(semver.MustParse("4.6.0")) {
-		userDataValue = []byte(fmt.Sprintf(`{"ignition":{"config":{"merge":[{"source":"http://%s/config/master","verification":{}}]},"security":{},"timeouts":{},"version":"3.1.0"},"networkd":{},"passwd":{},"storage":{},"systemd":{}}`, o.IgnitionProviderAddr))
-	} else {
-		userDataValue = []byte(fmt.Sprintf(`{"ignition":{"config":{"append":[{"source":"http://%s/config/master","verification":{}}]},"security":{},"timeouts":{},"version":"2.2.0"},"networkd":{},"passwd":{},"storage":{},"systemd":{}}`, o.IgnitionProviderAddr))
+func MachineConfigServerUserDataSecret(machineConfigServerNamespace, machineConfigServerName string) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: machineConfigServerNamespace,
+			Name:      fmt.Sprintf("user-data-%s", machineConfigServerName),
+		},
 	}
-
-	secret.Data = map[string][]byte{
-		"disableTemplating": disableTemplatingValue,
-		"value":             userDataValue,
-	}
-	return secret
 }
