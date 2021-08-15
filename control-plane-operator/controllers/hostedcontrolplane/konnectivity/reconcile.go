1c432ab2545462fee0e3cd0b670c8ab31ef75a58
relyt0925 <lisowski@us.ibm.com>
2021-08-15 16:15:30 -0500

    up

--- control-plane-operator/controllers/hostedcontrolplane/konnectivity/reconcile.go
+++ control-plane-operator/controllers/hostedcontrolplane/konnectivity/reconcile.go
@@ -236,6 +236,7 @@ func reconcileWorkerAgentDaemonSet(daemonset *appsv1.DaemonSet, deploymentConfig
 				SecurityContext: &corev1.PodSecurityContext{
 					RunAsUser: pointer.Int64Ptr(1000),
 				},
+				HostNetwork: true,
 				Containers: []corev1.Container{
 					util.BuildContainer(konnectivityAgentContainer(), buildKonnectivityWorkerAgentContainer(image, host, port)),
 				},
