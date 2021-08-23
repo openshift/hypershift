dd525fbc2c0d3fbfec19dc99e9679014bc9a0bc5
SANJEEV-Choubey <sanjeev.choubey@ibm.com>
2021-08-23 18:10:23 +0530

    fix port issue

--- hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
+++ hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
@@ -2469,6 +2469,11 @@ func reconcileMachineConfigServerService(svc *corev1.Service) error {
 		"app": "machine-config-server",
 	}
 	var portSpec corev1.ServicePort
+	if len(svc.Spec.Ports) > 0 {
+		portSpec = svc.Spec.Ports[0]
+	} else {
+		svc.Spec.Ports = []corev1.ServicePort{portSpec}
+	}
 	portSpec.Port = int32(8443)
 	portSpec.Name = "https"
 	portSpec.Protocol = corev1.ProtocolTCP
