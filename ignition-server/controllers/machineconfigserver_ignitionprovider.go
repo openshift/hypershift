bda5c0fedb536ba057745bc0b268d2b614238780
SANJEEV-Choubey <sanjeev.choubey@ibm.com>
2021-08-24 11:12:23 +0530

    use dns

--- ignition-server/controllers/machineconfigserver_ignitionprovider.go
+++ ignition-server/controllers/machineconfigserver_ignitionprovider.go
@@ -167,8 +167,7 @@ func (p *MCSIgnitionProvider) GetPayload(ctx context.Context, releaseImage strin
 		}
 		log.Println("Log 6")
 		// Build proxy request.
-		//proxyReq, err := http.NewRequest("GET", fmt.Sprintf("https://%s.machine-config-server.%s.svc.cluster.local:8443", mcsPod.Status.PodIP, p.Namespace), nil)
-		proxyReq, err := http.NewRequest("GET", fmt.Sprintf("https://machine-config-server.%s.svc.cluster.local:8443", p.Namespace), nil)
+		proxyReq, err := http.NewRequest("GET", fmt.Sprintf("dns:///%s.machine-config-server.%s.svc.cluster.local:8443", mcsPod.Name, p.Namespace), nil)
 		if err != nil {
 			return false, fmt.Errorf("error building https request for machine config server pod: %w", err)
 		}
