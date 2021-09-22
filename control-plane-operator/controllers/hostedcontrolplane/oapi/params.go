ff92d2f843305f21fcf53e998fd5960875987f43
Seth Jennings <sjenning@redhat.com>
2021-09-22 16:31:54 -0500

    apply correct HCP component priority

--- control-plane-operator/controllers/hostedcontrolplane/oapi/params.go
+++ control-plane-operator/controllers/hostedcontrolplane/oapi/params.go
@@ -88,7 +88,7 @@ func NewOpenShiftAPIServerParams(hcp *hyperv1.HostedControlPlane, globalConfig c
 	params.OpenShiftAPIServerDeploymentConfig.SetControlPlaneIsolation(hcp)
 	params.OpenShiftOAuthAPIServerDeploymentConfig = config.DeploymentConfig{
 		Scheduling: config.Scheduling{
-			PriorityClass: config.DefaultPriorityClass,
+			PriorityClass: config.APICriticalPriorityClass,
 		},
 		LivenessProbes: config.LivenessProbes{
 			oauthContainerMain().Name: {
