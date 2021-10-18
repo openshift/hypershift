openshift/hypershift

commit f82b00f6a563d1439b0d495872dd35fa9e833af1

allow CPO image override from cluster cluster

hypershift-operator/main.go
@@ -66,7 +66,6 @@ type StartOptions struct {
 	DeploymentName        string
 	MetricsAddr           string
 	EnableLeaderElection  bool
-	OperatorImage         string
 	IgnitionServerImage   string
 	OpenTelemetryEndpoint string
 }
@@ -84,7 +83,6 @@ func NewStartCommand() *cobra.Command {
 		DeploymentName:        "operator",
 		MetricsAddr:           "0",
 		EnableLeaderElection:  false,
-		OperatorImage:         "",
 		IgnitionServerImage:   "",
 		OpenTelemetryEndpoint: "",
 	}
@@ -95,7 +93,6 @@ func NewStartCommand() *cobra.Command {
 	cmd.Flags().BoolVar(&opts.EnableLeaderElection, "enable-leader-election", opts.EnableLeaderElection,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	cmd.Flags().StringVar(&opts.OperatorImage, "operator-image", opts.OperatorImage, "A control plane operator image to use (defaults to match this operator if running in a deployment)")
 	cmd.Flags().StringVar(&opts.IgnitionServerImage, "ignition-server-image", opts.IgnitionServerImage, "An ignition server image to use (defaults to match this operator if running in a deployment)")
 	cmd.Flags().StringVar(&opts.OpenTelemetryEndpoint, "otlp-endpoint", opts.OpenTelemetryEndpoint, "An OpenTelemetry collector endpoint (e.g. localhost:4317). If specified, OTLP traces will be exported to this endpoint.")
 
@@ -157,7 +154,7 @@ func run(ctx context.Context, opts *StartOptions, log logr.Logger) error {
 		}
 		return "", fmt.Errorf("couldn't locate operator container on deployment")
 	}
-	operatorImage, err := lookupOperatorImage(kubeClient.AppsV1().Deployments(opts.Namespace), opts.DeploymentName, opts.OperatorImage)
+	operatorImage, err := lookupOperatorImage(kubeClient.AppsV1().Deployments(opts.Namespace), opts.DeploymentName, "")
 	if err != nil {
 		return fmt.Errorf("failed to find operator image: %w", err)
 	}
@@ -170,9 +167,9 @@ func run(ctx context.Context, opts *StartOptions, log logr.Logger) error {
 	log.Info("using ignition server image", "image", ignitionServerImage)
 
 	if err = (&hostedcluster.HostedClusterReconciler{
-		Client:                          mgr.GetClient(),
-		HostedControlPlaneOperatorImage: operatorImage,
-		IgnitionServerImage:             ignitionServerImage,
+		Client:                  mgr.GetClient(),
+		HypershiftOperatorImage: operatorImage,
+		IgnitionServerImage:     ignitionServerImage,
 		ReleaseProvider: &releaseinfo.CachedProvider{
 			Inner: &releaseinfo.RegistryClientProvider{},
 			Cache: map[string]*releaseinfo.ReleaseImage{},

Makefile
@@ -144,4 +144,4 @@ docker-push:
 
 .PHONY: run-local
 run-local:
-	bin/hypershift-operator run --operator-image=$(IMAGE)
+	bin/hypershift-operator run

cmd/cluster/create.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/openshift/hypershift/api/v1alpha1"
+	hyperv1 "github.com/openshift/hypershift/api/v1alpha1"
 
 	hyperapi "github.com/openshift/hypershift/api"
 	apifixtures "github.com/openshift/hypershift/api/fixtures"
@@ -24,27 +24,28 @@ import (
 )
 
 type Options struct {
-	Namespace          string
-	Name               string
-	ReleaseImage       string
-	PullSecretFile     string
-	AWSCredentialsFile string
-	SSHKeyFile         string
-	NodePoolReplicas   int32
-	Render             bool
-	InfraID            string
-	InfrastructureJSON string
-	IAMJSON            string
-	InstanceType       string
-	Region             string
-	BaseDomain         string
-	IssuerURL          string
-	PublicZoneID       string
-	PrivateZoneID      string
-	Annotations        []string
-	NetworkType        string
-	FIPS               bool
-	AutoRepair         bool
+	Namespace                 string
+	Name                      string
+	ReleaseImage              string
+	ControlPlaneOperatorImage string
+	PullSecretFile            string
+	AWSCredentialsFile        string
+	SSHKeyFile                string
+	NodePoolReplicas          int32
+	Render                    bool
+	InfraID                   string
+	InfrastructureJSON        string
+	IAMJSON                   string
+	InstanceType              string
+	Region                    string
+	BaseDomain                string
+	IssuerURL                 string
+	PublicZoneID              string
+	PrivateZoneID             string
+	Annotations               []string
+	NetworkType               string
+	FIPS                      bool
+	AutoRepair                bool
 }
 
 func NewCreateCommand() *cobra.Command {
@@ -65,27 +66,29 @@ func NewCreateCommand() *cobra.Command {
 	}
 
 	opts := Options{
-		Namespace:          "clusters",
-		Name:               "example",
-		ReleaseImage:       releaseImage,
-		PullSecretFile:     "",
-		AWSCredentialsFile: "",
-		SSHKeyFile:         "",
-		NodePoolReplicas:   2,
-		Render:             false,
-		InfrastructureJSON: "",
-		Region:             "us-east-1",
-		InfraID:            "",
-		InstanceType:       "m4.large",
-		Annotations:        []string{},
-		NetworkType:        string(v1alpha1.OpenShiftSDN),
-		FIPS:               false,
-		AutoRepair:         false,
+		Namespace:                 "clusters",
+		Name:                      "example",
+		ReleaseImage:              releaseImage,
+		ControlPlaneOperatorImage: "",
+		PullSecretFile:            "",
+		AWSCredentialsFile:        "",
+		SSHKeyFile:                "",
+		NodePoolReplicas:          2,
+		Render:                    false,
+		InfrastructureJSON:        "",
+		Region:                    "us-east-1",
+		InfraID:                   "",
+		InstanceType:              "m4.large",
+		Annotations:               []string{},
+		NetworkType:               string(hyperv1.OpenShiftSDN),
+		FIPS:                      false,
+		AutoRepair:                false,
 	}
 
 	cmd.Flags().StringVar(&opts.Namespace, "namespace", opts.Namespace, "A namespace to contain the generated resources")
 	cmd.Flags().StringVar(&opts.Name, "name", opts.Name, "A name for the cluster")
 	cmd.Flags().StringVar(&opts.ReleaseImage, "release-image", opts.ReleaseImage, "The OCP release image for the cluster")
+	cmd.Flags().StringVar(&opts.ControlPlaneOperatorImage, "control-plane-operator-image", opts.ControlPlaneOperatorImage, "Override the default image used to deploy the control plane operator")
 	cmd.Flags().StringVar(&opts.PullSecretFile, "pull-secret", opts.PullSecretFile, "Path to a pull secret (required)")
 	cmd.Flags().StringVar(&opts.AWSCredentialsFile, "aws-creds", opts.AWSCredentialsFile, "Path to an AWS credentials file (required)")
 	cmd.Flags().StringVar(&opts.SSHKeyFile, "ssh-key", opts.SSHKeyFile, "Path to an SSH key file")
@@ -136,6 +139,10 @@ func CreateCluster(ctx context.Context, opts Options) error {
 		}
 		k, v := pair[0], pair[1]
 		annotations[k] = v
+	}
+
+	if len(opts.ControlPlaneOperatorImage) > 0 {
+		annotations[hyperv1.ControlPlaneOperatorImageAnnotation] = opts.ControlPlaneOperatorImage
 	}
 
 	pullSecret, err := ioutil.ReadFile(opts.PullSecretFile)
@@ -227,7 +234,7 @@ func CreateCluster(ctx context.Context, opts Options) error {
 		BaseDomain:       infra.BaseDomain,
 		PublicZoneID:     infra.PublicZoneID,
 		PrivateZoneID:    infra.PrivateZoneID,
-		NetworkType:      v1alpha1.NetworkType(opts.NetworkType),
+		NetworkType:      hyperv1.NetworkType(opts.NetworkType),
 		FIPS:             opts.FIPS,
 		AutoRepair:       opts.AutoRepair,
 		AWS: apifixtures.ExampleAWSOptions{

hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
@@ -91,9 +91,10 @@ var NoopReconcile controllerutil.MutateFn = func() error { return nil }
 type HostedClusterReconciler struct {
 	client.Client
 
-	// HostedControlPlaneOperatorImage is the image used to deploy the hosted control
-	// plane operator.
-	HostedControlPlaneOperatorImage string
+	// HypershiftOperatorImage is the image used to deploy the control plane operator if
+	// 1) There is no hypershift.openshift.io/control-plane-operator-image annotation on the HostedCluster and
+	// 2) The OCP version being deployed is the latest version supported by Hypershift
+	HypershiftOperatorImage string
 
 	// releaseProvider looks up the OCP version for the release images in HostedClusters
 	ReleaseProvider releaseinfo.Provider
@@ -1055,7 +1056,7 @@ func (r *HostedClusterReconciler) reconcileControlPlaneOperator(ctx context.Cont
 	if !ok {
 		return fmt.Errorf("expected %s key in pull secret", corev1.DockerConfigJsonKey)
 	}
-	controlPlaneOperatorImage, err := getControlPlaneOperatorImage(ctx, hcluster, r.ReleaseProvider, r.HostedControlPlaneOperatorImage, pullSecretBytes)
+	controlPlaneOperatorImage, err := getControlPlaneOperatorImage(ctx, hcluster, r.ReleaseProvider, r.HypershiftOperatorImage, pullSecretBytes)
 	if err != nil {
 		return err
 	}

commit 7115055cc25b434fa455ce7525f5c019b3c0fee2

Fix local hypershift operator CPO flag regression
This commit fixes a regression introduced in f82b00f6a563d1439b0d495872dd35fa9e833af1
which makes it impossible to run the hypershift operator locally without having the
operator deployment installed in the cluster. The user should be able to override the
CPO fallback image with a flag like before, and deployment image lookups should only
be used when running in-cluster (i.e. without the flag).

hypershift-operator/main.go
@@ -71,6 +71,7 @@ type StartOptions struct {
 	OpenTelemetryEndpoint      string
 	EnableOCPClusterMonitoring bool
 	EnableCIDebugOutput        bool
+	ControlPlaneOperatorImage  string
 }
 
 func NewStartCommand() *cobra.Command {
@@ -82,12 +83,13 @@ func NewStartCommand() *cobra.Command {
 	}
 
 	opts := StartOptions{
-		Namespace:             "hypershift",
-		DeploymentName:        "operator",
-		MetricsAddr:           "0",
-		EnableLeaderElection:  false,
-		IgnitionServerImage:   "",
-		OpenTelemetryEndpoint: "",
+		Namespace:                 "hypershift",
+		DeploymentName:            "operator",
+		MetricsAddr:               "0",
+		EnableLeaderElection:      false,
+		ControlPlaneOperatorImage: "",
+		IgnitionServerImage:       "",
+		OpenTelemetryEndpoint:     "",
 	}
 
 	cmd.Flags().StringVar(&opts.Namespace, "namespace", opts.Namespace, "The namespace this operator lives in")
@@ -96,6 +98,7 @@ func NewStartCommand() *cobra.Command {
 	cmd.Flags().BoolVar(&opts.EnableLeaderElection, "enable-leader-election", opts.EnableLeaderElection,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+	cmd.Flags().StringVar(&opts.ControlPlaneOperatorImage, "control-plane-operator-image", opts.ControlPlaneOperatorImage, "A control plane operator image to use (defaults to match this operator if running in a deployment)")
 	cmd.Flags().StringVar(&opts.IgnitionServerImage, "ignition-server-image", opts.IgnitionServerImage, "An ignition server image to use (defaults to match this operator if running in a deployment)")
 	cmd.Flags().StringVar(&opts.OpenTelemetryEndpoint, "otlp-endpoint", opts.OpenTelemetryEndpoint, "An OpenTelemetry collector endpoint (e.g. localhost:4317). If specified, OTLP traces will be exported to this endpoint.")
 	cmd.Flags().BoolVar(&opts.EnableOCPClusterMonitoring, "enable-ocp-cluster-monitoring", opts.EnableOCPClusterMonitoring, "Development-only option that will make your OCP cluster unsupported: If the cluster Prometheus should be configured to scrape metrics")
@@ -161,7 +164,7 @@ func run(ctx context.Context, opts *StartOptions, log logr.Logger) error {
 		}
 		return "", fmt.Errorf("couldn't locate operator container on deployment")
 	}
-	operatorImage, err := lookupOperatorImage(kubeClient.AppsV1().Deployments(opts.Namespace), opts.DeploymentName, "")
+	operatorImage, err := lookupOperatorImage(kubeClient.AppsV1().Deployments(opts.Namespace), opts.DeploymentName, opts.ControlPlaneOperatorImage)
 	if err != nil {
 		return fmt.Errorf("failed to find operator image: %w", err)
 	}