openshift/hypershift

commit abe306569744055a2bb4bd8192d7309725a72514

Replace global config templates with go reconciliation code
Adds code to reconcile global configuration in the resources controller
of the hosted cluster config operator. Removes the corresponding
cluster-bootstrap template files from the control plane operator.

Moves reconciliation of CVO initial configs from manifests-bootstrapper
to the CVO deployment.

hosted-cluster-config-operator/controllers/resources/monitoring/servicemonitor.go
@@ -1,0 +1,37 @@
+package monitoring
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	prometheusoperatorv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+)
+
+func ReconcileKubeAPIServerServiceMonitor(serviceMonitor *prometheusoperatorv1.ServiceMonitor) error {
+	serviceMonitor.Spec.NamespaceSelector = prometheusoperatorv1.NamespaceSelector{
+		MatchNames: []string{"default"},
+	}
+	serviceMonitor.Spec.Selector = metav1.LabelSelector{
+		MatchLabels: map[string]string{
+			"component": "apiserver",
+		},
+	}
+	serviceMonitor.Spec.Endpoints = []prometheusoperatorv1.Endpoint{
+		{
+			BearerTokenFile: "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
+			Interval:        "30s",
+			Scheme:          "https",
+			Port:            "https",
+			Path:            "/metrics",
+			MetricRelabelConfigs: []*prometheusoperatorv1.RelabelConfig{
+				{
+					Action: "keep",
+					Regex:  "etcd_object_counts.*",
+					SourceLabels: []string{
+						"__name__",
+					},
+				},
+			},
+		},
+	}
+	return nil
+}

control-plane-operator/controllers/hostedcontrolplane/assets/assets.go
@@ -2,8 +2,6 @@ package assets
 
 import "embed"
 
-//go:embed cluster-bootstrap/*
-//go:embed install-config/*
 //go:embed user-manifests-bootstrapper/*
 var content embed.FS
 

control-plane-operator/controllers/hostedcontrolplane/assets/cluster-bootstrap/00000_namespaces-needed-for-monitoring.yaml
@@ -1,45 +1,0 @@
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: openshift-apiserver
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: openshift-controller-manager
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: openshift-kube-apiserver
-  labels:
-    openshift.io/cluster-monitoring: "true"
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: openshift-kube-scheduler
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: openshift-kube-controller-manager
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: openshift-etcd
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  labels:
-    network.openshift.io/policy-group: ingress
-    openshift.io/cluster-monitoring: "true"
-  name: openshift-ingress
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: openshift-authentication

control-plane-operator/controllers/hostedcontrolplane/assets/cluster-bootstrap/cluster-config-v1-configmap.yaml
@@ -1,8 +1,0 @@
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: cluster-config-v1
-  namespace: kube-system
-data:
-  install-config: |
-{{ include "install-config/install-config.yaml" 4 }}

control-plane-operator/controllers/hostedcontrolplane/assets/cluster-bootstrap/cluster-csr-approver-clusterrole.yaml
@@ -1,38 +1,0 @@
-# https://github.com/openshift/cluster-kube-controller-manager-operator/blob/release-4.9/bindata/assets/kube-controller-manager/csr_approver_clusterrole.yaml
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  annotations:
-    rbac.authorization.kubernetes.io/autoupdate: "true"
-  name: system:openshift:controller:cluster-csr-approver-controller
-rules:
-  - apiGroups:
-    - certificates.k8s.io
-    resources:
-    - certificatesigningrequests
-    verbs:
-    - get
-    - list
-    - watch
-  - apiGroups:
-    - certificates.k8s.io
-    resources:
-    - certificatesigningrequests/approval
-    verbs:
-    - update
-  - apiGroups:
-    - certificates.k8s.io
-    resources:
-    - signers
-    resourceNames:
-    - kubernetes.io/kube-apiserver-client
-    verbs:
-    - approve
-  - apiGroups:
-      - ""
-    resources:
-      - events
-    verbs:
-      - create
-      - patch
-      - update

control-plane-operator/controllers/hostedcontrolplane/assets/cluster-bootstrap/cluster-csr-approver-clusterrolebinding.yaml
@@ -1,15 +1,0 @@
-# https://github.com/openshift/cluster-kube-controller-manager-operator/blob/release-4.9/bindata/assets/kube-controller-manager/csr_approver_clusterrolebinding.yaml
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  annotations:
-    rbac.authorization.kubernetes.io/autoupdate: "true"
-  name: system:openshift:controller:cluster-csr-approver-controller
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: system:openshift:controller:cluster-csr-approver-controller
-subjects:
-  - kind: ServiceAccount
-    name: cluster-csr-approver-controller
-    namespace: openshift-infra

control-plane-operator/controllers/hostedcontrolplane/assets/cluster-bootstrap/cluster-dns-02-config.yaml
@@ -1,16 +1,0 @@
-apiVersion: config.openshift.io/v1
-kind: DNS
-metadata:
-  creationTimestamp: null
-  name: cluster
-spec:
-  baseDomain: {{ .BaseDomain }}
-{{- if ne .PublicZoneID "" }}
-  publicZone:
-    id: {{ .PublicZoneID }}
-{{- end }}
-{{- if ne .PrivateZoneID "" }}
-  privateZone:
-    id: {{ .PrivateZoneID }}
-{{- end }}
-status: {}

control-plane-operator/controllers/hostedcontrolplane/assets/cluster-bootstrap/cluster-infrastructure-02-config.yaml
@@ -1,33 +1,0 @@
-apiVersion: config.openshift.io/v1
-kind: Infrastructure
-metadata:
-  creationTimestamp: null
-  name: cluster
-spec:
-  cloudConfig:
-    name: ""
-{{- if eq .PlatformType "AWS" }}
-  platformSpec:
-    aws: {}
-{{- end }}
-status:
-  apiServerInternalURI: https://{{ .ExternalAPIDNSName }}:{{ .ExternalAPIPort }}
-  apiServerURL: https://{{ .ExternalAPIDNSName }}:{{ .ExternalAPIPort }}
-  etcdDiscoveryDomain: {{ .BaseDomain }}
-  infrastructureName: "{{ .InfraID }}"
-  platform: {{ .PlatformType }}
-  controlPlaneTopology: HighlyAvailable
-  infrastructureTopology: {{ .InfrastructureAvailabilityPolicy }}
-  platformStatus:
-    type: {{ .PlatformType }}
-{{- if eq .PlatformType "AWS" }}
-    aws:
-      region: {{ .AWSRegion }}
-{{- if .AWSResourceTags }}
-      resourceTags:
-{{- range $tag := .AWSResourceTags }}
-      - key: {{ $tag.Key }}
-        value: {{ $tag.Value }}
-{{- end }}
-{{- end }}
-{{- end }}

commit 715ea064d8a7fe640f08711185867a7315360f92

Fix kube-apiserver servicemonitor
This broken in
https://github.com/openshift/hypershift/commit/abe306569744055a2bb4bd8192d7309725a72514,
as the CA file was omitted and the token file was pointing to the CA
file which obviously doesn't work.

This fixes the last conformance test which currently fails because
scraping fails due to the invalid config:
```
[sig-instrumentation] Prometheus when installed on the cluster shouldn't report any alerts in firing state apart from Watchdog and AlertmanagerReceiversNotConfigured
```

hosted-cluster-config-operator/controllers/resources/monitoring/servicemonitor.go
@@ -17,11 +17,17 @@ func ReconcileKubeAPIServerServiceMonitor(serviceMonitor *prometheusoperatorv1.S
 	}
 	serviceMonitor.Spec.Endpoints = []prometheusoperatorv1.Endpoint{
 		{
-			BearerTokenFile: "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
-			Interval:        "30s",
-			Scheme:          "https",
-			Port:            "https",
-			Path:            "/metrics",
+			BearerTokenFile: "/var/run/secrets/kubernetes.io/serviceaccount/token",
+			TLSConfig: &prometheusoperatorv1.TLSConfig{
+				SafeTLSConfig: prometheusoperatorv1.SafeTLSConfig{
+					ServerName: "kubernetes.default.svc",
+				},
+				CAFile: "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
+			},
+			Interval: "30s",
+			Scheme:   "https",
+			Port:     "https",
+			Path:     "/metrics",
 			MetricRelabelConfigs: []*prometheusoperatorv1.RelabelConfig{
 				{
 					Action: "keep",