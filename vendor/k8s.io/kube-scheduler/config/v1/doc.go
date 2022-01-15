e9791e6e80a34a5d99ecc911c521d0c43e14df32
relyt0925 <lisowski@us.ibm.com>
2022-01-14 21:03:15 -0500

    Revert "Merge pull request #838 from csrwng/update_schedconfig_version"
    This reverts commit 1cb533fa4788b696842ca656fb92c4f8dee76aad, reversing
    changes made to a451775ffbfebf992987508f28fd5e1841421d4f.

--- vendor/k8s.io/kube-scheduler/config/v1/doc.go
+++ vendor/k8s.io/kube-scheduler/config/v1/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Kubernetes Authors.
+Copyright 2016 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,4 +18,4 @@ limitations under the License.
 // +k8s:openapi-gen=true
 // +groupName=kubescheduler.config.k8s.io
 
-package v1beta2 // import "k8s.io/kube-scheduler/config/v1beta2"
+package v1 // import "k8s.io/kube-scheduler/config/v1"
--- vendor/k8s.io/kube-scheduler/config/v1beta1/register.go
+++ vendor/k8s.io/kube-scheduler/config/v1beta1/register.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Kubernetes Authors.
+Copyright 2020 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1beta2
+package v1beta1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime"
@@ -25,7 +25,7 @@ import (
 const GroupName = "kubescheduler.config.k8s.io"
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1beta2"}
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1beta1"}
 
 var (
 	// SchemeBuilder is the scheme builder with scheme init functions to run for this API package
@@ -40,9 +40,13 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&KubeSchedulerConfiguration{},
 		&DefaultPreemptionArgs{},
 		&InterPodAffinityArgs{},
-		&NodeResourcesBalancedAllocationArgs{},
+		&NodeLabelArgs{},
 		&NodeResourcesFitArgs{},
 		&PodTopologySpreadArgs{},
+		&RequestedToCapacityRatioArgs{},
+		&ServiceAffinityArgs{},
+		&NodeResourcesLeastAllocatedArgs{},
+		&NodeResourcesMostAllocatedArgs{},
 		&VolumeBindingArgs{},
 		&NodeAffinityArgs{},
 	)
