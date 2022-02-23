397ef88f6e25e66d94b1545c688a6610e9928109
David Vossel <davidvossel@gmail.com>
2022-02-23 12:15:51 -0500

    Use bridge binding for kubevirt platform
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- cmd/nodepool/kubevirt/create.go
+++ cmd/nodepool/kubevirt/create.go
@@ -69,6 +69,14 @@ func (o *KubevirtPlatformCreateOptions) UpdateNodePool(_ context.Context, nodePo
 										},
 									},
 								},
+								Interfaces: []kubevirtv1.Interface{
+									kubevirtv1.Interface{
+										Name: "default",
+										InterfaceBindingMethod: kubevirtv1.InterfaceBindingMethod{
+											Bridge: &kubevirtv1.InterfaceBridge{},
+										},
+									},
+								},
 							},
 						},
 						Volumes: []kubevirtv1.Volume{
@@ -78,6 +86,14 @@ func (o *KubevirtPlatformCreateOptions) UpdateNodePool(_ context.Context, nodePo
 									ContainerDisk: &kubevirtv1.ContainerDiskSource{
 										Image: o.ContainerDiskImage,
 									},
+								},
+							},
+						},
+						Networks: []kubevirtv1.Network{
+							kubevirtv1.Network{
+								Name: "default",
+								NetworkSource: kubevirtv1.NetworkSource{
+									Pod: &kubevirtv1.PodNetwork{},
 								},
 							},
 						},
