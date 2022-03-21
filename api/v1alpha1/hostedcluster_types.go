64494682f2bf9ecc04f0cfee6a494d36a65e7eb5
Dan Mace <ironcladlou@gmail.com>
2022-03-21 15:12:41 -0400

    Introduce `OIDCConfigurationInvalid` condition for OIDC setup validation
    This commit introduces a new `OIDCConfigurationInvalid` condition for
    HostedCluster. If OIDC documents cannot be reconciled for a cluster,
    `OIDCConfigurationInvalid=True`, otherwise `OIDCConfigurationInvalid` is
    removed. The message associated with the condition tries to provide some more
    helpful context about what might be wrong to help the user correct the issue.
    More error interpretation can be added in the future.

--- api/v1alpha1/hostedcluster_types.go
+++ api/v1alpha1/hostedcluster_types.go
@@ -1044,6 +1044,10 @@ const (
 	// ReconciliationPaused indicates if reconciliation of the hostedcluster is
 	// paused.
 	ReconciliationPaused ConditionType = "ReconciliationPaused"
+
+	// OIDCConfigurationInvalid indicates if an AWS cluster's OIDC condition is
+	// detected as invalid.
+	OIDCConfigurationInvalid ConditionType = "OIDCConfigurationInvalid"
 )
 
 const (
@@ -1078,6 +1082,8 @@ const (
 	UnmanagedEtcdAsExpected          = "UnmanagedEtcdAsExpected"
 
 	InsufficientClusterCapabilitiesReason = "InsufficientClusterCapabilities"
+
+	OIDCConfigurationInvalidReason = "OIDCConfigurationInvalid"
 )
 
 // HostedClusterStatus is the latest observed status of a HostedCluster.
