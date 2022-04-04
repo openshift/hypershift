3c027a8311b0ee0b636b9e344a7e6867c43ca786
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2022-04-04 12:15:37 -0400

    Fix priority class for olm cronjob and verify priorityclasses in e2e
    The olm cronjob had a prioryClass of openshift-user-critical which has a
    priority that is above all other controlplane components in the
    management cluster. Downgrade it to the standard
    hypershift-control-plane and add an e2e test that verifies that no pod
    has a priority higher than the etcd priority.

--- test/e2e/control_plane_upgrade_test.go
+++ test/e2e/control_plane_upgrade_test.go
@@ -62,4 +62,5 @@ func TestUpgradeControlPlane(t *testing.T) {
 	e2eutil.EnsureNodeCountMatchesNodePoolReplicas(t, testContext, client, guestClient, hostedCluster.Namespace)
 	e2eutil.EnsureNoCrashingPods(t, ctx, client, hostedCluster)
 	e2eutil.EnsureHCPContainersHaveResourceRequests(t, ctx, client, hostedCluster)
+	e2eutil.EnsureNoPodsWithTooHighPriority(t, ctx, client, hostedCluster)
 }
