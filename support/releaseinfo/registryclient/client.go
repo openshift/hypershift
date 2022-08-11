693e39383fade9239408fb3715f5228d9336437a
Bryan <brcox@redhat.com>
2022-08-11 09:20:05 -0400

    Extracts correct mco image based on hosted cp arch

--- support/releaseinfo/registryclient/client.go
+++ support/releaseinfo/registryclient/client.go
@@ -12,7 +12,9 @@ import (
 	"regexp"
 
 	"github.com/docker/distribution"
+	"github.com/docker/distribution/manifest/manifestlist"
 	"github.com/docker/distribution/registry/client/transport"
+	"github.com/opencontainers/go-digest"
 	"k8s.io/client-go/rest"
 
 	dockerarchive "github.com/openshift/hypershift/support/thirdparty/docker/pkg/archive"
@@ -20,6 +22,13 @@ import (
 	"github.com/openshift/hypershift/support/thirdparty/library-go/pkg/image/registryclient"
 	"github.com/openshift/hypershift/support/thirdparty/oc/pkg/cli/image/manifest"
 	"github.com/openshift/hypershift/support/thirdparty/oc/pkg/cli/image/manifest/dockercredentials"
+)
+
+const (
+	ArchitectureAMD64   = "amd64"
+	ArchitectureS390X   = "s390x"
+	ArchitecturePPC64LE = "ppc64le"
+	ArchitectureARM64   = "arm64"
 )
 
 // ExtractImageFiles extracts a list of files from a registry image given the image reference, pull secret and the
@@ -223,6 +232,24 @@ func ExtractImageFilesToDir(ctx context.Context, imageRef string, pullSecret []b
 }
 
 func getMetadata(ctx context.Context, imageRef string, pullSecret []byte) ([]distribution.Descriptor, distribution.BlobStore, error) {
+	repo, ref, err := GetRepoSetup(ctx, imageRef, pullSecret)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to repo setup: %w", err)
+	}
+	firstManifest, location, err := manifest.FirstManifest(ctx, *ref, repo)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to obtain root manifest for %s: %w", imageRef, err)
+	}
+	_, layers, err := manifest.ManifestToImageConfig(ctx, firstManifest, repo.Blobs(ctx), location)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to obtain image layers for %s: %w", imageRef, err)
+	}
+	return layers, repo.Blobs(ctx), nil
+}
+
+// GetRepoSetup connects to a repo and pulls the imageRef's docker image information from the repo. Returns the repo and the docker image.
+func GetRepoSetup(ctx context.Context, imageRef string, pullSecret []byte) (distribution.Repository, *reference.DockerImageReference, error) {
+	var dockerImageRef *reference.DockerImageReference
 	rt, err := rest.TransportFor(&rest.Config{})
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to create secure transport: %w", err)
@@ -239,6 +266,7 @@ func getMetadata(ctx context.Context, imageRef string, pullSecret []byte) ([]dis
 		WithRequestModifiers(transport.NewHeaderRequestModifier(http.Header{http.CanonicalHeaderKey("User-Agent"): []string{rest.DefaultKubernetesUserAgent()}}))
 
 	ref, err := reference.Parse(imageRef)
+	dockerImageRef = &ref
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to parse image reference %q: %w", imageRef, err)
 	}
@@ -246,13 +274,75 @@ func getMetadata(ctx context.Context, imageRef string, pullSecret []byte) ([]dis
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to create repository client for %s: %w", ref.DockerClientDefaults().RegistryURL(), err)
 	}
-	firstManifest, location, err := manifest.FirstManifest(ctx, ref, repo)
+	return repo, dockerImageRef, nil
+}
+
+// GetManifest gets the manifest from an image
+func GetManifest(ctx context.Context, imageRef string, pullSecret []byte) (distribution.Manifest, error) {
+	repo, ref, err := GetRepoSetup(ctx, imageRef, pullSecret)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to obtain root manifest for %s: %w", imageRef, err)
+		return nil, err
 	}
-	_, layers, err := manifest.ManifestToImageConfig(ctx, firstManifest, repo.Blobs(ctx), location)
+
+	var srcDigest digest.Digest
+	if len(ref.Tag) > 0 {
+		desc, err := repo.Tags(ctx).Get(ctx, ref.Tag)
+		if err != nil {
+			return nil, err
+		}
+		srcDigest = desc.Digest
+	}
+
+	if len(ref.ID) > 0 {
+		srcDigest = digest.Digest(ref.ID)
+	}
+
+	manifests, err := repo.Manifests(ctx)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to obtain image layers for %s: %w", imageRef, err)
+		return nil, err
 	}
-	return layers, repo.Blobs(ctx), nil
+
+	digestsManifest, err := manifests.Get(ctx, srcDigest, manifest.PreferManifestList)
+	if err != nil {
+		return nil, err
+	}
+
+	return digestsManifest, nil
+}
+
+// IsMultiArchManifestList determines whether an image is a manifest listed image and contains manifests the following processor architectures: amd64, arm64, s390x, ppc64le
+func IsMultiArchManifestList(ctx context.Context, imageRef string, pullSecret []byte) (bool, error) {
+	srcManifest, err := GetManifest(ctx, imageRef, pullSecret)
+	if err != nil {
+		return false, fmt.Errorf("failed to retrieve manifest %s: %w", imageRef, err)
+	}
+
+	mediaType, payload, err := srcManifest.Payload()
+	if err != nil {
+		return false, fmt.Errorf("failed to get payload %s: %w", imageRef, err)
+	}
+
+	// mediaType for manifest listed (aka fat manifest) images is either 'application/vnd.docker.distribution.manifest.list.v2+json' per the docker documentation - https://docs.docker.com/registry/spec/manifest-v2-2/
+	// or 'application/vnd.oci.image.index.v1+json' per https://github.com/opencontainers/image-spec/blob/main/media-types.md
+	if mediaType != "application/vnd.docker.distribution.manifest.list.v2+json" && mediaType != "application/vnd.oci.image.index.v1+json" {
+		return false, nil
+	}
+
+	deserializedManifestList := new(manifestlist.DeserializedManifestList)
+	if err = deserializedManifestList.UnmarshalJSON(payload); err != nil {
+		return false, fmt.Errorf("failed to get unmarshalled manifest list: %w", err)
+	}
+
+	count := 0
+	for _, arch := range deserializedManifestList.ManifestList.Manifests {
+		switch arch.Platform.Architecture {
+		case ArchitectureAMD64, ArchitectureS390X, ArchitecturePPC64LE, ArchitectureARM64:
+			count = count + 1
+		}
+	}
+
+	if count > 1 {
+		return true, nil
+	}
+	return false, nil
 }
