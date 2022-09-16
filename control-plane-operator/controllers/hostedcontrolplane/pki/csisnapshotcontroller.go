84d95ab5ebccf5ed3e8bb495296cb4e8e17b4d64
Jan Safranek <jsafrane@redhat.com>
2022-09-16 10:05:09 +0200

    Add comment about usage of TLS keys

--- control-plane-operator/controllers/hostedcontrolplane/pki/csisnapshotcontroller.go
+++ control-plane-operator/controllers/hostedcontrolplane/pki/csisnapshotcontroller.go
@@ -7,6 +7,10 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+// Create TLS keys for csi-snapshot-webhook.
+// In standalone OCP it's created automatically when csi-snapshot-controller-operator creates Service for
+// the webhook with annotation `service.openshift.io/serving-cert-secret-name`, in HyperShift
+// it must be done by control-plane-operator.
 func ReconcileCSISnapshotWebhookTLS(secret, ca *corev1.Secret, ownerRef config.OwnerRef) error {
 	dnsNames := []string{
 		"csi-snapshot-webhook",
