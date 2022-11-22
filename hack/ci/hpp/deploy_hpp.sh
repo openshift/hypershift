c24603269cff8c5918b3fe8d1f9fe03a7fc90164
Oren Cohen <ocohen@redhat.com>
2022-11-22 16:40:55 +0200

    Use HPP with CSI Driver for kubevirt provider in CI
    Signed-off-by: Oren Cohen <ocohen@redhat.com>

--- hack/ci/hpp/deploy_hpp.sh
+++ hack/ci/hpp/deploy_hpp.sh
@@ -1,53 +1,79 @@
 #!/bin/bash
 
-## HostPath Provisioner Deployment & Configuration
-#  Used as a temporary workaround for https://github.com/kubernetes-sigs/gcp-compute-persistent-disk-csi-driver/issues/608
-
 set -ex
 
+readonly HPP_DEFAULT_STORAGE_CLASS="${HPP_DEFAULT_STORAGE_CLASS:-hostpath-csi-basic}"
 readonly SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-readonly HCO_NAMESPACE="openshift-cnv"
 
-echo_debug()
-{
-    echo "$@" >&2
-}
+HPP_VOLUME_SIZE=${HPP_VOLUME_SIZE:-${VOLUME_SIZE:-70}}Gi
+VOLUME_BINDING_MODE="WaitForFirstConsumer"
 
-# Wait until master and worker MCP are Updated
-# or timeout after 90min.
-wait_mcp_for_updated()
-{
-    local mcp_updated="false"
+CLUSTER_PLATFORM=$(
+  oc get infrastructure cluster \
+    --output=jsonpath='{$.status.platform}'
+)
 
-    sleep 30
+case "${CLUSTER_PLATFORM}" in
+  Azure)
+    HPP_BACKEND_STORAGE_CLASS=managed-csi
+    ;;
+  GCP)
+    HPP_BACKEND_STORAGE_CLASS=standard-csi
+    ;;
+  AWS)
+    HPP_BACKEND_STORAGE_CLASS=gp2
+    ;;
+  OpenStack)
+    HPP_BACKEND_STORAGE_CLASS=local-block-hpp
+    ;;
+  BareMetal)
+    HPP_BACKEND_STORAGE_CLASS=ocs-storagecluster-ceph-rbd
+    ;;
+  None)
+  # UPI Installation
+    HPP_BACKEND_STORAGE_CLASS=${HPP_BACKEND_STORAGE_CLASS:-ocs-storagecluster-ceph-rbd}
+    ;;
+  *)
+    echo "[ERROR] Unsupported cluster platform: [${CLUSTER_PLATFORM}]" >&2
+    exit 1
+    ;;
+esac
 
-    for i in {1..60}
-    do
-      echo_debug "Attempt ${i}/60"
-      sleep 30
-      if oc wait mcp --all --for condition=updated --timeout=1m; then
-        echo_debug "MCP is Updated"
-        mcp_updated="true"
-        break
-      fi
-    done
+# Determine volume size for local-block-hpp SC
+if [[ "${HPP_BACKEND_STORAGE_CLASS}" = "local-block-hpp" ]] ;
+then
+  HPP_VOLUME_SIZE=$(
+    oc get persistentvolumes \
+      --selector="storage.openshift.com/local-volume-owner-name=${HPP_BACKEND_STORAGE_CLASS}" \
+      --output=jsonpath='{range $.items[*]}{$.spec.capacity.storage}{"\n"}{end}' \
+      | sort -h | head -n 1  # Get the size of the smaller PV
+  )
+  if [[ -z "${HPP_VOLUME_SIZE}" ]]; then
+    echo "[ERROR] Couldn't get the size of the local PVs." >&2
+    echo "Please check local-storage operator configuration." >&2
+    exit 1
+  fi
+fi
 
-    if [[ "$mcp_updated" == "false" ]]; then
-      echo_debug "Error: MCP didn't get Updated!!"
-      exit 1
-    fi
-}
+# Create HPP CustomResource using the StoragePool feature
+sed "${SCRIPT_DIR}/10_hpp_pool_cr.yaml" \
+  -e "s|^\( \+storage\): .*|\1: ${HPP_VOLUME_SIZE}|g" \
+  -e "s|^\( \+storageClassName\): .*|\1: ${HPP_BACKEND_STORAGE_CLASS}|g" \
+| oc create --filename=-
 
-# Create and mount a dedicated partition for PersistentVolumes provisioned by HPP
-# =>  https://github.com/kubevirt/hostpath-provisioner-operator/blob/master/contrib/machineconfig-selinux-hpp.yaml
-oc create --filename="${SCRIPT_DIR}/00_hpp_mc.yaml" -n ${HCO_NAMESPACE} || true  # Don't fail if resource already exists
-wait_mcp_for_updated
+# Create HPP StorageClass using the StoragePool feature
+sed "${SCRIPT_DIR}/30_hpp_pool_sc.yaml" \
+  -e "s|^\(volumeBindingMode\): .*|\1: ${VOLUME_BINDING_MODE}|g" \
+| oc create --filename=-
 
-# Create HPP CustomResource and StorageClass
-oc create --filename="${SCRIPT_DIR}/10_hpp_cr.yaml" -n ${HCO_NAMESPACE}
-oc create --filename="${SCRIPT_DIR}/20_hpp_sc.yaml"
-oc create --filename="${SCRIPT_DIR}/30_hpp_csi_sc.yaml"
 
-# Set HPP as default StorageClass for the cluster
-oc annotate storageclasses --all storageclass.kubernetes.io/is-default-class-
-oc annotate storageclass hostpath-provisioner storageclass.kubernetes.io/is-default-class='true'
+# Set HPP_DEFAULT_STORAGE_CLASS as default StorageClass for the cluster
+oc get storageclass -o name | xargs oc patch -p '{"metadata": {"annotations": {"storageclass.kubernetes.io/is-default-class": "false"}}}'
+oc patch storageclass "${HPP_DEFAULT_STORAGE_CLASS}" -p '{"metadata": {"annotations": {"storageclass.kubernetes.io/is-default-class": "true"}}}'
+echo "[DEBUG] Printing Storage Classes:"
+oc get storageclasses
+
+# Wait for HPP to be ready
+oc wait hostpathprovisioner hostpath-provisioner --for=condition='Available' --timeout='10m'
+
+oc patch csidriver kubevirt.io.hostpath-provisioner --type merge --patch '{"spec": {"storageCapacity": false}}'
