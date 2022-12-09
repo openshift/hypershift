6d862d189fee83a20a2a0a5500f9f9e09bbfdc99
Jan Šafránek <jsafrane@redhat.com>
2022-12-09 07:58:34 +0100

    STOR-1037: Add cluster-storage-operator to mgmt. cluster (#1748)
    * Add cluster-storage-operator
    
    * Don't install cluster-storage-operator in the guest cluster
    
    It will be installed by control-plane-operator in the mgmt cluster.
    
    * Fix RBACs for cluster-storage-operator
    
    Right now, the operator needs more permissions that would be necessary, but
    we need to fix cluster-storage-operator and aws-ebs-csi-driver-operators
    first.
    
    * Add env. var replacer + unit test for it
    
    * Add readme for asset files
    
    * Rename roles

--- control-plane-operator/controllers/hostedcontrolplane/cvo/reconcile.go
+++ control-plane-operator/controllers/hostedcontrolplane/cvo/reconcile.go
@@ -80,6 +80,7 @@ var (
 		"0000_50_cluster-image-registry-operator_07-operator-ibm-cloud-managed.yaml",
 		"0000_50_cluster-image-registry-operator_07-operator-service.yaml",
 		"0000_90_cluster-image-registry-operator_02_operator-servicemonitor.yaml",
+		"0000_50_cluster-storage-operator_10_deployment-ibm-cloud-managed.yaml",
 
 		// TODO: Remove these when cluster profiles annotations are fixed
 		"0000_50_cloud-credential-operator_01-operator-config.yaml",
@@ -249,6 +250,12 @@ func resourcesToRemove() []resourceDesc {
 			kind:       "Deployment",
 			name:       "cluster-image-registry-operator",
 			namespace:  "openshift-image-registry",
+		},
+		{
+			apiVersion: "apps/v1",
+			kind:       "Deployment",
+			name:       "cluster-storage-operator",
+			namespace:  "openshift-cluster-storage-operator",
 		},
 		{
 			apiVersion: "apps/v1",
