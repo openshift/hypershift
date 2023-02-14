502989503d7bb1e3c880bb5d883d55eaeabdf942
Brett Tofel <btofel@redhat.com>
2023-02-14 11:21:43 -0600

    Revert "Worker cluster visible change"
    This reverts commit 28c3c036dbce64d3a620a7c432d40e773e0b7aab.

--- hypershift-operator/controllers/manifests/monitoring/resources.go
+++ hypershift-operator/controllers/manifests/monitoring/resources.go
@@ -9,7 +9,7 @@ import (
 func MonitoringNamespace() *corev1.Namespace {
 	return &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "openshift-monitoring-x",
+			Name: "openshift-monitoring",
 		},
 	}
 }
