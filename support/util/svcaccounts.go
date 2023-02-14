4d34e7cd4edeb3743210b6bfbf7d42c437817d53
Mulham Raee <mraee@redhat.com>
2023-02-14 18:15:16 +0100

    Validate etcd KMS config
    This intorduces a new condition indicating whether both key and role are compatible/operational

--- support/util/svcaccounts.go
+++ support/util/svcaccounts.go
@@ -1,7 +1,14 @@
 package util
 
 import (
+	"context"
+	"fmt"
+
+	authenticationv1 "k8s.io/api/authentication/v1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 func EnsurePullSecret(serviceAccount *corev1.ServiceAccount, secretName string) {
@@ -14,4 +21,28 @@ func EnsurePullSecret(serviceAccount *corev1.ServiceAccount, secretName string) 
 	serviceAccount.ImagePullSecrets = append(serviceAccount.ImagePullSecrets, corev1.LocalObjectReference{
 		Name: secretName,
 	})
+}
+
+func CreateTokenForServiceAccount(ctx context.Context, serviceAccount *corev1.ServiceAccount, client *kubernetes.Clientset) (string, error) {
+	serviceAccount, err := client.CoreV1().ServiceAccounts(serviceAccount.Namespace).Get(ctx, serviceAccount.Name, metav1.GetOptions{})
+	if err != nil {
+		if !apierrors.IsNotFound(err) {
+			return "", fmt.Errorf("failed to get serviceaccount: %w", err)
+		}
+		return "", fmt.Errorf("serviceaccount not found: %w", err)
+	}
+
+	treq := &authenticationv1.TokenRequest{
+		Spec: authenticationv1.TokenRequestSpec{
+			Audiences: []string{"openshift"},
+		},
+	}
+
+	// Create the service account token
+	token, err := client.CoreV1().ServiceAccounts(serviceAccount.Namespace).CreateToken(ctx, serviceAccount.Name, treq, metav1.CreateOptions{})
+	if err != nil {
+		return "", fmt.Errorf("failed to create token: %w", err)
+	}
+
+	return token.Status.Token, nil
 }
