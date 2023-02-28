8438b120a8a32a7531272c570ba38f8f8cecc363
Mulham Raee <mraee@redhat.com>
2023-02-28 14:51:43 +0100

    Refactor e2e nodepool tests

--- test/e2e/nodepool_autorepair_test.go
+++ test/e2e/nodepool_autorepair_test.go
@@ -16,112 +16,81 @@ import (
 	"github.com/openshift/hypershift/cmd/cluster/core"
 	awsutil "github.com/openshift/hypershift/cmd/infra/aws/util"
 	e2eutil "github.com/openshift/hypershift/test/e2e/util"
-	"k8s.io/apimachinery/pkg/api/errors"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func testNodePoolAutoRepair(parentCtx context.Context, mgmtClient crclient.Client, hostedCluster *hyperv1.HostedCluster, hostedClusterClient crclient.Client, clusterOpts core.CreateOptions) func(t *testing.T) {
-	return func(t *testing.T) {
-		t.Parallel()
-		g := NewWithT(t)
+type NodePoolAutoRepairTest struct {
+	ctx context.Context
 
-		ctx, cancel := context.WithCancel(parentCtx)
-		originalNP := hyperv1.NodePool{}
-		defer func() {
-			t.Log("Test: NodePoolAutoRepair finished")
-			cancel()
-		}()
+	hostedCluster       *hyperv1.HostedCluster
+	hostedClusterClient crclient.Client
+	clusterOpts         core.CreateOptions
+}
 
-		// List NodePools (should exists only one)
-		nodePools := &hyperv1.NodePoolList{}
-		err := mgmtClient.List(ctx, nodePools, &crclient.ListOptions{
-			Namespace: hostedCluster.Namespace,
-		})
-		g.Expect(err).NotTo(HaveOccurred(), "failed getting existant nodepools")
-		for _, nodePool := range nodePools.Items {
-			if !strings.Contains(nodePool.Name, "-test-") {
-				originalNP = nodePool
-			}
-		}
-		g.Expect(originalNP.Name).NotTo(BeEmpty())
-		g.Expect(originalNP.Name).NotTo(ContainSubstring("test"))
-		awsNPInfo := originalNP.Spec.Platform.AWS
-
-		// Define a new Nodepool
-		nodePool := &hyperv1.NodePool{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "NodePool",
-				APIVersion: hyperv1.GroupVersion.String(),
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      hostedCluster.Name + "-" + "test-autorepair",
-				Namespace: hostedCluster.Namespace,
-			},
-			Spec: hyperv1.NodePoolSpec{
-				Management: hyperv1.NodePoolManagement{
-					UpgradeType: hyperv1.UpgradeTypeReplace,
-					AutoRepair:  true,
-				},
-				ClusterName: hostedCluster.Name,
-				Replicas:    &oneReplicas,
-				Release: hyperv1.Release{
-					Image: hostedCluster.Spec.Release.Image,
-				},
-				Platform: hyperv1.NodePoolPlatform{
-					Type: hostedCluster.Spec.Platform.Type,
-					AWS:  awsNPInfo,
-				},
-			},
-		}
-
-		// Create NodePool for current test
-		err = mgmtClient.Create(ctx, nodePool)
-		if err != nil {
-			if !errors.IsAlreadyExists(err) {
-				t.Fatalf("failed to create nodePool %s with Autorepair function: %v", nodePool.Name, err)
-			}
-			err = nodePoolRecreate(t, ctx, nodePool, mgmtClient)
-			g.Expect(err).NotTo(HaveOccurred(), "failed to Create the NodePool")
-		}
-
-		numNodes := int32(1)
-
-		t.Logf("Waiting for Nodes %d\n", numNodes)
-		nodes := e2eutil.WaitForNReadyNodesByNodePool(t, ctx, hostedClusterClient, numNodes, hostedCluster.Spec.Platform.Type, nodePool.Name)
-		t.Logf("Desired replicas available for nodePool: %v", nodePool.Name)
-
-		// Wait for the rollout to be reported complete
-		t.Logf("Waiting for cluster rollout. Image: %s", globalOpts.LatestReleaseImage)
-		e2eutil.WaitForImageRollout(t, ctx, mgmtClient, hostedCluster, globalOpts.LatestReleaseImage)
-
-		// Terminate one of the machines belonging to the cluster
-		t.Log("Terminating AWS Instance with a autorepare NodePool")
-		nodeToReplace := nodes[0].Name
-		awsSpec := nodes[0].Spec.ProviderID
-		g.Expect(len(awsSpec)).NotTo(BeZero())
-		instanceID := awsSpec[strings.LastIndex(awsSpec, "/")+1:]
-		t.Logf("Terminating AWS instance: %s", instanceID)
-		ec2client := ec2Client(clusterOpts.AWSPlatform.AWSCredentialsFile, clusterOpts.AWSPlatform.Region)
-		_, err = ec2client.TerminateInstances(&ec2.TerminateInstancesInput{
-			InstanceIds: []*string{aws.String(instanceID)},
-		})
-		g.Expect(err).NotTo(HaveOccurred(), "failed to terminate AWS instance")
-
-		// Wait for nodes to be ready again, without the node that was terminated
-		t.Logf("Waiting for %d available nodes without %s", numNodes, nodeToReplace)
-		err = wait.PollUntil(30*time.Second, func() (done bool, err error) {
-			nodes := e2eutil.WaitForNReadyNodesByNodePool(t, ctx, hostedClusterClient, numNodes, hostedCluster.Spec.Platform.Type, nodePool.Name)
-			for _, node := range nodes {
-				if node.Name == nodeToReplace {
-					return false, nil
-				}
-			}
-			return true, nil
-		}, ctx.Done())
-		g.Expect(err).NotTo(HaveOccurred(), "failed to wait for new node to become available")
+func NewNodePoolAutoRepairTest(ctx context.Context, hostedCluster *hyperv1.HostedCluster, hcClient crclient.Client, clusterOpts core.CreateOptions) *NodePoolAutoRepairTest {
+	return &NodePoolAutoRepairTest{
+		ctx:                 ctx,
+		hostedCluster:       hostedCluster,
+		hostedClusterClient: hcClient,
+		clusterOpts:         clusterOpts,
 	}
+}
+
+func (ar *NodePoolAutoRepairTest) Setup(t *testing.T) {
+	if globalOpts.Platform != hyperv1.AWSPlatform {
+		t.Skip("test only supported on platform AWS")
+	}
+}
+
+func (ar *NodePoolAutoRepairTest) BuildNodePoolManifest(defaultNodepool hyperv1.NodePool) (*hyperv1.NodePool, error) {
+	nodePool := &hyperv1.NodePool{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      ar.hostedCluster.Name + "-" + "test-autorepair",
+			Namespace: ar.hostedCluster.Namespace,
+		},
+	}
+	defaultNodepool.Spec.DeepCopyInto(&nodePool.Spec)
+
+	nodePool.Spec.Replicas = &oneReplicas
+	nodePool.Spec.Management.AutoRepair = true
+
+	return nodePool, nil
+}
+
+func (ar *NodePoolAutoRepairTest) Run(t *testing.T, nodePool hyperv1.NodePool, nodes []corev1.Node) {
+	g := NewWithT(t)
+
+	// Terminate one of the machines belonging to the cluster
+	t.Log("Terminating AWS Instance with a autorepair NodePool")
+	nodeToReplace := nodes[0].Name
+	awsSpec := nodes[0].Spec.ProviderID
+	g.Expect(len(awsSpec)).NotTo(BeZero())
+	instanceID := awsSpec[strings.LastIndex(awsSpec, "/")+1:]
+	t.Logf("Terminating AWS instance: %s", instanceID)
+	ec2client := ec2Client(ar.clusterOpts.AWSPlatform.AWSCredentialsFile, ar.clusterOpts.AWSPlatform.Region)
+	_, err := ec2client.TerminateInstances(&ec2.TerminateInstancesInput{
+		InstanceIds: []*string{aws.String(instanceID)},
+	})
+	g.Expect(err).NotTo(HaveOccurred(), "failed to terminate AWS instance")
+
+	numNodes := *nodePool.Spec.Replicas
+
+	// Wait for nodes to be ready again, without the node that was terminated
+	t.Logf("Waiting for %d available nodes without %s", numNodes, nodeToReplace)
+	err = wait.PollUntil(30*time.Second, func() (done bool, err error) {
+		nodes := e2eutil.WaitForNReadyNodesByNodePool(t, ar.ctx, ar.hostedClusterClient, numNodes, ar.hostedCluster.Spec.Platform.Type, nodePool.Name)
+		for _, node := range nodes {
+			if node.Name == nodeToReplace {
+				return false, nil
+			}
+		}
+		return true, nil
+	}, ar.ctx.Done())
+	g.Expect(err).NotTo(HaveOccurred(), "failed to wait for new node to become available")
+
 }
 
 func ec2Client(awsCredsFile, region string) *ec2.EC2 {
--- test/e2e/nodepool_nto_machineconfig_test.go
+++ test/e2e/nodepool_nto_machineconfig_test.go
@@ -7,19 +7,18 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
 	. "github.com/onsi/gomega"
 
 	hyperv1 "github.com/openshift/hypershift/api/v1beta1"
-	"github.com/openshift/hypershift/cmd/cluster/core"
 	e2eutil "github.com/openshift/hypershift/test/e2e/util"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/util/wait"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -50,285 +49,137 @@ spec:
 	tuningConfigKey             = "tuning"
 )
 
-func testNTOMachineConfigGetsRolledOut(parentCtx context.Context, mgmtClient crclient.Client, hostedCluster *hyperv1.HostedCluster, hostedClusterClient crclient.Client, clusterOpts core.CreateOptions) func(t *testing.T) {
-	return func(t *testing.T) {
-		t.Parallel()
-		g := NewWithT(t)
+type NTOMachineConfigRolloutTest struct {
+	ctx        context.Context
+	mgmtClient crclient.Client
 
-		ctx, cancel := context.WithCancel(parentCtx)
-		originalNP := hyperv1.NodePool{}
-		defer func() {
-			t.Log("Test: NTO MachineConfig Replace finished")
-			cancel()
-		}()
+	hostedCluster       *hyperv1.HostedCluster
+	hostedClusterClient crclient.Client
+}
 
-		// List NodePools (should exists only one)
-		nodePools := &hyperv1.NodePoolList{}
-		err := mgmtClient.List(ctx, nodePools, &crclient.ListOptions{
-			Namespace: hostedCluster.Namespace,
-		})
-		g.Expect(err).NotTo(HaveOccurred(), "failed getting existant nodepools")
-		for _, nodePool := range nodePools.Items {
-			if !strings.Contains(nodePool.Name, "-test-") {
-				originalNP = nodePool
-			}
-		}
-		g.Expect(originalNP.Name).NotTo(BeEmpty())
-		g.Expect(originalNP.Name).NotTo(ContainSubstring("test"))
-		awsNPInfo := originalNP.Spec.Platform.AWS
-
-		// Define a new Nodepool
-		nodePool := &hyperv1.NodePool{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "NodePool",
-				APIVersion: hyperv1.GroupVersion.String(),
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      hostedCluster.Name + "-" + "test-ntomachineconfig-replace",
-				Namespace: hostedCluster.Namespace,
-			},
-			Spec: hyperv1.NodePoolSpec{
-				Management: hyperv1.NodePoolManagement{
-					UpgradeType: hyperv1.UpgradeTypeReplace,
-					AutoRepair:  true,
-					Replace: &hyperv1.ReplaceUpgrade{
-						Strategy: hyperv1.UpgradeStrategyRollingUpdate,
-						RollingUpdate: &hyperv1.RollingUpdate{
-							MaxUnavailable: func(v intstr.IntOrString) *intstr.IntOrString { return &v }(intstr.FromInt(0)),
-							MaxSurge:       func(v intstr.IntOrString) *intstr.IntOrString { return &v }(intstr.FromInt(int(twoReplicas))),
-						},
-					},
-				},
-				ClusterName: hostedCluster.Name,
-				Replicas:    &twoReplicas,
-				Release: hyperv1.Release{
-					Image: hostedCluster.Spec.Release.Image,
-				},
-				Platform: hyperv1.NodePoolPlatform{
-					Type: hostedCluster.Spec.Platform.Type,
-					AWS:  awsNPInfo,
-				},
-			},
-		}
-		// Create NodePool for current test
-		err = mgmtClient.Create(ctx, nodePool)
-		if err != nil {
-			if !errors.IsAlreadyExists(err) {
-				t.Fatalf("failed to create nodePool %s with Autorepair function: %v", nodePool.Name, err)
-			}
-			err = nodePoolRecreate(t, ctx, nodePool, mgmtClient)
-			g.Expect(err).NotTo(HaveOccurred(), "failed to Create the NodePool")
-		}
-
-		numNodes := twoReplicas
-
-		t.Logf("Waiting for Nodes %d\n", numNodes)
-		nodes := e2eutil.WaitForNReadyNodesByNodePool(t, ctx, hostedClusterClient, numNodes, hostedCluster.Spec.Platform.Type, nodePool.Name)
-		t.Logf("Desired replicas available for nodePool: %v", nodePool.Name)
-
-		// Wait for the rollout to be reported complete
-		t.Logf("Waiting for cluster rollout. Image: %s", globalOpts.LatestReleaseImage)
-
-		e2eutil.WaitForImageRollout(t, ctx, mgmtClient, hostedCluster, globalOpts.LatestReleaseImage)
-
-		tuningConfigConfigMap := &corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hugepages-tuned-test",
-				Namespace: hostedCluster.Namespace,
-			},
-			Data: map[string]string{tuningConfigKey: hugepagesTuned},
-		}
-		if err := mgmtClient.Create(ctx, tuningConfigConfigMap); err != nil {
-			if !errors.IsAlreadyExists(err) {
-				t.Fatalf("failed to create configmap for custom Tuned object: %v", err)
-			}
-		}
-
-		np := nodePool.DeepCopy()
-		nodePool.Spec.TuningConfig = append(nodePool.Spec.TuningConfig, corev1.LocalObjectReference{Name: tuningConfigConfigMap.Name})
-		if err := mgmtClient.Patch(ctx, nodePool, crclient.MergeFrom(np)); err != nil {
-			t.Fatalf("failed to update nodepool %s after adding Tuned config: %v", nodePool.Name, err)
-		}
-
-		// DS Customization
-		ds := ntoMachineConfigUpdatedVerificationDS.DeepCopy()
-		dsName := ds.Name + "-replace"
-		e2eutil.CorrelateDaemonSet(ds, nodePool, dsName)
-
-		if err := hostedClusterClient.Create(ctx, ds); err != nil {
-			if !errors.IsAlreadyExists(err) {
-				t.Fatalf("failed to create %s DaemonSet in guestcluster: %v", ds.Name, err)
-			}
-		}
-
-		t.Logf("waiting for rollout of NodePools with NTO-generated config")
-		err = wait.PollImmediateWithContext(ctx, 10*time.Second, 15*time.Minute, func(ctx context.Context) (bool, error) {
-			if ctx.Err() != nil {
-				return false, err
-			}
-			pods := &corev1.PodList{}
-			if err := hostedClusterClient.List(ctx, pods, crclient.InNamespace(ds.Namespace), crclient.MatchingLabels(ds.Spec.Selector.MatchLabels)); err != nil {
-				t.Logf("WARNING: failed to list pods, will retry: %v", err)
-				return false, nil
-			}
-
-			if len(pods.Items) != len(nodes) {
-				return false, nil
-			}
-
-			for _, pod := range pods.Items {
-				if !isPodReady(&pod) {
-					return false, nil
-				}
-			}
-
-			return true, nil
-		})
-
-		g.Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed waiting for all pods in the NTO MachineConfig update verification DS to be ready: %v", err))
-		g.Expect(nodePool.Status.Replicas).To(BeEquivalentTo(len(nodes)))
-		e2eutil.EnsureNoCrashingPods(t, ctx, mgmtClient, hostedCluster)
-		e2eutil.EnsureAllContainersHavePullPolicyIfNotPresent(t, ctx, mgmtClient, hostedCluster)
-		e2eutil.EnsureHCPContainersHaveResourceRequests(t, ctx, mgmtClient, hostedCluster)
-		e2eutil.EnsureNoPodsWithTooHighPriority(t, ctx, mgmtClient, hostedCluster)
+func NewNTOMachineConfigRolloutTest(ctx context.Context, mgmtClient crclient.Client, hostedCluster *hyperv1.HostedCluster, hcClient crclient.Client) *NTOMachineConfigRolloutTest {
+	return &NTOMachineConfigRolloutTest{
+		ctx:                 ctx,
+		hostedCluster:       hostedCluster,
+		hostedClusterClient: hcClient,
+		mgmtClient:          mgmtClient,
 	}
 }
 
-func testNTOMachineConfigAppliedInPlace(parentCtx context.Context, mgmtClient crclient.Client, hostedCluster *hyperv1.HostedCluster, hostedClusterClient crclient.Client, clusterOpts core.CreateOptions) func(t *testing.T) {
-	return func(t *testing.T) {
-		t.Parallel()
-		g := NewWithT(t)
+func (mc *NTOMachineConfigRolloutTest) Setup(t *testing.T) {}
 
-		ctx, cancel := context.WithCancel(parentCtx)
-		originalNP := hyperv1.NodePool{}
-		defer func() {
-			t.Log("Test: NTO MachineConfig InPlace finished")
-			cancel()
-		}()
-
-		// List NodePools (should exists only one)
-		nodePools := &hyperv1.NodePoolList{}
-		err := mgmtClient.List(ctx, nodePools, &crclient.ListOptions{
-			Namespace: hostedCluster.Namespace,
-		})
-		g.Expect(err).NotTo(HaveOccurred(), "failed getting existant nodepools")
-		for _, nodePool := range nodePools.Items {
-			if !strings.Contains(nodePool.Name, "-test-") {
-				originalNP = nodePool
-			}
-		}
-		g.Expect(originalNP.Name).NotTo(BeEmpty())
-		g.Expect(originalNP.Name).NotTo(ContainSubstring("test"))
-		awsNPInfo := originalNP.Spec.Platform.AWS
-
-		// Define a new Nodepool
-		nodePool := &hyperv1.NodePool{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "NodePool",
-				APIVersion: hyperv1.GroupVersion.String(),
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      hostedCluster.Name + "-" + "test-ntomachineconfig-inplace",
-				Namespace: hostedCluster.Namespace,
-			},
-			Spec: hyperv1.NodePoolSpec{
-				Management: hyperv1.NodePoolManagement{
-					UpgradeType: hyperv1.UpgradeTypeInPlace,
-					AutoRepair:  true,
-				},
-				ClusterName: hostedCluster.Name,
-				Replicas:    &twoReplicas,
-				Release: hyperv1.Release{
-					Image: hostedCluster.Spec.Release.Image,
-				},
-				Platform: hyperv1.NodePoolPlatform{
-					Type: hostedCluster.Spec.Platform.Type,
-					AWS:  awsNPInfo,
-				},
-			},
-		}
-		// Create NodePool for current test
-		err = mgmtClient.Create(ctx, nodePool)
-		if err != nil {
-			if !errors.IsAlreadyExists(err) {
-				t.Fatalf("failed to create nodePool %s with Autorepair function: %v", nodePool.Name, err)
-			}
-			err = nodePoolRecreate(t, ctx, nodePool, mgmtClient)
-			g.Expect(err).NotTo(HaveOccurred(), "failed to Create the NodePool")
-		}
-
-		numNodes := twoReplicas
-
-		t.Logf("Waiting for Nodes %d\n", numNodes)
-		nodes := e2eutil.WaitForNReadyNodesByNodePool(t, ctx, hostedClusterClient, numNodes, hostedCluster.Spec.Platform.Type, nodePool.Name)
-		t.Logf("Desired replicas available for nodePool: %v", nodePool.Name)
-
-		// Wait for the rollout to be reported complete
-		t.Logf("Waiting for cluster rollout. Image: %s", globalOpts.LatestReleaseImage)
-		e2eutil.WaitForImageRollout(t, ctx, mgmtClient, hostedCluster, globalOpts.LatestReleaseImage)
-
-		tuningConfigConfigMap := &corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hugepages-tuned-test-inplace",
-				Namespace: hostedCluster.Namespace,
-			},
-			Data: map[string]string{tuningConfigKey: hugepagesTuned},
-		}
-
-		if err := mgmtClient.Create(ctx, tuningConfigConfigMap); err != nil {
-			if !errors.IsAlreadyExists(err) {
-				t.Fatalf("failed to create configmap for custom Tuned object: %v", err)
-			}
-		}
-
-		np := nodePool.DeepCopy()
-		nodePool.Spec.TuningConfig = append(nodePool.Spec.TuningConfig, corev1.LocalObjectReference{Name: tuningConfigConfigMap.Name})
-		if err := mgmtClient.Patch(ctx, nodePool, crclient.MergeFrom(np)); err != nil {
-			t.Fatalf("failed to update nodepool %s after adding Tuned config: %v", nodePool.Name, err)
-		}
-
-		// DS Customization
-		ds := ntoMachineConfigUpdatedVerificationDS.DeepCopy()
-		dsName := ds.Name + "-inplace"
-		e2eutil.CorrelateDaemonSet(ds, nodePool, dsName)
-
-		if err := hostedClusterClient.Create(ctx, ds); err != nil {
-			if !errors.IsAlreadyExists(err) {
-				t.Fatalf("failed to create %s DaemonSet in guestcluster: %v", ds.Name, err)
-			}
-		}
-
-		t.Logf("waiting for NodePools in-place update with NTO-generated MachineConfig")
-		err = wait.PollImmediateWithContext(ctx, 5*time.Second, 15*time.Minute, func(ctx context.Context) (bool, error) {
-			if ctx.Err() != nil {
-				return false, err
-			}
-			pods := &corev1.PodList{}
-			if err := hostedClusterClient.List(ctx, pods, crclient.InNamespace(ds.Namespace), crclient.MatchingLabels(ds.Spec.Selector.MatchLabels)); err != nil {
-				t.Logf("WARNING: failed to list pods, will retry: %v", err)
-				return false, nil
-			}
-
-			if len(pods.Items) != len(nodes) {
-				return false, nil
-			}
-
-			for _, pod := range pods.Items {
-				if !isPodReady(&pod) {
-					return false, nil
-				}
-			}
-
-			return true, nil
-		})
-
-		g.Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed waiting for all pods in the NTO MachineConfig update verification DS to be ready: %v", err))
-		g.Expect(nodePool.Status.Replicas).To(BeEquivalentTo(len(nodes)))
-		e2eutil.EnsureNoCrashingPods(t, ctx, mgmtClient, hostedCluster)
-		e2eutil.EnsureAllContainersHavePullPolicyIfNotPresent(t, ctx, mgmtClient, hostedCluster)
-		e2eutil.EnsureHCPContainersHaveResourceRequests(t, ctx, mgmtClient, hostedCluster)
-		e2eutil.EnsureNoPodsWithTooHighPriority(t, ctx, mgmtClient, hostedCluster)
+func (mc *NTOMachineConfigRolloutTest) BuildNodePoolManifest(defaultNodepool hyperv1.NodePool) (*hyperv1.NodePool, error) {
+	nodePool := &hyperv1.NodePool{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      mc.hostedCluster.Name + "-" + "test-ntomachineconfig-replace",
+			Namespace: mc.hostedCluster.Namespace,
+		},
 	}
+	defaultNodepool.Spec.DeepCopyInto(&nodePool.Spec)
+
+	nodePool.Spec.Replicas = &twoReplicas
+	nodePool.Spec.Management.Replace = &hyperv1.ReplaceUpgrade{
+		Strategy: hyperv1.UpgradeStrategyRollingUpdate,
+		RollingUpdate: &hyperv1.RollingUpdate{
+			MaxUnavailable: func(v intstr.IntOrString) *intstr.IntOrString { return &v }(intstr.FromInt(0)),
+			MaxSurge:       func(v intstr.IntOrString) *intstr.IntOrString { return &v }(intstr.FromInt(int(twoReplicas))),
+		},
+	}
+
+	return nodePool, nil
+}
+
+func (mc *NTOMachineConfigRolloutTest) Run(t *testing.T, nodePool hyperv1.NodePool, nodes []corev1.Node) {
+	g := NewWithT(t)
+
+	ctx := mc.ctx
+
+	tuningConfigConfigMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "hugepages-tuned-test",
+			Namespace: mc.hostedCluster.Namespace,
+		},
+		Data: map[string]string{tuningConfigKey: hugepagesTuned},
+	}
+	if err := mc.mgmtClient.Create(ctx, tuningConfigConfigMap); err != nil {
+		if !errors.IsAlreadyExists(err) {
+			t.Fatalf("failed to create configmap for custom Tuned object: %v", err)
+		}
+	}
+
+	np := nodePool.DeepCopy()
+	nodePool.Spec.TuningConfig = append(nodePool.Spec.TuningConfig, corev1.LocalObjectReference{Name: tuningConfigConfigMap.Name})
+	if err := mc.mgmtClient.Patch(ctx, &nodePool, crclient.MergeFrom(np)); err != nil {
+		t.Fatalf("failed to update nodepool %s after adding Tuned config: %v", nodePool.Name, err)
+	}
+
+	// DS Customization
+	ds := ntoMachineConfigUpdatedVerificationDS.DeepCopy()
+	dsName := nodePool.Name
+	e2eutil.CorrelateDaemonSet(ds, &nodePool, dsName)
+
+	if err := mc.hostedClusterClient.Create(ctx, ds); err != nil {
+		if !errors.IsAlreadyExists(err) {
+			t.Fatalf("failed to create %s DaemonSet in guestcluster: %v", ds.Name, err)
+		}
+	}
+
+	t.Logf("waiting for rollout of NodePools with NTO-generated config")
+	err := wait.PollImmediateWithContext(ctx, 10*time.Second, 15*time.Minute, func(ctx context.Context) (bool, error) {
+		if ctx.Err() != nil {
+			return false, ctx.Err()
+		}
+		pods := &corev1.PodList{}
+		if err := mc.hostedClusterClient.List(ctx, pods, crclient.InNamespace(ds.Namespace), crclient.MatchingLabels(ds.Spec.Selector.MatchLabels)); err != nil {
+			t.Logf("WARNING: failed to list pods, will retry: %v", err)
+			return false, nil
+		}
+
+		if len(pods.Items) != len(nodes) {
+			return false, nil
+		}
+
+		for _, pod := range pods.Items {
+			if !isPodReady(&pod) {
+				return false, nil
+			}
+		}
+
+		return true, nil
+	})
+
+	g.Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed waiting for all pods in the NTO MachineConfig update verification DS to be ready: %v", err))
+	g.Expect(nodePool.Status.Replicas).To(BeEquivalentTo(len(nodes)))
+	e2eutil.EnsureNoCrashingPods(t, ctx, mc.mgmtClient, mc.hostedCluster)
+	e2eutil.EnsureAllContainersHavePullPolicyIfNotPresent(t, ctx, mc.mgmtClient, mc.hostedCluster)
+	e2eutil.EnsureHCPContainersHaveResourceRequests(t, ctx, mc.mgmtClient, mc.hostedCluster)
+	e2eutil.EnsureNoPodsWithTooHighPriority(t, ctx, mc.mgmtClient, mc.hostedCluster)
+
+}
+
+type NTOMachineConfigInPlaceRolloutTestManifest struct {
+	hostedCluster *hyperv1.HostedCluster
+}
+
+func NewNTOMachineConfigInPlaceRolloutTestManifest(hostedCluster *hyperv1.HostedCluster) *NTOMachineConfigInPlaceRolloutTestManifest {
+	return &NTOMachineConfigInPlaceRolloutTestManifest{
+		hostedCluster: hostedCluster,
+	}
+}
+
+func (mc *NTOMachineConfigInPlaceRolloutTestManifest) BuildNodePoolManifest(defaultNodepool hyperv1.NodePool) (*hyperv1.NodePool, error) {
+	nodePool := &hyperv1.NodePool{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      mc.hostedCluster.Name + "-" + "test-ntomachineconfig-inplace",
+			Namespace: mc.hostedCluster.Namespace,
+		},
+	}
+	defaultNodepool.Spec.DeepCopyInto(&nodePool.Spec)
+
+	nodePool.Spec.Replicas = &twoReplicas
+	nodePool.Spec.Management.UpgradeType = hyperv1.UpgradeTypeInPlace
+
+	return nodePool, nil
 }
 
 //go:embed nodepool_nto_machineconfig_verification_ds.yaml
