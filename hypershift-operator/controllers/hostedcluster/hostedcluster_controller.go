ff7240bc778ba4c2e697c10dd0773981d45eff6a
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2023-05-23 10:09:03 +0200

    OCPBUGS-13034: Fixed revoking some permissions to CAPI Manager Clusterrole
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
+++ hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
@@ -2711,11 +2711,6 @@ func reconcileCAPIManagerClusterRole(role *rbacv1.ClusterRole) error {
 			Resources: []string{"customresourcedefinitions"},
 			Verbs:     []string{"get", "list", "watch"},
 		},
-		{
-			APIGroups: []string{""},
-			Resources: []string{"events"},
-			Verbs:     []string{"update", "create", "patch"},
-		},
 	}
 	return nil
 }
