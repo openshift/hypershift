6faad14c9772bf91f52bd0ab9b9b1d96f1bcb5f7
Bryan Cox <brcox@redhat.com>
2023-07-26 12:37:03 -0400

    Add HCP CLI Command to Create a NodePool on AWS
    Signed-off-by: Bryan Cox <brcox@redhat.com>

--- product-cli/cmd/nodepool/create.go
+++ product-cli/cmd/nodepool/create.go
@@ -5,13 +5,10 @@ import (
 
 	hyperv1 "github.com/openshift/hypershift/api/v1beta1"
 	"github.com/openshift/hypershift/cmd/nodepool/core"
-	hyperShiftKubeVirt "github.com/openshift/hypershift/cmd/nodepool/kubevirt"
 	"github.com/openshift/hypershift/product-cli/cmd/nodepool/agent"
+	"github.com/openshift/hypershift/product-cli/cmd/nodepool/aws"
 	"github.com/openshift/hypershift/product-cli/cmd/nodepool/kubevirt"
 )
-
-// The following lines are needed in order to validate that any platform implementing PlatformOptions satisfy the interface
-var _ core.PlatformOptions = &hyperShiftKubeVirt.KubevirtPlatformCreateOptions{}
 
 func NewCreateCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -21,26 +18,25 @@ func NewCreateCommand() *cobra.Command {
 	}
 
 	opts := &core.CreateNodePoolOptions{
-		Namespace:       "clusters",
 		ClusterName:     "example",
+		Namespace:       "clusters",
 		NodeCount:       2,
-		ReleaseImage:    "",
 		NodeUpgradeType: hyperv1.UpgradeTypeReplace,
-		Arch:            "amd64",
+		ReleaseImage:    "",
 	}
 
-	cmd.PersistentFlags().StringVar(&opts.Name, "name", opts.Name, "The name of the NodePool")
-	cmd.PersistentFlags().StringVar(&opts.Namespace, "namespace", opts.Namespace, "The namespace in which to create the NodePool")
-	cmd.PersistentFlags().Int32Var(&opts.NodeCount, "node-count", opts.NodeCount, "The number of nodes to create in the NodePool")
-	cmd.PersistentFlags().StringVar(&opts.ClusterName, "cluster-name", opts.ClusterName, "The name of the HostedCluster nodes in this pool will join")
-	cmd.PersistentFlags().StringVar(&opts.ReleaseImage, "release-image", opts.ReleaseImage, "The release image for nodes. If empty, defaults to the same release image as the HostedCluster.")
+	cmd.PersistentFlags().StringVar(&opts.Name, "name", opts.Name, "The name of the NodePool.")
+	cmd.PersistentFlags().StringVar(&opts.Namespace, "namespace", opts.Namespace, "The namespace in which to create the NodePool.")
+	cmd.PersistentFlags().Int32Var(&opts.NodeCount, "node-count", opts.NodeCount, "The number of nodes to create in the NodePool.")
+	cmd.PersistentFlags().StringVar(&opts.ClusterName, "cluster-name", opts.ClusterName, "The name of the HostedCluster nodes in this pool will join.")
+	cmd.PersistentFlags().StringVar(&opts.ReleaseImage, "release-image", opts.ReleaseImage, "The release image for nodes; if this is empty, defaults to the same release image as the HostedCluster.")
 	cmd.PersistentFlags().Var(&opts.NodeUpgradeType, "node-upgrade-type", "The NodePool upgrade strategy for how nodes should behave when upgraded. Supported options: Replace, InPlace")
-	cmd.PersistentFlags().StringVar(&opts.Arch, "arch", opts.Arch, "The processor architecture for the NodePool (e.g. arm64, amd64)")
-	cmd.PersistentFlags().BoolVar(&opts.Render, "render", false, "Render output as YAML to stdout instead of applying")
+	cmd.PersistentFlags().BoolVar(&opts.Render, "render", false, "Render output as YAML to stdout instead of applying.")
 
 	_ = cmd.MarkPersistentFlagRequired("name")
 
 	cmd.AddCommand(agent.NewCreateCommand(opts))
+	cmd.AddCommand(aws.NewCreateCommand(opts))
 	cmd.AddCommand(kubevirt.NewCreateCommand(opts))
 
 	return cmd
