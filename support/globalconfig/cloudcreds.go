74749d66efc9f6e68083450079d0dd469cd13111
Cesar Wong <cewong@redhat.com>
2023-08-22 16:27:47 -0400

    Reconcile cloud credentials configuration to hosted cluster
    The console catalog page is now querying for the cloudcredentials
    configuration resource in the OperatorHub page. Because the resource
    does not exist in HyperShift clusters, the page displays 404. This
    change fixes the issue by reconciling a simple cloud credentials
    operator configuration into the guest cluster.

---
+++ support/globalconfig/cloudcreds.go
@@ -0,0 +1,24 @@
+package globalconfig
+
+import (
+	operatorv1 "github.com/openshift/api/operator/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func CloudCredentialsConfiguration() *operatorv1.CloudCredential {
+	return &operatorv1.CloudCredential{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster",
+		},
+	}
+}
+
+func ReconcileCloudCredentialsConfiguration(cfg *operatorv1.CloudCredential) error {
+	// Always use default mode for cloud credentials
+	cfg.Spec.CredentialsMode = operatorv1.CloudCredentialsModeDefault
+
+	// Because we don't run the CCO, setting the management state to unmanaged.
+	// This should change if/when we run the CCO on the control plane side.
+	cfg.Spec.ManagementState = operatorv1.Unmanaged
+	return nil
+}
