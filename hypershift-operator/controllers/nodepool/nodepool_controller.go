db236312a7a4ea546a1d2a3112d1e8ff27205112
Bryan Cox <brcox@redhat.com>
2023-08-24 15:02:32 -0400

    OCPBUGS-11939: Use img metadata in from ICSP/IDMS
    OCPBUGS-11939: Use the image metadata from any ICSP/IDMS in a management
     cluster
    
    Signed-off-by: Bryan Cox <brcox@redhat.com>

--- hypershift-operator/controllers/nodepool/nodepool_controller.go
+++ hypershift-operator/controllers/nodepool/nodepool_controller.go
@@ -2603,7 +2603,7 @@ func (r *NodePoolReconciler) detectCPOCapabilities(ctx context.Context, hostedCl
 		return nil, fmt.Errorf("failed to get controlPlaneOperatorImage: %w", err)
 	}
 
-	controlPlaneOperatorImageMetadata, err := r.ImageMetadataProvider.ImageMetadata(ctx, controlPlaneOperatorImage, pullSecretBytes, hostedCluster.Spec.ImageContentSources)
+	controlPlaneOperatorImageMetadata, err := r.ImageMetadataProvider.ImageMetadata(ctx, controlPlaneOperatorImage, pullSecretBytes)
 	if err != nil {
 		return nil, fmt.Errorf("failed to look up image metadata for %s: %w", controlPlaneOperatorImage, err)
 	}
