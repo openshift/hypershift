42204d106d87afad078265dd56342e1e4d0530b5
CrystalChun <cchun@redhat.com>
2023-09-18 12:02:04 -0700

    OCPBUGS-19516: Update dependencies for new Agent CAPI APIs
    https://issues.redhat.com//browse/OCPBUGS-19516
    Original issue: https://issues.redhat.com//browse/MGMT-14838

--- vendor/github.com/openshift/api/config/v1/types_feature.go
+++ vendor/github.com/openshift/api/config/v1/types_feature.go
@@ -163,21 +163,26 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 		Disabled: []FeatureGateDescription{},
 	},
 	TechPreviewNoUpgrade: newDefaultFeatures().
+		without(validatingAdmissionPolicy).
 		with(externalCloudProvider).
-		with(externalCloudProviderAzure).
 		with(externalCloudProviderGCP).
 		with(csiDriverSharedResource).
-		with(buildCSIVolumes).
 		with(nodeSwap).
 		with(machineAPIProviderOpenStack).
 		with(insightsConfigAPI).
-		with(matchLabelKeysInPodTopologySpread).
 		with(retroactiveDefaultStorageClass).
-		with(pdbUnhealthyPodEvictionPolicy).
 		with(dynamicResourceAllocation).
 		with(admissionWebhookMatchConditions).
-		with(azureWorkloadIdentity).
 		with(gateGatewayAPI).
+		with(maxUnavailableStatefulSet).
+		without(eventedPleg).
+		with(sigstoreImageVerification).
+		with(gcpLabelsTags).
+		with(vSphereStaticIPs).
+		with(routeExternalCertificate).
+		with(automatedEtcdBackup).
+		without(machineAPIOperatorDisableMachineHealthCheckController).
+		with(adminNetworkPolicy).
 		toFeatures(defaultFeatures),
 	LatencySensitive: newDefaultFeatures().
 		toFeatures(defaultFeatures),
@@ -186,6 +191,13 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 var defaultFeatures = &FeatureGateEnabledDisabled{
 	Enabled: []FeatureGateDescription{
 		openShiftPodSecurityAdmission,
+		alibabaPlatform, // This is a bug, it should be TechPreviewNoUpgrade. This must be downgraded before 4.14 is shipped.
+		azureWorkloadIdentity,
+		cloudDualStackNodeIPs,
+		externalCloudProviderAzure,
+		externalCloudProviderExternal,
+		privateHostedZoneAWS,
+		buildCSIVolumes,
 	},
 	Disabled: []FeatureGateDescription{
 		retroactiveDefaultStorageClass,
