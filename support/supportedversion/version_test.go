619d6451d0087ebe8c34e206c514ddc8fa23d0dd
Cesar Wong <cewong@redhat.com>
2024-02-05 16:26:41 -0500

    Set a fixed minimum supported version
    In order to support a set of known OCP versions, we are setting a fixed
    minimum supported version, instead of a number of previous versions.

--- support/supportedversion/version_test.go
+++ support/supportedversion/version_test.go
@@ -12,7 +12,7 @@ import (
 
 func TestSupportedVersions(t *testing.T) {
 	g := NewGomegaWithT(t)
-	g.Expect(Supported()).To(Equal([]string{"4.16", "4.15", "4.14"}))
+	g.Expect(Supported()).To(Equal([]string{"4.16", "4.15", "4.14", "4.13"}))
 }
 
 func TestIsValidReleaseVersion(t *testing.T) {
