de6628b752cb9cb67c9752a4f08c687fb08b8776
Seth Jennings <sjenning@redhat.com>
2024-03-14 15:00:23 -0500

    disable preemption for hypershift PriorityClasses

--- cmd/install/assets/hypershift_operator.go
+++ cmd/install/assets/hypershift_operator.go
@@ -1181,9 +1181,10 @@ func HyperShiftControlPlanePriorityClass() *schedulingv1.PriorityClass {
 		ObjectMeta: metav1.ObjectMeta{
 			Name: DefaultPriorityClass,
 		},
-		Value:         100000000,
-		GlobalDefault: false,
-		Description:   "This priority class should be used for hypershift control plane pods not critical to serving the API.",
+		Value:            100000000,
+		GlobalDefault:    false,
+		Description:      "This priority class should be used for hypershift control plane pods not critical to serving the API.",
+		PreemptionPolicy: ptr.To(corev1.PreemptNever),
 	}
 }
 
@@ -1196,9 +1197,10 @@ func HyperShiftAPICriticalPriorityClass() *schedulingv1.PriorityClass {
 		ObjectMeta: metav1.ObjectMeta{
 			Name: APICriticalPriorityClass,
 		},
-		Value:         100001000,
-		GlobalDefault: false,
-		Description:   "This priority class should be used for hypershift control plane pods critical to serving the API.",
+		Value:            100001000,
+		GlobalDefault:    false,
+		Description:      "This priority class should be used for hypershift control plane pods critical to serving the API.",
+		PreemptionPolicy: ptr.To(corev1.PreemptNever),
 	}
 }
 
@@ -1211,9 +1213,10 @@ func HyperShiftEtcdPriorityClass() *schedulingv1.PriorityClass {
 		ObjectMeta: metav1.ObjectMeta{
 			Name: EtcdPriorityClass,
 		},
-		Value:         100002000,
-		GlobalDefault: false,
-		Description:   "This priority class should be used for hypershift etcd pods.",
+		Value:            100002000,
+		GlobalDefault:    false,
+		Description:      "This priority class should be used for hypershift etcd pods.",
+		PreemptionPolicy: ptr.To(corev1.PreemptNever),
 	}
 }
 
