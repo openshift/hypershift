a330a3f00979db1b6854b589fa6504e6186b48fe
Feilian Xie <fxie@redhat.com>
2024-04-07 00:51:04 +0800

    Allow PublicAndPrivate or Private clusters to be created with LB KAS. Update guest cluster components to use the correct port to communicate with KAS..
    Signed-off-by: Feilian Xie <fxie@redhat.com>

---
+++ support/util/expose_test.go
@@ -0,0 +1,92 @@
+package util
+
+import (
+	"testing"
+
+	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
+)
+
+func TestIsLBKASByHC(t *testing.T) {
+	tests := []struct {
+		description string
+		hc          *hyperv1.HostedCluster
+		expected    bool
+	}{
+		{
+			description: "hc.spec.services is an empty array",
+			hc: &hyperv1.HostedCluster{
+				Spec: hyperv1.HostedClusterSpec{
+					Services: []hyperv1.ServicePublishingStrategyMapping{},
+				},
+			},
+			expected: false,
+		},
+		{
+			description: "hc.spec.services does not contain an entry for KAS",
+			hc: &hyperv1.HostedCluster{
+				Spec: hyperv1.HostedClusterSpec{
+					Services: []hyperv1.ServicePublishingStrategyMapping{
+						{
+							Service: hyperv1.OAuthServer,
+							ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{
+								Type: hyperv1.Route,
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			description: "hc.spec.services contains an LB KAS entry",
+			hc: &hyperv1.HostedCluster{
+				Spec: hyperv1.HostedClusterSpec{
+					Services: []hyperv1.ServicePublishingStrategyMapping{
+						{
+							Service: hyperv1.OAuthServer,
+							ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{
+								Type: hyperv1.Route,
+							},
+						},
+						{
+							Service: hyperv1.APIServer,
+							ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{
+								Type: hyperv1.LoadBalancer,
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			description: "hc.spec.services contains a Route KAS entry",
+			hc: &hyperv1.HostedCluster{
+				Spec: hyperv1.HostedClusterSpec{
+					Services: []hyperv1.ServicePublishingStrategyMapping{
+						{
+							Service: hyperv1.OAuthServer,
+							ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{
+								Type: hyperv1.Route,
+							},
+						},
+						{
+							Service: hyperv1.APIServer,
+							ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{
+								Type: hyperv1.Route,
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			if res := IsLBKASByHC(test.hc); res != test.expected {
+				t.Errorf("IsLBKASByHC() = %v, expected %v", res, test.expected)
+			}
+		})
+	}
+}
