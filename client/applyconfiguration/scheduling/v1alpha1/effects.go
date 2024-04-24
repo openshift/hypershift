a8e9e9b6bdd6425689bc000ea7c3e44b5ab18fb4
Cesar Wong <cewong@redhat.com>
2024-04-24 14:20:51 -0400

    Allow specifying resource requests per cluster size
    Allows memory and CPU requests to be specified by deployment/container
    per size.

--- client/applyconfiguration/scheduling/v1alpha1/effects.go
+++ client/applyconfiguration/scheduling/v1alpha1/effects.go
@@ -24,25 +24,17 @@ import (
 // EffectsApplyConfiguration represents an declarative configuration of the Effects type for use
 // with apply.
 type EffectsApplyConfiguration struct {
-	KASMemoryRequest              *resource.Quantity `json:"kasMemoryRequest,omitempty"`
-	KASGoMemLimit                 *resource.Quantity `json:"kasGoMemLimit,omitempty"`
-	ControlPlanePriorityClassName *string            `json:"controlPlanePriorityClassName,omitempty"`
-	EtcdPriorityClassName         *string            `json:"etcdPriorityClassName,omitempty"`
-	APICriticalPriorityClassName  *string            `json:"APICriticalPriorityClassName,omitempty"`
+	KASGoMemLimit                 *resource.Quantity                  `json:"kasGoMemLimit,omitempty"`
+	ControlPlanePriorityClassName *string                             `json:"controlPlanePriorityClassName,omitempty"`
+	EtcdPriorityClassName         *string                             `json:"etcdPriorityClassName,omitempty"`
+	APICriticalPriorityClassName  *string                             `json:"APICriticalPriorityClassName,omitempty"`
+	ResourceRequests              []ResourceRequestApplyConfiguration `json:"resourceRequests,omitempty"`
 }
 
 // EffectsApplyConfiguration constructs an declarative configuration of the Effects type for use with
 // apply.
 func Effects() *EffectsApplyConfiguration {
 	return &EffectsApplyConfiguration{}
-}
-
-// WithKASMemoryRequest sets the KASMemoryRequest field in the declarative configuration to the given value
-// and returns the receiver, so that objects can be built by chaining "With" function invocations.
-// If called multiple times, the KASMemoryRequest field is set to the value of the last call.
-func (b *EffectsApplyConfiguration) WithKASMemoryRequest(value resource.Quantity) *EffectsApplyConfiguration {
-	b.KASMemoryRequest = &value
-	return b
 }
 
 // WithKASGoMemLimit sets the KASGoMemLimit field in the declarative configuration to the given value
@@ -74,5 +66,18 @@ func (b *EffectsApplyConfiguration) WithEtcdPriorityClassName(value string) *Eff
 // If called multiple times, the APICriticalPriorityClassName field is set to the value of the last call.
 func (b *EffectsApplyConfiguration) WithAPICriticalPriorityClassName(value string) *EffectsApplyConfiguration {
 	b.APICriticalPriorityClassName = &value
+	return b
+}
+
+// WithResourceRequests adds the given value to the ResourceRequests field in the declarative configuration
+// and returns the receiver, so that objects can be build by chaining "With" function invocations.
+// If called multiple times, values provided by each call will be appended to the ResourceRequests field.
+func (b *EffectsApplyConfiguration) WithResourceRequests(values ...*ResourceRequestApplyConfiguration) *EffectsApplyConfiguration {
+	for i := range values {
+		if values[i] == nil {
+			panic("nil value passed to WithResourceRequests")
+		}
+		b.ResourceRequests = append(b.ResourceRequests, *values[i])
+	}
 	return b
 }
