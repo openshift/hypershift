e0b8c0fbccda5ed8c211231f2b73447f8f687200
Steve Kuznetsov <skuznets@redhat.com>
2024-06-11 06:33:53 -0600

    hack: remove old arguments and scripts
    Consumers now pass arguments directly in the job, so there's no need to
    have them here.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/ci-test-e2e.sh
+++ hack/ci-test-e2e.sh
@@ -31,46 +31,6 @@ generate_junit() {
 }
 trap generate_junit EXIT
 
-REQUEST_SERVING_COMPONENT_TEST="${REQUEST_SERVING_COMPONENT_TEST:-}"
-REQUEST_SERVING_COMPONENT_PARAMS=""
-
-if [[ -n "${REQUEST_SERVING_COMPONENT_TEST}" ]]; then
-   REQUEST_SERVING_COMPONENT_PARAMS="--e2e.test-request-serving-isolation \
-  --e2e.management-parent-kubeconfig=${MGMT_PARENT_KUBECONFIG} \
-  --e2e.management-cluster-namespace=$(cat "${SHARED_DIR}/management_cluster_namespace") \
-  --e2e.management-cluster-name=$(cat "${SHARED_DIR}/management_cluster_name")"
-fi
-
-declare -a default_args=(
-  -test.v
-  -test.run=${CI_TESTS_RUN}
-  -test.parallel=20 \
-  --e2e.aws-credentials-file=/etc/hypershift-pool-aws-credentials/credentials \
-  --e2e.aws-zones=us-east-1a,us-east-1b,us-east-1c \
-  --e2e.aws-oidc-s3-bucket-name=hypershift-ci-oidc \
-  --e2e.aws-kms-key-alias=alias/hypershift-ci \
-  --e2e.pull-secret-file=/etc/ci-pull-credentials/.dockerconfigjson \
-  --e2e.base-domain=ci.hypershift.devcluster.openshift.com \
-  --e2e.latest-release-image="${OCP_IMAGE_LATEST}" \
-  --e2e.previous-release-image="${OCP_IMAGE_PREVIOUS}" \
-  --e2e.disable-pki-reconciliation="${DISABLE_PKI_RECONCILIATION:=false}" \
-  --e2e.additional-tags="expirationDate=$(date -d '4 hours' --iso=minutes --utc)" \
-  --e2e.aws-endpoint-access=PublicAndPrivate \
-  --e2e.external-dns-domain=service.ci.hypershift.devcluster.openshift.com \
-  ${REQUEST_SERVING_COMPONENT_PARAMS}
-)
-
-# We would like all end-to-end testing for HyperShift to use this script, so we can set flags centrally
-# and provide the jUnit results, etc, for everyone in the same way. In order to do that, we need to allow
-# each consumer to pass disjoint sets of flags to the end-to-end binary. We already accept one argument,
-# the set of tests to run, so we will continue to honor the previous calling convention unless the caller
-# is passing more flags. That allows us to default to the current behavior and let callers opt into the
-# new paradigm over time. Once that migration is done, default_args will be removed.
-declare -a args="$@"
-if [[ "$#" -lt 2 ]]; then
-  args="${default_args[@]}"
-fi
-
-bin/test-e2e ${args} | tee /tmp/test_out &
+bin/test-e2e "$@"| tee /tmp/test_out &
 
 wait $!
