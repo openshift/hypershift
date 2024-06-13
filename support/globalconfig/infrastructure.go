bfc4205db41113bd8238c4d755985693003e5494
Emilien Macchi <emacchi@redhat.com>
2024-06-13 16:10:18 -0400

    OSASINFRA-3309: populating OpenShift's Infrastructure CRD

--- support/globalconfig/infrastructure.go
+++ support/globalconfig/infrastructure.go
@@ -10,6 +10,7 @@ import (
 
 	configv1 "github.com/openshift/api/config/v1"
 	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
+	supportutil "github.com/openshift/hypershift/support/util"
 )
 
 func InfrastructureConfig() *configv1.Infrastructure {
@@ -86,7 +87,11 @@ func ReconcileInfrastructure(infra *configv1.Infrastructure, hcp *hyperv1.Hosted
 		infra.Spec.PlatformSpec.OpenStack = &configv1.OpenStackPlatformSpec{}
 		infra.Status.PlatformStatus.OpenStack = &configv1.OpenStackPlatformStatus{
 			CloudName:            "openstack",
-			APIServerInternalIPs: []string{}, // TODO(dulek): Strongly not sure how to do this.
+			APIServerInternalIPs: supportutil.IPsToStrings(infra.Spec.PlatformSpec.OpenStack.APIServerInternalIPs),
+			IngressIPs:           supportutil.IPsToStrings(infra.Spec.PlatformSpec.OpenStack.IngressIPs),
+			LoadBalancer:         &configv1.OpenStackPlatformLoadBalancer{Type: configv1.LoadBalancerTypeOpenShiftManagedDefault},
+			NodeDNSIP:            "", // TODO(emilien) not sure about this one
+			MachineNetworks:      infra.Spec.PlatformSpec.OpenStack.MachineNetworks,
 		}
 	}
 }
