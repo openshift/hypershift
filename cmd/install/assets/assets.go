26a3d61097ee61a93ac9b892c92042802a97405a
Maysa Macedo <maysa.macedo95@gmail.com>
2024-06-26 13:37:14 -0300

    Adds IPAM and openstackfloatingippools CR

--- cmd/install/assets/assets.go
+++ cmd/install/assets/assets.go
@@ -54,6 +54,7 @@ var capiResources = map[string]string{
 	"cluster-api-provider-openstack/infrastructure.cluster.x-k8s.io_openstackclusters.yaml":          "v1beta1",
 	"cluster-api-provider-openstack/infrastructure.cluster.x-k8s.io_openstackmachines.yaml":          "v1beta1",
 	"cluster-api-provider-openstack/infrastructure.cluster.x-k8s.io_openstackmachinetemplates.yaml":  "v1beta1",
+	"cluster-api-provider-openstack/infrastructure.cluster.x-k8s.io_openstackfloatingippools.yaml":   "v1alpha1",
 }
 
 func getContents(fs embed.FS, file string) []byte {
