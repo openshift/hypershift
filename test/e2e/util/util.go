452a613a4a9d2d9cd1cdcbd252e29f3eafb2152c
Oren Cohen <ocohen@redhat.com>
2024-07-03 19:09:06 +0300

    e2e tests: skip checking ValidKubeVirtInfraNetworkMTU if no workers
    In case there are no worker nodes in the HostedCluster, the condition ValidKubeVirtInfraNetworkMTU is missing since the MTU prober
    hasn't been run, so no MTU is being reported by the Cluster Network Operator. In such case, we're removing that condition from the
    expectedConditions map.
    
    Signed-off-by: Oren Cohen <ocohen@redhat.com>

--- test/e2e/util/util.go
+++ test/e2e/util/util.go
@@ -1278,6 +1278,7 @@ func validateHostedClusterConditions(t *testing.T, ctx context.Context, client c
 		expectedConditions[hyperv1.ClusterVersionAvailable] = metav1.ConditionFalse
 		expectedConditions[hyperv1.ClusterVersionSucceeding] = metav1.ConditionFalse
 		expectedConditions[hyperv1.ClusterVersionProgressing] = metav1.ConditionTrue
+		delete(expectedConditions, hyperv1.ValidKubeVirtInfraNetworkMTU)
 	}
 
 	var predicates []Predicate[*hyperv1.HostedCluster]
