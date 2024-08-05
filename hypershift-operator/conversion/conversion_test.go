e51264693b55bc3f0fd7cb8c01510e460939f7f3
Bryan Cox <brcox@redhat.com>
2024-08-05 09:30:34 -0400

    Add Managed Identity Support in Azure HC API
    Adds fields in the Azure HostedCluster API for the managed identities
    used for the following control plane components: azure cloud provider,
    KMS, CAPZ, the control plane operator, the image registry operator,
    cluster ingress operator, cluster network
    operator/cluster-network-config-controller, azure-disk-controller and
     azure-file-controller.
    
    This commit also adds fields adding information related to an Azure Key
    Vault on the management cluster.
    
    Signed-off-by: Bryan Cox <brcox@redhat.com>

--- hypershift-operator/conversion/conversion_test.go
+++ hypershift-operator/conversion/conversion_test.go
@@ -409,6 +409,12 @@ func fixHosterClusterAfterFuzz(in runtime.Object) {
 		hc.Spec.Platform.Azure = nil
 	}
 
+	// There was no formal support for Azure on alpha so we shouldn't fuzz
+	// the beta object and expect it to equal the alpha version.
+	if hc.Spec.SecretEncryption != nil && hc.Spec.SecretEncryption.KMS != nil && hc.Spec.SecretEncryption.KMS.Azure != nil {
+		hc.Spec.SecretEncryption.KMS.Azure = nil
+	}
+
 	hc.Status.PayloadArch = ""
 
 	if hc.Spec.Platform.AWS != nil {
