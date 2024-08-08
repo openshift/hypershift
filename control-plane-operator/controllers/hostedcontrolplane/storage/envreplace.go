d913b1acb7642d7919ffdbf0dc6bb6ee99881a1c
pstefans <pstefans@redhat.com>
2024-08-08 17:40:45 +0100

    OCPBUGS-38183: make use of azure-disk-driver-control-plane-image

--- control-plane-operator/controllers/hostedcontrolplane/storage/envreplace.go
+++ control-plane-operator/controllers/hostedcontrolplane/storage/envreplace.go
@@ -48,6 +48,8 @@ var (
 		"POWERVS_BLOCK_CSI_DRIVER_IMAGE":                  "powervs-block-csi-driver",
 		"HYPERSHIFT_IMAGE":                                "token-minter",
 		"AWS_EBS_DRIVER_CONTROL_PLANE_IMAGE":              "aws-ebs-csi-driver",
+		"AZURE_DISK_DRIVER_CONTROL_PLANE_IMAGE":           "azure-disk-csi-driver",
+		"AZURE_FILE_DRIVER_CONTROL_PLANE_IMAGE":           "azure-file-csi-driver",
 		"LIVENESS_PROBE_CONTROL_PLANE_IMAGE":              "csi-livenessprobe",
 		"TOOLS_IMAGE":                                     "tools",
 	}
