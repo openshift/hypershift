269c89e8a9e2bebf57d02a40d8d04a1f6b06d5b0
enxebre <alberto.garcial@hotmail.com>
2024-09-02 15:29:13 +0200

    Add unit test to validate mco binaries are extracted as expected

---
+++ ignition-server/controllers/local_ignitionprovider_test.go
@@ -0,0 +1,126 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestExtractMCOBinaries(t *testing.T) {
+	testCases := []struct {
+		name                  string
+		mcoOSReleaseVersion   string
+		cpoOSReleaseVersion   string
+		expectedBinaryVersion string
+		cacheFunc             regClient
+	}{
+		{
+			name:                  "When both MCO and CPO are on RHEL 8, it should extract the RHEL 8 binaries with no prefix",
+			mcoOSReleaseVersion:   "8.1",
+			cpoOSReleaseVersion:   "8.2",
+			expectedBinaryVersion: "rhel8",
+		},
+		{
+			name:                  "When both MCO is in RHEL 8 and CPO on RHEL 9, it should extract the RHEL 9 binaries with the .rhel9 prefix",
+			mcoOSReleaseVersion:   "8.1",
+			cpoOSReleaseVersion:   "9.1",
+			expectedBinaryVersion: "rhel9",
+		},
+		{
+			name:                  "When MCO is in too old version and CPO on RHEL 9, and the RHEL 9 binaries do not exist it should extract the RHEL 8 binaries with no prefix",
+			mcoOSReleaseVersion:   "8.0",
+			cpoOSReleaseVersion:   "9.1",
+			expectedBinaryVersion: "rhel8",
+			cacheFunc: func(ctx context.Context, imageRef string, pullSecret []byte, file string, out io.Writer) error {
+				switch file {
+				case "usr/lib/os-release":
+					_, err := out.Write([]byte(fmt.Sprintf("VERSION_ID=\"%s\"\n", "8.0")))
+					return err
+				case "usr/bin/machine-config-operator", "usr/bin/machine-config-controller", "usr/bin/machine-config-server":
+					_, err := out.Write([]byte("rhel8"))
+					return err
+				case "usr/bin/machine-config-operator.rhel9", "usr/bin/machine-config-controller.rhel9", "usr/bin/machine-config-server.rhel9":
+					return fmt.Errorf("file not found: %s", file)
+				default:
+					return fmt.Errorf("unexpected file: %s", file)
+				}
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+
+			tempDir, err := os.MkdirTemp("", "testExtractBinaries-*")
+			if err != nil {
+				t.Fatalf("Failed to create temporary directory: %v", err)
+			}
+			defer os.RemoveAll(tempDir)
+
+			// Set up the necessary variables for testing.
+			ctx := context.Background()
+			mcoImage := "fake"
+			pullSecret := []byte{}
+			binDir := filepath.Join(tempDir, "bin")
+			os.Mkdir(binDir, 0755)
+
+			// Create a fake file cache that returns the expected binaries.
+			imageFileCache := &imageFileCache{
+				cacheMap: make(map[cacheKey]cacheValue),
+				cacheDir: tempDir,
+			}
+			imageFileCache.regClient = func(ctx context.Context, imageRef string, pullSecret []byte, file string, out io.Writer) error {
+				switch file {
+				case "usr/lib/os-release":
+					_, err := out.Write([]byte(fmt.Sprintf("VERSION_ID=\"%s\"\n", tc.mcoOSReleaseVersion)))
+					return err
+				case "usr/bin/machine-config-operator", "usr/bin/machine-config-controller", "usr/bin/machine-config-server":
+					_, err := out.Write([]byte("rhel8"))
+					return err
+				case "usr/bin/machine-config-operator.rhel9", "usr/bin/machine-config-controller.rhel9", "usr/bin/machine-config-server.rhel9":
+					_, err := out.Write([]byte("rhel9"))
+					return err
+				default:
+					return fmt.Errorf("unexpected file: %s", file)
+				}
+			}
+
+			// If the test case has a custom cache function, use it.
+			// This is useful to simulate the case where the ocp release for the NodePool is too old that it doesn't have the RHEL binaries.
+			if tc.cacheFunc != nil {
+				imageFileCache.regClient = tc.cacheFunc
+			}
+
+			// Create a fake cpo os-release file
+			cpoOSRelease := fmt.Sprintf("VERSION_ID=\"%s\"\n", tc.cpoOSReleaseVersion)
+			cpoOSReleaseFilePath := filepath.Join(tempDir, "usr/lib/os-release")
+			err = os.MkdirAll(filepath.Dir(cpoOSReleaseFilePath), 0755)
+			g.Expect(err).NotTo(HaveOccurred())
+			err = os.WriteFile(cpoOSReleaseFilePath, []byte(cpoOSRelease), 0644)
+			g.Expect(err).NotTo(HaveOccurred())
+
+			// Create a LocalIgnitionProvider instance for testing.
+			provider := &LocalIgnitionProvider{
+				ImageFileCache: imageFileCache,
+				WorkDir:        tempDir,
+			}
+
+			// Call the extractMCOBinaries.
+			err = provider.extractMCOBinaries(ctx, cpoOSReleaseFilePath, mcoImage, pullSecret, binDir)
+			g.Expect(err).NotTo(HaveOccurred())
+
+			// Verify the extracted binaries match the expected version.
+			for _, name := range []string{"machine-config-operator", "machine-config-controller", "machine-config-server"} {
+				filePath := filepath.Join(binDir, name)
+				fileContent, err := os.ReadFile(filePath)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(string(fileContent)).To(Equal(tc.expectedBinaryVersion))
+			}
+		})
+	}
+}
