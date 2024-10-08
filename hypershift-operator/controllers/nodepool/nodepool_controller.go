3826dd621da50eaae54f164fbaab5725a9b9a3d6
enxebre <alberto.garcial@hotmail.com>
2024-10-08 11:26:58 +0200

    Let conditions getting the token secret to not shortcircuit when is not found
    Since this runs early now, shortcicuiting would never give the token secret the chance to be reconciled

--- hypershift-operator/controllers/nodepool/nodepool_controller.go
+++ hypershift-operator/controllers/nodepool/nodepool_controller.go
@@ -771,7 +771,6 @@ func (r NodePoolReconciler) createReachedIgnitionEndpointCondition(ctx context.C
 				Message:            err.Error(),
 				ObservedGeneration: generation,
 			}
-			return nil, fmt.Errorf("failed to get token secret: %w", err)
 		} else {
 			condition = &hyperv1.NodePoolCondition{
 				Type:               hyperv1.NodePoolReachedIgnitionEndpoint,
@@ -818,7 +817,6 @@ func (r NodePoolReconciler) createValidGeneratedPayloadCondition(ctx context.Con
 				Message:            err.Error(),
 				ObservedGeneration: generation,
 			}
-			return nil, fmt.Errorf("failed to get token secret: %w", err)
 		} else {
 			condition = &hyperv1.NodePoolCondition{
 				Type:               hyperv1.NodePoolValidGeneratedPayloadConditionType,
