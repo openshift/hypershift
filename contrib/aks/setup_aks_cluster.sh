178e4bf3b7a761d3c2331cfee59eccc7bddfc746
Bryan Cox <brcox@redhat.com>
2024-10-22 13:28:30 -0400

    Update AKS contrib docs
    Update the AKS contrib docs since Managed Identities was introduced.
    
    Signed-off-by: Bryan Cox <brcox@redhat.com>

--- contrib/aks/setup_aks_cluster.sh
+++ contrib/aks/setup_aks_cluster.sh
@@ -2,33 +2,63 @@
 set -x
 
 # Constants
-PREFIX="your_prefix"
-RG="${PREFIX}-aks-rg"
+PREFIX="<your-prefix-here>"
+AKS_RG=${PREFIX}"-aks-rg"
 LOCATION="eastus"
-AKS_CLUSTER_NAME="${PREFIX}-aks-cluster"
+AKS_CLUSTER_NAME=${PREFIX}"-aks-cluster"
+SUBSCRIPTION_ID="<your-subscription-id-here>"
+KV_NAME="<your-key-vault-name-here>"
+
+# I got this from searching my login name in Azure and pulling up my user profile
+OBJECT_ID="<your-object-id>"
 
 # Clear out existing Azure RG
-az group delete -n ${RG} --yes
+az group delete -n ${AKS_RG} --yes
 
 # Create Azure RG
 az group create \
---name ${RG} \
+--name ${AKS_RG} \
 --location ${LOCATION}
 
 # Create AKS Cluster
 az aks create \
---resource-group ${RG} \
+--resource-group ${AKS_RG} \
 --name ${AKS_CLUSTER_NAME} \
 --node-count 2 \
 --generate-ssh-keys \
 --load-balancer-sku standard \
 --os-sku AzureLinux \
 --node-vm-size Standard_D4s_v4 \
---enable-fips-image
+--enable-fips-image \
+--enable-addons azure-keyvault-secrets-provider
+
+# Create Management Azure Key Vault
+az keyvault create \
+--name ${KV_NAME} \
+--resource-group ${AKS_RG} \
+--location ${LOCATION} \
+--enable-rbac-authorization
+
+# Save the KV MI Info
+AZURE_KEY_VAULT_AUTHORIZED_USER_ID=$(az aks show -n ${AKS_CLUSTER_NAME} -g ${AKS_RG} | jq .addonProfiles.azureKeyvaultSecretsProvider.identity.clientId -r)
+AZURE_KEY_VAULT_AUTHORIZED_OBJECT_ID=$(az aks show -n ${AKS_CLUSTER_NAME} -g ${AKS_RG} | jq .addonProfiles.azureKeyvaultSecretsProvider.identity.objectId -r)
+
+# Give the MI the Key Vault Secrets role
+az role assignment create \
+--assignee-object-id "${AZURE_KEY_VAULT_AUTHORIZED_OBJECT_ID}" \
+--role "Key Vault Secrets User" \
+--scope /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/"${AKS_RG}" \
+--assignee-principal-type ServicePrincipal
+
+## Associate your SP with the Key Vault; this is so you can use your SP with the CLI to create certificates in the key vault
+az role assignment create \
+    --assignee ${OBJECT_ID} \
+    --scope /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/"${AKS_RG}"/providers/Microsoft.KeyVault/vaults/${KV_NAME} \
+    --role "Key Vault Administrator"
 
 # Get kubeconfig access
 az aks get-credentials \
---resource-group ${RG} \
+--resource-group ${AKS_RG} \
 --name ${AKS_CLUSTER_NAME} \
 --overwrite-existing
 
--- contrib/aks/setup_install_ho_on_aks.sh
+++ contrib/aks/setup_install_ho_on_aks.sh
@@ -3,8 +3,9 @@ set -x
 
 # Constants
 MGMT_DNS_ZONE_NAME="azure.blah.com"
-HYPERSHIFT_BINARY_PATH=<path>
-CUSTOM_IMAGE="quay.io/user/hypershift:mytag1"
+PULL_SECRET="/Users/your-username/all-the-pull-secrets.json"
+SERVICE_PRINCIPAL_FILEPATH="/Users/your-username/azure_mgmt.json"
+TAG="quay.io/user/hypershift:mytag1"
 
 ######################################## HyperShift Operator Install ########################################
 
@@ -14,16 +15,17 @@ oc apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-ope
 oc apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
 oc apply -f https://raw.githubusercontent.com/openshift/api/master/route/v1/zz_generated.crd-manifests/routes-Default.crd.yaml
 
-# Install HyperShift Operator (HO)
-# Include this flag in the command below if you want to run a custom HO image
-# --hypershift-image ${CUSTOM_IMAGE}
-
+# Install HO; leave off --hypershift-image line if you aren't testing with a particular HO image
+# tech-preview-no-upgrade and aro-hcp-key-vault-users-client-id is needed for managed identity
 ${HYPERSHIFT_BINARY_PATH}/hypershift install \
 --enable-conversion-webhook=false \
 --external-dns-provider=azure \
---external-dns-credentials /Users/user/azure_mgmt.json \
---pull-secret /Users/user/all-the-pull-secrets.json \
+--external-dns-credentials ${SERVICE_PRINCIPAL_FILEPATH} \
+--pull-secret ${PULL_SECRET} \
 --external-dns-domain-filter ${MGMT_DNS_ZONE_NAME} \
---managed-service ARO-HCP
+--managed-service ARO-HCP \
+--aro-hcp-key-vault-users-client-id "${AZURE_KEY_VAULT_AUTHORIZED_USER_ID}" \
+--hypershift-image quay.io/username/hypershift:${TAG} \
+--tech-preview-no-upgrade
 
 set +x
