514b95ca1aaa5058ef0ff8db7b4579d0e565d7ef
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2024-12-12 12:20:25 +0100

    OCPBUGS-46342: Allow ARM64 arch deployment on Agent platform
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- vendor/github.com/openshift/hypershift/api/hypershift/v1beta1/nodepool_types.go
+++ vendor/github.com/openshift/hypershift/api/hypershift/v1beta1/nodepool_types.go
@@ -78,7 +78,7 @@ type NodePool struct {
 
 // NodePoolSpec is the desired behavior of a NodePool.
 // +kubebuilder:validation:XValidation:rule="!has(oldSelf.arch) || has(self.arch)", message="Arch is required once set"
-// +kubebuilder:validation:XValidation:rule="self.arch != 'arm64' || has(self.platform.aws)", message="Setting Arch to arm64 is only supported for AWS"
+// +kubebuilder:validation:XValidation:rule="self.arch != 'arm64' || has(self.platform.aws) || has(self.platform.azure) || has(self.platform.agent)", message="Setting Arch to arm64 is only supported for AWS, Azure and Agent"
 // +kubebuilder:validation:XValidation:rule="!has(self.replicas) || !has(self.autoScaling)", message="Both replicas or autoScaling should not be set"
 type NodePoolSpec struct {
 	// ClusterName is the name of the HostedCluster this NodePool belongs to.
