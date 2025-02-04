3508ee3ce387a91dda5f9e2ef3fa3848aecc8f7b
Bryan Cox <brcox@redhat.com>
2025-02-04 13:35:20 -0500

    Run `make verify`
    Signed-off-by: Bryan Cox <brcox@redhat.com>

--- vendor/github.com/openshift/hypershift/api/hypershift/v1beta1/azure.go
+++ vendor/github.com/openshift/hypershift/api/hypershift/v1beta1/azure.go
@@ -463,15 +463,17 @@ type AzureResourceManagedIdentities struct {
 // used, by an HCP component, to authenticate with the Azure API.
 type ManagedIdentity struct {
 	// clientID is the client ID of a managed identity.
+	// Deprecated: This field was previously required as part of the MIWI phase 2 work; however, this field will be
+	// removed as part of the MIWI phase 3 work, https://issues.redhat.com/browse/OCPSTRAT-1856.
 	//
 	// +kubebuilder:validation:XValidation:rule="self.matches('^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$')",message="the client ID of a managed identity must be a valid UUID. It should be 5 groups of hyphen separated hexadecimal characters in the form 8-4-4-4-12."
-	// +kubebuilder:validation:Required
 	ClientID string `json:"clientID"`
 
 	// certificateName is the name of the certificate backing the managed identity. This certificate is expected to
 	// reside in an Azure Key Vault on the management cluster.
+	// Deprecated: This field was previously required as part of the MIWI phase 2 work; however, this field will be
+	// removed as part of the MIWI phase 3 work, https://issues.redhat.com/browse/OCPSTRAT-1856.
 	//
-	// +kubebuilder:validation:Required
 	CertificateName string `json:"certificateName"`
 
 	// objectEncoding represents the encoding for the Azure Key Vault secret containing the certificate related to
@@ -487,6 +489,20 @@ type ManagedIdentity struct {
 	// +kubebuilder:validation:Enum:=utf-8;hex;base64
 	// +kubebuilder:default:="utf-8"
 	ObjectEncoding ObjectEncodingFormat `json:"objectEncoding"`
+
+	// credentialsSecretName is the name of an Azure Key Vault secret. This field assumes the secret contains the JSON
+	// format of a UserAssignedIdentityCredentials struct. At a minimum, the secret needs to contain the ClientId,
+	// ClientSecret, AuthenticationEndpoint, NotBefore, and NotAfter, and TenantId.
+	//
+	// More info on this struct can be found here - https://github.com/Azure/msi-dataplane/blob/63fb37d3a1aaac130120624674df795d2e088083/pkg/dataplane/internal/generated_client.go#L156.
+	//
+	// credentialsSecretName must be between 1 and 127 characters and use only alphanumeric characters and hyphens.
+	// credentialsSecretName must also be unique within the Azure Key Vault. See more details here - https://azure.github.io/PSRule.Rules.Azure/en/rules/Azure.KeyVault.SecretName/.
+	//
+	// TODO set the validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=127
+	// TODO set validation:Pattern=`^[a-zA-Z0-9-]+$`
+	CredentialsSecretName string `json:"credentialsSecretName"`
 }
 
 // ControlPlaneManagedIdentities contains the managed identities on the HCP control plane needing to authenticate with
