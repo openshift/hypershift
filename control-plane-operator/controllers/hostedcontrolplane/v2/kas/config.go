96d572977a23d5d0feacb29f3c864881fa957cbc
Joseph Goergen <joseph.goergen@ibm.com>
2025-03-07 13:03:51 -0600

    KAS: Bump audit-webhook-initial-backoff and TerminationGracePeriodSeconds when audit webhook is enabled
    related: https://github.com/openshift/hypershift/pull/5475
    https://github.com/openshift/hypershift/pull/5491

--- control-plane-operator/controllers/hostedcontrolplane/v2/kas/config.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/kas/config.go
@@ -165,6 +165,7 @@ func generateConfig(p KubeAPIServerConfigParams) *kcpv1.KubeAPIServerConfig {
 	if p.AuditWebhookEnabled {
 		args.Set("audit-webhook-config-file", auditWebhookConfigFile())
 		args.Set("audit-webhook-mode", "batch")
+		args.Set("audit-webhook-initial-backoff", "5s")
 	}
 	if p.DisableProfiling {
 		args.Set("profiling", "false")
