e19907a025b2c8a27e617f7a7398daafbea5845a
Bryan Cox <brcox@redhat.com>
2025-03-07 15:15:02 -0500

    Bump msi-dataplane dependency
    This commit bumps the msi-dataplane dependency to the latest version,
    which contains updated error handling around filewatcher.

--- vendor/github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/azsecrets/build.go
+++ vendor/github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/azsecrets/build.go
@@ -1,11 +1,9 @@
-//go:build go1.18
-// +build go1.18
+//go:generate tsp-client update
+//go:generate go run ./internal/transforms.go
+//go:generate rm ./constants.go
+//go:generate goimports -w .
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-package internal
-
-const (
-	version = "v1.1.1" //nolint
-)
+package azsecrets
