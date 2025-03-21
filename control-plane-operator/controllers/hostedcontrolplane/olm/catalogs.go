cbacec8d3b62248d7f80644e446ddeb81f4b349b
Cesar Wong <cewong@redhat.com>
2025-03-21 16:22:01 -0400

    Check for availability of individual catalog images
    This commit refactors the code that fetches the images to use for OLM
    catalogs into a common support package. It checks individual catalog
    images instead of assuming that all of them have the same tag available.
    It also introduces a cache for images so that every reconcile loop does
    not result in registry lookups.

--- control-plane-operator/controllers/hostedcontrolplane/olm/catalogs.go
+++ control-plane-operator/controllers/hostedcontrolplane/olm/catalogs.go
@@ -1,7 +1,6 @@
 package olm
 
 import (
-	"context"
 	"fmt"
 	"math/big"
 	"strings"
@@ -21,7 +20,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/utils/ptr"
 
-	"github.com/blang/semver"
 	prometheusoperatorv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 )
 
@@ -120,67 +118,6 @@ func findTagReference(tags []imagev1.TagReference, name string) *imagev1.TagRefe
 		}
 	}
 	return nil
-}
-
-func GetCatalogImages(ctx context.Context, hcp hyperv1.HostedControlPlane, pullSecret []byte, imageMetadataProvider util.ImageMetadataProvider, registryOverrides map[string][]string) (map[string]string, error) {
-	imageRef := hcp.Spec.ReleaseImage
-	imageConfig, _, _, err := imageMetadataProvider.GetMetadata(ctx, imageRef, pullSecret)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get image metadata: %w", err)
-	}
-
-	version, err := semver.Parse(imageConfig.Config.Labels["io.openshift.release"])
-	if err != nil {
-		return nil, fmt.Errorf("invalid OpenShift release version format: %s", imageConfig.Config.Labels["io.openshift.release"])
-	}
-
-	registries := []string{
-		"registry.redhat.io/redhat",
-	}
-	if len(registryOverrides) > 0 {
-		for registrySource, registryDest := range registryOverrides {
-			if registries[0] == registrySource {
-				registries = registryDest
-				break
-			}
-		}
-	}
-
-	//check catalogs of last 4 supported version in case new version is not available
-	supportedVersions := 4
-	imageRegistry := ""
-	for i := 0; i < supportedVersions; i++ {
-		for _, registry := range registries {
-			testImage := fmt.Sprintf("%s/certified-operator-index:v%d.%d", registry, version.Major, version.Minor)
-
-			_, dockerImage, err := imageMetadataProvider.GetDigest(ctx, testImage, pullSecret)
-			if err == nil {
-				imageRegistry = fmt.Sprintf("%s/%s", dockerImage.Registry, dockerImage.Namespace)
-				break
-			}
-
-			// Manifest unknown error is expected if the image is not available.
-			if !strings.Contains(err.Error(), "manifest unknown") {
-				return nil, err // Return if it's an unexpected error
-			}
-		}
-		if imageRegistry != "" {
-			break
-		}
-		if i == supportedVersions-1 {
-			return nil, fmt.Errorf("failed to get image digest for 4 previous versions of certified-operator-index: %w", err)
-		}
-		version.Minor--
-	}
-
-	operators := map[string]string{
-		"certified-operators": fmt.Sprintf("%s/certified-operator-index:v%d.%d", imageRegistry, version.Major, version.Minor),
-		"community-operators": fmt.Sprintf("%s/community-operator-index:v%d.%d", imageRegistry, version.Major, version.Minor),
-		"redhat-marketplace":  fmt.Sprintf("%s/redhat-marketplace-index:v%d.%d", imageRegistry, version.Major, version.Minor),
-		"redhat-operators":    fmt.Sprintf("%s/redhat-operator-index:v%d.%d", imageRegistry, version.Major, version.Minor),
-	}
-
-	return operators, nil
 }
 
 func ReconcileCatalogsImageStream(imageStream *imagev1.ImageStream, ownerRef config.OwnerRef, catalogImages map[string]string) error {
