97d2eaf3fdeff189a5632f6dd078f290e014c38c
Cesar Wong <cewong@redhat.com>
2025-03-31 13:18:16 -0400

    Use a CPO image label to determine whether to run expired cert remediation
    Adds a commented label to the CPO image that should be uncommented whenever we
    switch to using CPO v2 as the default. Uses that label (and annotation) to determine
    whether we should remediate expired certs from the hypershift operator.
    
    We were previously using the HC version, assuming that 4.19 would default to CPOv2,
    but that won't be the case.

--- hypershift-operator/controllers/hostedcluster/hostedcluster_controller_test.go
+++ hypershift-operator/controllers/hostedcluster/hostedcluster_controller_test.go
@@ -57,7 +57,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	"github.com/blang/semver"
 	"github.com/docker/distribution"
 	"github.com/docker/distribution/manifest/manifestlist"
 	"github.com/go-logr/logr"
@@ -740,46 +739,47 @@ func TestShouldCheckForStaleCerts(t *testing.T) {
 	tests := []struct {
 		name           string
 		hcAnnotations  map[string]string
-		hcVersion      string
+		cpov2label     bool
 		expectedResult bool
 	}{
 		{
-			name: "version older than 4.19",
+			name: "cpo without cpov2label",
 			hcAnnotations: map[string]string{
 				hcmetrics.HasBeenAvailableAnnotation: "true",
 			},
-			hcVersion:      "4.18.7",
+			cpov2label:     false,
 			expectedResult: true,
 		},
 		{
-			name: "ci version with 4.19",
+			name: "cpo with cpov2label",
 			hcAnnotations: map[string]string{
 				hcmetrics.HasBeenAvailableAnnotation: "true",
 			},
-			hcVersion:      "4.19.0-0.ci-2025-02-03-120046",
+			cpov2label:     true,
 			expectedResult: false,
 		},
 		{
-			name: "4.20",
+			name: "cpo without cpov2label, but has annotation for cpov2",
 			hcAnnotations: map[string]string{
-				hcmetrics.HasBeenAvailableAnnotation: "true",
+				hcmetrics.HasBeenAvailableAnnotation:     "true",
+				hyperv1.ControlPlaneOperatorV2Annotation: "true",
 			},
-			hcVersion:      "4.20.0",
+			cpov2label:     false,
 			expectedResult: false,
 		},
 		{
-			name:           "version older than 4.19, never been available",
+			name:           "has not been available",
 			hcAnnotations:  nil,
-			hcVersion:      "4.17.4",
+			cpov2label:     false,
 			expectedResult: false,
 		},
 		{
-			name: "version older than 4.19, has been available, does not reconcile pki",
+			name: "has been available, does not reconcile pki",
 			hcAnnotations: map[string]string{
 				hcmetrics.HasBeenAvailableAnnotation:       "true",
 				hyperv1.DisablePKIReconciliationAnnotation: "true",
 			},
-			hcVersion:      "4.16.20",
+			cpov2label:     false,
 			expectedResult: false,
 		},
 	}
@@ -788,7 +788,7 @@ func TestShouldCheckForStaleCerts(t *testing.T) {
 			g := NewGomegaWithT(t)
 			hc := &hyperv1.HostedCluster{}
 			hc.Annotations = test.hcAnnotations
-			fn := shouldCheckForStaleCerts(hc, semver.MustParse(test.hcVersion))
+			fn := shouldCheckForStaleCerts(hc, test.cpov2label)
 			result := fn()
 			g.Expect(result).To(Equal(test.expectedResult))
 		})
