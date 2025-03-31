f6f556c66a3fba9c9a5aa2c937b139b6319d36d3
Evan Reilly <ereilly@Evans-MBP.lan>
2025-03-31 15:23:24 -0500

    Address review comments

--- control-plane-operator/controllers/hostedcontrolplane/registryoperator/reconcile.go
+++ control-plane-operator/controllers/hostedcontrolplane/registryoperator/reconcile.go
@@ -143,7 +143,7 @@ func NewParams(hcp *hyperv1.HostedControlPlane, version string, releaseImageProv
 				containerWebIdentityTokenMinter().Name: {
 					Requests: corev1.ResourceList{
 						corev1.ResourceCPU:    resource.MustParse("10m"),
-						corev1.ResourceMemory: resource.MustParse("10Mi"),
+						corev1.ResourceMemory: resource.MustParse("30Mi"),
 					},
 				},
 			},
