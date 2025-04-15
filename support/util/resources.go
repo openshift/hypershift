e9bce5793bcc7a6abc26c5e5c7b6e44117821fa8
Bryan Cox <brcox@redhat.com>
2025-04-15 09:17:34 -0400

    Limit watch resources in HC by platforms installed
    This commit limits the resources to watch by the HostedCluster based on
    the platforms that are installed by the HO.

---
+++ support/util/resources.go
@@ -0,0 +1,98 @@
+package util
+
+import (
+	"strings"
+
+	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
+
+	agentv1 "github.com/openshift/cluster-api-provider-agent/api/v1beta1"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+
+	capiaws "sigs.k8s.io/cluster-api-provider-aws/v2/api/v1beta2"
+	capiazure "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
+	capiibmv1 "sigs.k8s.io/cluster-api-provider-ibmcloud/api/v1beta2"
+	capikubevirt "sigs.k8s.io/cluster-api-provider-kubevirt/api/v1alpha1"
+	capiopenstackv1beta1 "sigs.k8s.io/cluster-api-provider-openstack/api/v1beta1"
+	capiv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	secretsstorev1 "sigs.k8s.io/secrets-store-csi-driver/apis/v1"
+)
+
+var (
+	BaseResources = []client.Object{
+		&capiv1.Cluster{},
+	}
+
+	AWSResources = []client.Object{
+		&capiaws.AWSCluster{},
+		&hyperv1.AWSEndpointService{},
+	}
+
+	AzureResources = []client.Object{
+		&capiazure.AzureCluster{},
+		&capiazure.AzureClusterIdentity{},
+	}
+
+	ManagedAzure = []client.Object{
+		&secretsstorev1.SecretProviderClass{},
+	}
+
+	IBMCloudResources = []client.Object{
+		&capiibmv1.IBMVPCCluster{},
+	}
+
+	KubevirtResources = []client.Object{
+		&capikubevirt.KubevirtCluster{},
+	}
+
+	AgentResources = []client.Object{
+		&agentv1.AgentCluster{},
+	}
+
+	OpenStackResources = []client.Object{
+		&capiopenstackv1beta1.OpenStackCluster{},
+	}
+)
+
+func GetHostedClusterManagedResources(platformsInstalled string) []client.Object {
+	var managedResources []client.Object
+
+	platformsInstalledList := sets.New[string]()
+	for _, p := range strings.Split(platformsInstalled, ",") {
+		p = strings.TrimSpace(p)
+		if p != "" {
+			platformsInstalledList.Insert(p)
+		}
+	}
+
+	if platformsInstalledList.Len() == 0 {
+		return managedResources
+	}
+
+	if platformsInstalledList.Len() == 1 && strings.EqualFold(platformsInstalledList.UnsortedList()[0], string(hyperv1.NonePlatform)) {
+		return managedResources
+	}
+
+	// All platforms have the same base resources
+	managedResources = append(managedResources, BaseResources...)
+
+	for _, platform := range platformsInstalledList.UnsortedList() {
+		switch {
+		case strings.EqualFold(platform, string(hyperv1.AWSPlatform)):
+			managedResources = append(managedResources, AWSResources...)
+		case strings.EqualFold(platform, string(hyperv1.AzurePlatform)):
+			managedResources = append(managedResources, AzureResources...)
+		case strings.EqualFold(platform, string(hyperv1.IBMCloudPlatform)):
+			managedResources = append(managedResources, IBMCloudResources...)
+		case strings.EqualFold(platform, string(hyperv1.KubevirtPlatform)):
+			managedResources = append(managedResources, KubevirtResources...)
+		case strings.EqualFold(platform, string(hyperv1.AgentPlatform)):
+			managedResources = append(managedResources, AgentResources...)
+		case strings.EqualFold(platform, string(hyperv1.OpenStackPlatform)):
+			managedResources = append(managedResources, OpenStackResources...)
+		}
+	}
+
+	return managedResources
+}
