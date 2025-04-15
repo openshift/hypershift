530ca96d518b913527c7f8128235d64c867df45e
Seth Jennings <sjenning@redhat.com>
2025-04-15 12:12:47 -0500

    switch admissionregistration API to v1

--- control-plane-operator/hostedclusterconfigoperator/controllers/resources/manifests/admissionpolicies.go
+++ control-plane-operator/hostedclusterconfigoperator/controllers/resources/manifests/admissionpolicies.go
@@ -1,22 +1,22 @@
 package manifests
 
 import (
-	k8sadmissionv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	k8sadmissionv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func ValidatingAdmissionPolicy(name string) *k8sadmissionv1beta1.ValidatingAdmissionPolicy {
-	return &k8sadmissionv1beta1.ValidatingAdmissionPolicy{
+func ValidatingAdmissionPolicy(name string) *k8sadmissionv1.ValidatingAdmissionPolicy {
+	return &k8sadmissionv1.ValidatingAdmissionPolicy{
 		TypeMeta: metav1.TypeMeta{
-			APIVersion: k8sadmissionv1beta1.SchemeGroupVersion.String(),
+			APIVersion: k8sadmissionv1.SchemeGroupVersion.String(),
 			Kind:       "ValidatingAdmissionPolicy",
 		},
 		ObjectMeta: metav1.ObjectMeta{Name: name},
 	}
 }
 
-func ValidatingAdmissionPolicyBinding(bindingName string) *k8sadmissionv1beta1.ValidatingAdmissionPolicyBinding {
-	return &k8sadmissionv1beta1.ValidatingAdmissionPolicyBinding{
+func ValidatingAdmissionPolicyBinding(bindingName string) *k8sadmissionv1.ValidatingAdmissionPolicyBinding {
+	return &k8sadmissionv1.ValidatingAdmissionPolicyBinding{
 		ObjectMeta: metav1.ObjectMeta{Name: bindingName},
 	}
 }
