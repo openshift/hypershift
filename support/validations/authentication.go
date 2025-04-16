58b20aa83e54d8b7e351d95101ac98461f5dcc28
Bryce Palmer <bpalmer@redhat.com>
2025-04-16 08:52:15 -0400

    update structured auth validation to use minimum supported kube version for CEL compilation validation
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- support/validations/authentication.go
+++ support/validations/authentication.go
@@ -6,8 +6,11 @@ import (
 
 	configv1 "github.com/openshift/api/config/v1"
 	"github.com/openshift/hypershift/control-plane-operator/controllers/hostedcontrolplane/kas"
+	"github.com/openshift/hypershift/support/supportedversion"
+	"k8s.io/apimachinery/pkg/util/version"
 	"k8s.io/apiserver/pkg/apis/apiserver/validation"
 	"k8s.io/apiserver/pkg/authentication/cel"
+	"k8s.io/apiserver/pkg/cel/environment"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -42,9 +45,24 @@ func ValidateAuthenticationSpecForTypeOIDC(ctx context.Context, client crclient.
 		return fmt.Errorf("generating structured authentication configuration: %w", err)
 	}
 
-    // TODO: using the default compiler means that we are allowing CEL library usage for the Kube version that maps to our
-    // dependency import. This should align with the version of Kubernetes that will be running for a guest cluster instead
-    // since that is what will actually load and validate the configuration.
-    fieldErrors := validation.ValidateAuthenticationConfiguration(cel.NewDefaultCompiler() , authConfig, disallowIssuers)
-    return fieldErrors.ToAggregate()
+	// TODO: implement logic for getting the current/desired version for the control plane and get the corresponding ube version based on that.
+	// For now, always use the minimum supported OCP version to ensure we are never getting false positives when validating CEL expression compiliation.
+	// Older versions of Kubernetes are not guaranteed to have the same CEL libraries available as newer ones.
+	// Always using the minimum supported OCP version will likely result in false negatives and the workaround is for users to adapt their CEL expressions
+	// accordingly.
+	// The current line of thinking is that false negatives are better than false positives because false positives could result in invalid configurations
+	// attempting to be rolled out.
+	kubeVersion, err := supportedversion.GetKubeVersionForSupportedVersion(supportedversion.MinSupportedVersion)
+	if err != nil {
+		return fmt.Errorf("getting the corresponding kubernetes version for OCP version %q", supportedversion.MinSupportedVersion.String())
+	}
+
+	envVersion, err := version.Parse(kubeVersion.String())
+	if err != nil {
+		return fmt.Errorf("parsing kubernetes version %q", kubeVersion.String())
+	}
+	celCompiler := cel.NewCompiler(environment.MustBaseEnvSet(envVersion, true))
+
+	fieldErrors := validation.ValidateAuthenticationConfiguration(celCompiler, authConfig, disallowIssuers)
+	return fieldErrors.ToAggregate()
 }
