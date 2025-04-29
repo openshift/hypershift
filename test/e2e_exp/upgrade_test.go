4616aa40c1aa2c0b2b52ac5a9a8df9ea55631208
Ahmed Abdalla <aabdelre@redhat.com>
2025-04-29 10:04:42 +0200

    Add an experiment to run e2e tests in batches

---
+++ test/e2e_exp/upgrade_test.go
@@ -0,0 +1,35 @@
+//go:build e2e_batch
+// +build e2e_batch
+
+package e2e_exp
+
+import (
+    "testing"
+    "math/rand"
+    "time"
+)
+
+func TestUpgrade(t *testing.T) {
+    t.Parallel()
+    t.Log("Running TestUpgrade")
+
+    tests := []struct {
+        name string
+        duration time.Duration
+    }{
+        {"ControlPlaneUpgrade", time.Duration(rand.Intn(5000)) * time.Millisecond},
+        {"WorkerUpgrade", time.Duration(rand.Intn(4000)) * time.Millisecond},
+        {"ValidatePostUpgrade", time.Duration(rand.Intn(2000)) * time.Millisecond},
+    }
+
+    for _, tt := range tests {
+        tt := tt
+        t.Run(tt.name, func(t *testing.T) {
+            t.Parallel()
+            time.Sleep(tt.duration)
+            if rand.Float64() < FailureProb() {
+                t.Fatalf("Random failure in upgrade test: %s", tt.name)
+            }
+        })
+    }
+}
