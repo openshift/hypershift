082b50f88ab5896ecac050b6cb7cb5de3ac5e442
Mulham Raee <mulham.raee@gmail.com>
2025-05-08 19:09:52 +0200

    Fix Job status returning empty reason and message
    This fixes the issue of the controller failing to update ControlPlaneComponent status condition on older versions of openshift/k8s management clusters where the job controller doesn't set the reason and message fields in the Job conditions.

--- support/controlplane-component/job.go
+++ support/controlplane-component/job.go
@@ -48,32 +48,42 @@ func (c *jobProvider) Replicas(object *batchv1.Job) *int32 {
 
 // IsAvailable implements WorkloadProvider.
 func (c *jobProvider) IsAvailable(job *batchv1.Job) (status metav1.ConditionStatus, reason string, message string) {
-	complete := util.FindJobCondition(job, batchv1.JobComplete)
-	if complete != nil {
-		return metav1.ConditionStatus(complete.Status), complete.Reason, complete.Message
-	}
-	failed := util.FindJobCondition(job, batchv1.JobFailed)
-	if failed != nil {
-		return metav1.ConditionFalse, failed.Reason, failed.Message
-	}
 	if job.Status.Active > 0 {
 		return metav1.ConditionTrue, "JobActive", "Job is still running"
 	}
-	return metav1.ConditionFalse, "Unknown", "Job status unknown"
+	return JobCompletionStatus(job)
 }
 
 // IsReady implements WorkloadProvider.
 func (c *jobProvider) IsReady(job *batchv1.Job) (status metav1.ConditionStatus, reason string, message string) {
-	complete := util.FindJobCondition(job, batchv1.JobComplete)
-	if complete != nil {
-		return metav1.ConditionStatus(complete.Status), complete.Reason, complete.Message
-	}
-	failed := util.FindJobCondition(job, batchv1.JobFailed)
-	if failed != nil {
-		return metav1.ConditionFalse, failed.Reason, failed.Message
-	}
 	if job.Status.Active > 0 {
 		return metav1.ConditionFalse, "JobActive", "Job is still running"
 	}
+	return JobCompletionStatus(job)
+}
+
+// JobCompletionStatus checks the status of a job and returns the appropriate condition status, reason, and message.
+// It checks if the job is complete or failed and returns the corresponding status.
+// If the job is neither complete nor failed, it returns unknown status.
+func JobCompletionStatus(job *batchv1.Job) (status metav1.ConditionStatus, reason string, message string) {
+	complete := util.FindJobCondition(job, batchv1.JobComplete)
+	if complete != nil && complete.Status == corev1.ConditionTrue {
+		return metav1.ConditionTrue, "JobComplete", "Job completed successfully"
+	}
+
+	failed := util.FindJobCondition(job, batchv1.JobFailed)
+	if failed != nil && failed.Status == corev1.ConditionTrue {
+		// If the job failed, we return false and the reason and message from the condition to provide more context.
+		// we set default values for the reason and message if not set to avoid errors as reason and message are required fields
+		// in the ControlPlaneComponent status conditions.
+		if failed.Reason == "" {
+			failed.Reason = "JobFailed"
+		}
+		if failed.Message == "" {
+			failed.Message = "Job failed"
+		}
+		return metav1.ConditionFalse, failed.Reason, failed.Message
+	}
+
 	return metav1.ConditionFalse, "Unknown", "Job status unknown"
 }
