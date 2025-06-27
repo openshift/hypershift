88deed62c57d045120336a516370c6ab128eb5de
enxebre <alberto.garcial@hotmail.com>
2025-06-27 13:32:57 +0200

    Run EnsureNetworkPolicies on Azure

--- test/e2e/util/util.go
+++ test/e2e/util/util.go
@@ -86,6 +86,8 @@ var expectedKasManagementComponents = []string{
 	"cloud-controller-manager",
 	"olm-collect-profiles",
 	"aws-ebs-csi-driver-operator",
+	"azure-disk-csi-driver-operator",
+	"azure-file-csi-driver-operator",
 	"karpenter",
 	"karpenter-operator",
 	"featuregate-generator",
@@ -967,7 +969,7 @@ func EnsureAllRoutesUseHCPRouter(t *testing.T, ctx context.Context, hostClient c
 
 func EnsureNetworkPolicies(t *testing.T, ctx context.Context, c crclient.Client, hostedCluster *hyperv1.HostedCluster) {
 	t.Run("EnsureNetworkPolicies", func(t *testing.T) {
-		if hostedCluster.Spec.Platform.Type != hyperv1.AWSPlatform {
+		if hostedCluster.Spec.Platform.Type != hyperv1.AWSPlatform && hostedCluster.Spec.Platform.Type != hyperv1.AzurePlatform {
 			t.Skipf("test only supported on AWS platform, saw %s", hostedCluster.Spec.Platform.Type)
 		}
 
