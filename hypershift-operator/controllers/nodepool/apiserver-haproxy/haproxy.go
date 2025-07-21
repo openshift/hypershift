9a9745a9ab4b3149e8f71ff6bf690a2f08c03766
Mulham Raee <mulham.raee@gmail.com>
2025-07-21 14:28:03 +0200

    feat(shared-ingress): Add reloader sidecar to avoid restarts on config changes.
    This commit adds a new reloader sidecar container to the shared-ingress deployment that watches changes to the config and reloads the main HAproxy container to avoid a restart and downtime.

--- hypershift-operator/controllers/nodepool/apiserver-haproxy/haproxy.go
+++ hypershift-operator/controllers/nodepool/apiserver-haproxy/haproxy.go
@@ -256,7 +256,7 @@ func apiServerProxyConfig(haProxyImage, cpoImage, clusterID,
 
 	if sharedingress.UseSharedIngress() {
 		// proxy protocol v2 with TLV support (custom proxy protocol header) requires haproxy v2.9+, see: https://www.haproxy.com/blog/announcing-haproxy-2-9#proxy-protocol-tlv-fields
-		haProxyImage = "quay.io/redhat-user-workloads/crt-redhat-acm-tenant/hypershift-shared-ingress-main@sha256:d443537f72ec48b2078d24de9852c3369c68c60c06ac82d51c472b2144d41309"
+		haProxyImage = "quay.io/redhat-user-workloads/crt-redhat-acm-tenant/hypershift-shared-ingress-main@sha256:e03b49dd7dd2f28c67726b31ca140ce2145fb17e4bd305b74f98ac704567c8e5"
 	}
 
 	filesToAdd := []fileToAdd{
