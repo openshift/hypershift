664c68f7fe80a340dbb243a375363de64a657dfc
Bryan Cox <brcox@redhat.com>
2025-07-23 11:47:13 -0400

    refactor(azure): extract AKS managed identity setup into separate script
    - Move managed identity creation from setup_aks_cluster.sh to setup_aks_mi.sh
    - Add setup_aks_mi.sh to first-time setup flow in setup_all.sh
    - Improves modularity and reusability of Azure identity setup

--- contrib/managed-azure/setup_all.sh
+++ contrib/managed-azure/setup_all.sh
@@ -21,6 +21,7 @@ if [[ "$1" == "--first-time" ]]; then
   "${SCRIPT_DIR}/setup_MIv3_kv.sh"
   "${SCRIPT_DIR}/setup_oidc_provider.sh"
   "${SCRIPT_DIR}/setup_dataplane_identities.sh"
+  "${SCRIPT_DIR}/setup_aks_mi.sh"
 fi
 
 # Per-cluster setup scripts
