98f1cc44c13d9d3a8fcfb7152b5bdf373574c46d
Gergo Huszty <gergo.huszty@ibm.com>
2025-07-28 22:59:23 +0200

    fetch existing ignition service in adapter
    In case of IBM Cloud, nodeport must be preserved.

--- control-plane-operator/controllers/hostedcontrolplane/v2/ignitionserver/service.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/ignitionserver/service.go
@@ -8,11 +8,27 @@ import (
 	"github.com/openshift/hypershift/support/util"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func adaptService(cpContext component.WorkloadContext, svc *corev1.Service) error {
 	if cpContext.HCP.Spec.Platform.Type != hyperv1.IBMCloudPlatform {
 		return nil
+	}
+
+	existingServiceUsesNodePort := false
+	existingService := &corev1.Service{}
+	if err := cpContext.Client.Get(cpContext.Context, client.ObjectKeyFromObject(svc), existingService); err != nil {
+		if !apierrors.IsNotFound(err) {
+			return fmt.Errorf("failed to get existing ignition service: %w", err)
+		}
+	} else {
+		if len(existingService.Spec.Ports) != 1 {
+			return fmt.Errorf("existing ignition service must have exactly one port exposed, this has: %d", len(existingService.Spec.Ports))
+		}
+		existingServiceUsesNodePort = existingService.Spec.Type == corev1.ServiceTypeNodePort
 	}
 
 	strategy := util.ServicePublishingStrategyByTypeForHCP(cpContext.HCP, hyperv1.Ignition)
@@ -27,9 +43,13 @@ func adaptService(cpContext component.WorkloadContext, svc *corev1.Service) erro
 			svc.Spec.Ports[0].NodePort = strategy.NodePort.Port
 		}
 	case hyperv1.Route:
-		if (cpContext.HCP.Spec.Platform.Type != hyperv1.IBMCloudPlatform) ||
-			(cpContext.HCP.Spec.Platform.Type == hyperv1.IBMCloudPlatform && svc.Spec.Type != corev1.ServiceTypeNodePort) {
-			svc.Spec.Type = corev1.ServiceTypeClusterIP
+		if existingServiceUsesNodePort {
+			svc.Spec.Type = corev1.ServiceTypeNodePort
+			if strategy.NodePort != nil {
+				svc.Spec.Ports[0].NodePort = strategy.NodePort.Port
+			} else {
+				svc.Spec.Ports[0].NodePort = existingService.Spec.Ports[0].NodePort
+			}
 		}
 	default:
 		return fmt.Errorf("invalid publishing strategy for Ignition service: %s", strategy.Type)
--- control-plane-operator/controllers/hostedcontrolplane/v2/ignitionserver/service_test.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/ignitionserver/service_test.go
@@ -6,53 +6,53 @@ import (
 
 	. "github.com/onsi/gomega"
 
-	"github.com/openshift/hypershift/api/hypershift/v1beta1"
+	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
+	"github.com/openshift/hypershift/control-plane-operator/hostedclusterconfigoperator/api"
 	component "github.com/openshift/hypershift/support/controlplane-component"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 func TestIgnitionServiceReconcile(t *testing.T) {
-	// Define common inputs
-
 	testCases := []struct {
-		name     string
-		platform v1beta1.PlatformType
-		services []v1beta1.ServicePublishingStrategyMapping
-		svc_in   corev1.Service
-		svc_out  corev1.Service
-		err      error
+		name         string
+		platform     hyperv1.PlatformType
+		services     []hyperv1.ServicePublishingStrategyMapping
+		svc_existing *corev1.Service
+		svc_out      corev1.Service
+		err          error
 	}{
 		{
 			name:     "IBM Cloud, NodePort strategy, NodePort service, expected to fill port number from strategy",
-			platform: v1beta1.IBMCloudPlatform,
-			services: []v1beta1.ServicePublishingStrategyMapping{
+			platform: hyperv1.IBMCloudPlatform,
+			services: []hyperv1.ServicePublishingStrategyMapping{
 				{
-					Service: v1beta1.Ignition,
-					ServicePublishingStrategy: v1beta1.ServicePublishingStrategy{
-						Type:     v1beta1.NodePort,
-						NodePort: &v1beta1.NodePortPublishingStrategy{Port: 1125},
+					Service: hyperv1.Ignition,
+					ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{
+						Type:     hyperv1.NodePort,
+						NodePort: &hyperv1.NodePortPublishingStrategy{Port: 1125},
 					},
 				},
 			},
-			svc_in: corev1.Service{Spec: corev1.ServiceSpec{
-				Type: corev1.ServiceTypeNodePort,
-				Ports: []corev1.ServicePort{
-					{
-						Protocol:   corev1.ProtocolTCP,
-						Port:       6443,
-						TargetPort: intstr.IntOrString{IntVal: 6443},
+			svc_existing: &corev1.Service{
+				Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeNodePort,
+					Ports: []corev1.ServicePort{
+						{},
 					},
 				},
-			}},
+			},
 			svc_out: corev1.Service{Spec: corev1.ServiceSpec{
 				Type: corev1.ServiceTypeNodePort,
 				Ports: []corev1.ServicePort{
 					{
+						Name:       "https",
 						Protocol:   corev1.ProtocolTCP,
-						Port:       6443,
-						TargetPort: intstr.IntOrString{IntVal: 6443},
+						Port:       443,
+						TargetPort: intstr.IntOrString{IntVal: 9090},
 						NodePort:   1125,
 					},
 				},
@@ -61,30 +61,30 @@ func TestIgnitionServiceReconcile(t *testing.T) {
 		},
 		{
 			name:     "IBM Cloud, Route strategy, NodePort service with existing port number, expected not to change",
-			platform: v1beta1.IBMCloudPlatform,
-			services: []v1beta1.ServicePublishingStrategyMapping{
+			platform: hyperv1.IBMCloudPlatform,
+			services: []hyperv1.ServicePublishingStrategyMapping{
 				{
-					Service:                   v1beta1.Ignition,
-					ServicePublishingStrategy: v1beta1.ServicePublishingStrategy{Type: v1beta1.Route},
+					Service:                   hyperv1.Ignition,
+					ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{Type: hyperv1.Route},
 				}},
-			svc_in: corev1.Service{Spec: corev1.ServiceSpec{
-				Type: corev1.ServiceTypeNodePort,
-				Ports: []corev1.ServicePort{
-					{
-						Protocol:   corev1.ProtocolTCP,
-						Port:       6443,
-						TargetPort: intstr.IntOrString{IntVal: 6443},
-						NodePort:   1125,
+			svc_existing: &corev1.Service{
+				Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeNodePort,
+					Ports: []corev1.ServicePort{
+						{
+							NodePort: 1125,
+						},
 					},
 				},
-			}},
+			},
 			svc_out: corev1.Service{Spec: corev1.ServiceSpec{
 				Type: corev1.ServiceTypeNodePort,
 				Ports: []corev1.ServicePort{
 					{
+						Name:       "https",
 						Protocol:   corev1.ProtocolTCP,
-						Port:       6443,
-						TargetPort: intstr.IntOrString{IntVal: 6443},
+						Port:       443,
+						TargetPort: intstr.IntOrString{IntVal: 9090},
 						NodePort:   1125,
 					},
 				},
@@ -93,29 +93,20 @@ func TestIgnitionServiceReconcile(t *testing.T) {
 		},
 		{
 			name:     "Non-IBM Cloud, Route strategy, ClusterIP service, expected to not change",
-			platform: v1beta1.AWSPlatform,
-			services: []v1beta1.ServicePublishingStrategyMapping{
+			platform: hyperv1.AWSPlatform,
+			services: []hyperv1.ServicePublishingStrategyMapping{
 				{
-					Service:                   v1beta1.Ignition,
-					ServicePublishingStrategy: v1beta1.ServicePublishingStrategy{Type: v1beta1.Route},
+					Service:                   hyperv1.Ignition,
+					ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{Type: hyperv1.Route},
 				}},
-			svc_in: corev1.Service{Spec: corev1.ServiceSpec{
-				Type: corev1.ServiceTypeClusterIP,
-				Ports: []corev1.ServicePort{
-					{
-						Protocol:   corev1.ProtocolTCP,
-						Port:       6443,
-						TargetPort: intstr.IntOrString{IntVal: 6443},
-					},
-				},
-			}},
 			svc_out: corev1.Service{Spec: corev1.ServiceSpec{
 				Type: corev1.ServiceTypeClusterIP,
 				Ports: []corev1.ServicePort{
 					{
+						Name:       "https",
 						Protocol:   corev1.ProtocolTCP,
-						Port:       6443,
-						TargetPort: intstr.IntOrString{IntVal: 6443},
+						Port:       443,
+						TargetPort: intstr.IntOrString{IntVal: 9090},
 					},
 				},
 			}},
@@ -123,38 +114,60 @@ func TestIgnitionServiceReconcile(t *testing.T) {
 		},
 		{
 			name:     "IBM Cloud, Invalid strategy",
-			platform: v1beta1.IBMCloudPlatform,
-			services: []v1beta1.ServicePublishingStrategyMapping{
+			platform: hyperv1.IBMCloudPlatform,
+			services: []hyperv1.ServicePublishingStrategyMapping{
 				{
-					Service:                   v1beta1.Ignition,
-					ServicePublishingStrategy: v1beta1.ServicePublishingStrategy{Type: v1beta1.LoadBalancer},
+					Service:                   hyperv1.Ignition,
+					ServicePublishingStrategy: hyperv1.ServicePublishingStrategy{Type: hyperv1.LoadBalancer},
 				}},
 			err: fmt.Errorf("invalid publishing strategy for Ignition service: LoadBalancer"),
 		},
 		{
 			name:     "IBM Cloud, strategy missing",
-			platform: v1beta1.IBMCloudPlatform,
-			services: []v1beta1.ServicePublishingStrategyMapping{},
+			platform: hyperv1.IBMCloudPlatform,
+			services: []hyperv1.ServicePublishingStrategyMapping{},
 			err:      fmt.Errorf("ignition service strategy not specified"),
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+
+			clientBuilder := fake.NewClientBuilder().WithScheme(api.Scheme)
+			if tc.svc_existing != nil {
+				clientBuilder = clientBuilder.WithObjects(tc.svc_existing)
+			}
+			client := clientBuilder.Build()
+
 			ctx := component.WorkloadContext{
-				HCP: &v1beta1.HostedControlPlane{
-					Spec: v1beta1.HostedControlPlaneSpec{
-						Platform: v1beta1.PlatformSpec{Type: tc.platform},
+				HCP: &hyperv1.HostedControlPlane{
+					Spec: hyperv1.HostedControlPlaneSpec{
+						Platform: hyperv1.PlatformSpec{Type: tc.platform},
 						Services: tc.services,
 					},
 				},
+				Client: client,
 			}
-			err := adaptService(ctx, &tc.svc_in)
+			svc_out := corev1.Service{
+				Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP,
+					Ports: []corev1.ServicePort{
+						{
+							Name:       "https",
+							Protocol:   corev1.ProtocolTCP,
+							Port:       443,
+							TargetPort: intstr.IntOrString{IntVal: 9090},
+						},
+					},
+					Selector: map[string]string{"app": "ignition-server"},
+				}}
+
+			err := adaptService(ctx, &svc_out)
 
 			g := NewWithT(t)
 			if tc.err == nil {
 				g.Expect(err).To(BeNil())
-				g.Expect(tc.svc_in.Spec.Type).To(Equal(tc.svc_out.Spec.Type))
-				g.Expect(tc.svc_in.Spec.Ports).To(Equal(tc.svc_out.Spec.Ports))
+				g.Expect(svc_out.Spec.Type).To(Equal(tc.svc_out.Spec.Type))
+				g.Expect(svc_out.Spec.Ports).To(Equal(tc.svc_out.Spec.Ports))
 			} else {
 				g.Expect(tc.err.Error()).To(Equal(err.Error()))
 			}
