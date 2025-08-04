5ddb58a9cb459dea41840b90e7e0a4368d314f37
Seth Jennings <sjenning@redhat.com>
2025-08-04 09:48:41 -0500

    fix(OCPBUGS-60064): add missing app label to some components
    HCM is looking to track per-component metrics with the app label but
    it is missing on a few HCP components.
    
    The PR adds the app label to these components.

--- test/e2e/create_cluster_test.go
+++ test/e2e/create_cluster_test.go
@@ -1712,6 +1712,7 @@ func TestCreateCluster(t *testing.T) {
 		e2eutil.EnsureAPIUX(t, ctx, mgtClient, hostedCluster)
 		e2eutil.EnsureCustomLabels(t, ctx, mgtClient, hostedCluster)
 		e2eutil.EnsureCustomTolerations(t, ctx, mgtClient, hostedCluster)
+		e2eutil.EnsureAppLabel(t, ctx, mgtClient, hostedCluster)
 
 		// ensure KAS DNS name is configured with a KAS Serving cert
 		e2eutil.EnsureKubeAPIDNSNameCustomCert(t, ctx, mgtClient, hostedCluster)
