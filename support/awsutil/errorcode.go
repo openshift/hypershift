c04df2ad7a2ea36fd851017d3b7cb4485086b05f
Mulham Raee <mulham.raee@gmail.com>
2025-08-07 17:48:37 +0200

    Support day2 tags changes for AWS default SecurityGroup
    Reconcile the AWS default security group tags day2 on HostedCluster .Spec.Platform.AWS.ResourceTags field changes. last applied tags are tracked through an annotation on the HCP resource.

--- support/awsutil/errorcode.go
+++ support/awsutil/errorcode.go
@@ -6,10 +6,21 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 )
 
+const (
+	AuthFailure           = "AuthFailure"
+	UnauthorizedOperation = "UnauthorizedOperation"
+)
+
 func AWSErrorCode(err error) string {
 	var awsErr awserr.Error
 	if errors.As(err, &awsErr) {
 		return awsErr.Code()
 	}
 	return "Unknown"
+}
+
+// IsPermissionsError returns true if on aws permission errors.
+func IsPermissionsError(err error) bool {
+	code := AWSErrorCode(err)
+	return code == AuthFailure || code == UnauthorizedOperation
 }
