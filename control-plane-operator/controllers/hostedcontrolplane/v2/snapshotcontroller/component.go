59c5143166cc4943eaafe115a7f0bc45698de8bb
Mulham Raee <mulham.raee@gmail.com>
2025-08-07 12:38:48 +0200

    feat: monitor operands rollout status for cno, storage and snapshotcontroller
    This commit adds custom operands monitoring functions for cno, storage and snapshotcontroller.
    This is a temporary solution until our contract is enforced on all operands.

--- control-plane-operator/controllers/hostedcontrolplane/v2/snapshotcontroller/component.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/snapshotcontroller/component.go
@@ -1,12 +1,17 @@
 package snapshotcontroller
 
 import (
+	"fmt"
+
 	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
 	oapiv2 "github.com/openshift/hypershift/control-plane-operator/controllers/hostedcontrolplane/v2/oapi"
 	component "github.com/openshift/hypershift/support/controlplane-component"
 	"github.com/openshift/hypershift/support/util"
 
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -44,6 +49,7 @@ func NewComponent() component.ControlPlaneComponent {
 				{Group: "operator.openshift.io", Version: "v1", Kind: "CSISnapshotController"},
 			},
 		}).
+		WithCustomOperandsRolloutCheckFunc(checkOperandsRolloutStatus).
 		Build()
 }
 
@@ -51,5 +57,28 @@ func isStorageAndCSIManaged(cpContext component.WorkloadContext) (bool, error) {
 	if cpContext.HCP.Spec.Platform.Type == hyperv1.IBMCloudPlatform || cpContext.HCP.Spec.Platform.Type == hyperv1.PowerVSPlatform {
 		return false, nil
 	}
+	return true, nil
+}
+
+func checkOperandsRolloutStatus(cpContext component.WorkloadContext) (bool, error) {
+	deployment := &appsv1.Deployment{}
+	if err := cpContext.Client.Get(cpContext, client.ObjectKey{Namespace: cpContext.HCP.Namespace, Name: "csi-snapshot-controller"}, deployment); err != nil {
+		return false, fmt.Errorf("failed to get deployment csi-snapshot-controller: %w", err)
+	}
+
+	expectedImage := cpContext.ReleaseImageProvider.GetImage("csi-snapshot-controller")
+	for _, container := range deployment.Spec.Template.Spec.Containers {
+		if container.Name == "snapshot-controller" {
+			if container.Image != expectedImage {
+				return false, fmt.Errorf("container %s in deployment %s is not using the expected image %s", "snapshot-controller", "csi-snapshot-controller", expectedImage)
+			}
+			break
+		}
+	}
+
+	if !util.IsDeploymentReady(cpContext, deployment) {
+		return false, fmt.Errorf("deployment csi-snapshot-controller is not ready")
+	}
+
 	return true, nil
 }
