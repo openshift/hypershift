0e953172691e781930427011b0266b290631226e
red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>
2025-08-24 20:57:42 +0000

    chore(deps): update module github.com/mgechev/revive to v1.11.0
    Signed-off-by: red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>

--- hack/tools/vendor/github.com/mgechev/revive/config/config.go
+++ hack/tools/vendor/github.com/mgechev/revive/config/config.go
@@ -83,6 +83,7 @@ var allRules = append([]lint.Rule{
 	&rule.UselessBreak{},
 	&rule.UncheckedTypeAssertionRule{},
 	&rule.TimeEqualRule{},
+	&rule.TimeDateRule{},
 	&rule.BannedCharsRule{},
 	&rule.OptimizeOperandsOrderRule{},
 	&rule.UseAnyRule{},
@@ -101,6 +102,9 @@ var allRules = append([]lint.Rule{
 	&rule.RedundantBuildTagRule{},
 	&rule.UseErrorsNewRule{},
 	&rule.RedundantTestMainExitRule{},
+	&rule.UnnecessaryFormatRule{},
+	&rule.UseFmtPrintRule{},
+	&rule.EnforceSwitchStyleRule{},
 }, defaultRules...)
 
 // allFormatters is a list of all available formatters to output the linting results.
@@ -125,7 +129,7 @@ func getFormatters() map[string]lint.Formatter {
 	return result
 }
 
-// GetLintingRules yields the linting rules that must be applied by the linter
+// GetLintingRules yields the linting rules that must be applied by the linter.
 func GetLintingRules(config *lint.Config, extraRules []lint.Rule) ([]lint.Rule, error) {
 	rulesMap := map[string]lint.Rule{}
 	for _, r := range allRules {
@@ -227,7 +231,7 @@ func normalizeConfig(config *lint.Config) {
 
 const defaultConfidence = 0.8
 
-// GetConfig yields the configuration
+// GetConfig yields the configuration.
 func GetConfig(configPath string) (*lint.Config, error) {
 	config := &lint.Config{}
 	switch {
@@ -246,7 +250,7 @@ func GetConfig(configPath string) (*lint.Config, error) {
 	return config, nil
 }
 
-// GetFormatter yields the formatter for lint failures
+// GetFormatter yields the formatter for lint failures.
 func GetFormatter(formatterName string) (lint.Formatter, error) {
 	formatters := getFormatters()
 	if formatterName == "" {
