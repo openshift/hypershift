6d038d3b737fdf36330780de4ea9f48f32d26c0f
Max Cao <macao@redhat.com>
2025-08-28 12:16:34 -0700

    chore(autonode): fix spelling of ProvisionerKarpenter constant
    Signed-off-by: Max Cao <macao@redhat.com>

--- cmd/cluster/aws/create.go
+++ cmd/cluster/aws/create.go
@@ -273,7 +273,7 @@ func (o *CreateOptions) ApplyPlatformSpecifics(cluster *hyperv1.HostedCluster) e
 	if o.AutoNode {
 		cluster.Spec.AutoNode = &hyperv1.AutoNode{
 			Provisioner: &hyperv1.ProvisionerConfig{
-				Name: hyperv1.ProvisionerKarpeneter,
+				Name: hyperv1.ProvisionerKarpenter,
 				Karpenter: &hyperv1.KarpenterConfig{
 					Platform: hyperv1.AWSPlatform,
 					AWS: &hyperv1.KarpenterAWSConfig{
