a708609ccc70f985418311ec21b82c986af95aed
Enrique Llorente <ellorent@redhat.com>
2025-10-09 13:16:37 +0200

    chore(client): Generate applyconfiguration for IngressOperatorSpec
    This commit includes auto-generated client applyconfiguration code
    for the new IngressOperatorSpec type and updates to the
    OperatorConfiguration applyconfiguration.
    
    These files are generated by applyconfiguration-gen tooling.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- client/applyconfiguration/utils.go
+++ client/applyconfiguration/utils.go
@@ -183,6 +183,8 @@ func ForKind(kind schema.GroupVersionKind) interface{} {
 		return &hypershiftv1beta1.IBMCloudPlatformSpecApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("ImageContentSource"):
 		return &hypershiftv1beta1.ImageContentSourceApplyConfiguration{}
+	case v1beta1.SchemeGroupVersion.WithKind("IngressOperatorSpec"):
+		return &hypershiftv1beta1.IngressOperatorSpecApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("InPlaceUpgrade"):
 		return &hypershiftv1beta1.InPlaceUpgradeApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("KarpenterAWSConfig"):
