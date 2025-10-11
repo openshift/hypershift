c3b9c6bb71560fa886fa713fa60feb43095016d8
Or Rener <orrener2000or@gmail.com>
2025-10-11 10:23:04 -0400

    fix(api/hypershift/v1beta1/hostedcluster_types.go): Add quotes to validation rule and allow to submit values starting with a number to the CustomIngressControllerName field.

--- api/hypershift/v1beta1/hostedcluster_types.go
+++ api/hypershift/v1beta1/hostedcluster_types.go
@@ -761,7 +761,7 @@ type HostedClusterSpec struct {
 	// +kubebuilder:default=default
 	// +kubebuilder:validation:MinLength=1
 	// +kubebuilder:validation:MaxLength=27
-	// +kubebuilder:validation:XValidation:rule=`self.matches(^[a-z]([-a-z0-9]*[a-z0-9])?$)`, message="customIngressControllerName has to be a valid kubernetes label value."
+	// +kubebuilder:validation:XValidation:rule=`self.matches('^[a-z0-9]([-a-z0-9]*[a-z0-9])?$')`, message="customIngressControllerName has to be a valid kubernetes label value."
 	// +optional
 	CustomIngressControllerName string `json:"customIngressControllerName,omitempty"`
 }
