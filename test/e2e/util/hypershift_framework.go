43df45c3f10590088714b4d3cf231a36b1f9805c
Seth Jennings <sjenning@redhat.com>
2025-10-13 14:30:56 -0500

    fix(e2e): refactor createHostedCluster to remove deps between option settings
    Currently, createHostedCluster only sets some options related to the
    Ingress and Authentication configuration if the signing key is
    provided to the e2e binary in advance (vs being created on the fly).
    
    There is no reason for the dependency.  The signing key options, ingress
    options, and authentication options have no interdependencies and should
    be set independently from one another.

--- test/e2e/util/hypershift_framework.go
+++ test/e2e/util/hypershift_framework.go
@@ -275,6 +275,32 @@ func (h *hypershiftTest) createHostedCluster(opts *PlatformAgnosticOptions, plat
 	err := h.client.Create(h.ctx, namespace)
 	g.Expect(err).NotTo(HaveOccurred(), "failed to create namespace")
 
+	if platform == hyperv1.AWSPlatform {
+		originalBeforeApply := opts.BeforeApply
+		opts.BeforeApply = func(o crclient.Object) {
+			if originalBeforeApply != nil {
+				originalBeforeApply(o)
+			}
+
+			switch v := o.(type) {
+			case *hyperv1.HostedCluster:
+				if v.Spec.Configuration == nil {
+					v.Spec.Configuration = &hyperv1.ClusterConfiguration{}
+				}
+				v.Spec.Configuration.Ingress = &configv1.IngressSpec{
+					LoadBalancer: configv1.LoadBalancer{
+						Platform: configv1.IngressPlatformSpec{
+							Type: configv1.AWSPlatformType,
+							AWS: &configv1.AWSIngressSpec{
+								Type: configv1.NLB,
+							},
+						},
+					},
+				}
+			}
+		}
+	}
+
 	// create serviceAccount signing key secret
 	if len(serviceAccountSigningKey) > 0 {
 		serviceAccountSigningKeySecret := &corev1.Secret{
@@ -290,37 +316,6 @@ func (h *hypershiftTest) createHostedCluster(opts *PlatformAgnosticOptions, plat
 		err = h.client.Create(h.ctx, serviceAccountSigningKeySecret)
 		g.Expect(err).NotTo(HaveOccurred(), "failed to create serviceAccountSigningKeySecret")
 
-		// create external oidc secret and configmap
-		if opts.ExtOIDCConfig != nil {
-			consoleClientSecret := &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      opts.ExtOIDCConfig.ConsoleClientSecretName,
-					Namespace: namespace.Name,
-				},
-				Type: corev1.SecretTypeOpaque,
-				StringData: map[string]string{
-					"clientSecret": opts.ExtOIDCConfig.ConsoleClientSecretValue,
-				},
-			}
-			err := h.client.Create(h.ctx, consoleClientSecret)
-			g.Expect(err).NotTo(HaveOccurred(), "failed to create external oidc secret")
-
-			caData, err := os.ReadFile(opts.ExtOIDCConfig.IssuerCABundleFile)
-			g.Expect(err).NotTo(HaveOccurred(), "failed to read external oidc issuer ca bundle file")
-
-			oidcCAConfigmap := &corev1.ConfigMap{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      opts.ExtOIDCConfig.IssuerCAConfigmapName,
-					Namespace: namespace.Name,
-				},
-				Data: map[string]string{
-					"ca-bundle.crt": string(caData),
-				},
-			}
-			err = h.client.Create(h.ctx, oidcCAConfigmap)
-			g.Expect(err).NotTo(HaveOccurred(), "failed to create external oidc issuer ca configmap")
-		}
-
 		originalBeforeApply := opts.BeforeApply
 		opts.BeforeApply = func(o crclient.Object) {
 			if originalBeforeApply != nil {
@@ -332,22 +327,48 @@ func (h *hypershiftTest) createHostedCluster(opts *PlatformAgnosticOptions, plat
 				v.Spec.ServiceAccountSigningKey = &corev1.LocalObjectReference{
 					Name: serviceAccountSigningKeySecret.Name,
 				}
-				if platform == hyperv1.AWSPlatform {
-					if v.Spec.Configuration == nil {
-						v.Spec.Configuration = &hyperv1.ClusterConfiguration{}
-					}
-					v.Spec.Configuration.Ingress = &configv1.IngressSpec{
-						LoadBalancer: configv1.LoadBalancer{
-							Platform: configv1.IngressPlatformSpec{
-								Type: configv1.AWSPlatformType,
-								AWS: &configv1.AWSIngressSpec{
-									Type: configv1.NLB,
-								},
-							},
-						},
-					}
-				}
+			}
+		}
+	}
 
+	// create external oidc secret and configmap
+	if opts.ExtOIDCConfig != nil {
+		consoleClientSecret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      opts.ExtOIDCConfig.ConsoleClientSecretName,
+				Namespace: namespace.Name,
+			},
+			Type: corev1.SecretTypeOpaque,
+			StringData: map[string]string{
+				"clientSecret": opts.ExtOIDCConfig.ConsoleClientSecretValue,
+			},
+		}
+		err := h.client.Create(h.ctx, consoleClientSecret)
+		g.Expect(err).NotTo(HaveOccurred(), "failed to create external oidc secret")
+
+		caData, err := os.ReadFile(opts.ExtOIDCConfig.IssuerCABundleFile)
+		g.Expect(err).NotTo(HaveOccurred(), "failed to read external oidc issuer ca bundle file")
+
+		oidcCAConfigmap := &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      opts.ExtOIDCConfig.IssuerCAConfigmapName,
+				Namespace: namespace.Name,
+			},
+			Data: map[string]string{
+				"ca-bundle.crt": string(caData),
+			},
+		}
+		err = h.client.Create(h.ctx, oidcCAConfigmap)
+		g.Expect(err).NotTo(HaveOccurred(), "failed to create external oidc issuer ca configmap")
+
+		originalBeforeApply := opts.BeforeApply
+		opts.BeforeApply = func(o crclient.Object) {
+			if originalBeforeApply != nil {
+				originalBeforeApply(o)
+			}
+
+			switch v := o.(type) {
+			case *hyperv1.HostedCluster:
 				if opts.ExtOIDCConfig != nil {
 					if v.Spec.Configuration == nil {
 						v.Spec.Configuration = &hyperv1.ClusterConfiguration{}
