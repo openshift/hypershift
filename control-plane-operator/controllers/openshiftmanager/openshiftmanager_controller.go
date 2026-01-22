c836567ec9e109125816b9bc57a5a653f56c5fa0
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2026-01-22 16:23:12 +0100

    WIP: wire om controller

--- control-plane-operator/controllers/openshiftmanager/openshiftmanager_controller.go
+++ control-plane-operator/controllers/openshiftmanager/openshiftmanager_controller.go
@@ -1,7 +1,14 @@
 package openshiftmanager
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // Reconciler is responsible for integration with OpenShiftManager.
@@ -12,7 +19,25 @@ import (
 type Reconciler struct {
 }
 
+func (r *Reconciler) Reconcile(ctx context.Context, operatorName string) (ctrl.Result, error) {
+	return ctrl.Result{}, fmt.Errorf("not implemented")
+}
+
 func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
 	inputResInitializer := newInputResourceInitializer(mgr.GetRESTMapper(), mgr.GetCache())
+
+	omCtrl, err := controller.NewTyped[string]("openshift-manager", mgr, controller.TypedOptions[string]{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	channelSource := source.TypedChannel[string, string](inputResInitializer.ResultChan(), handler.TypedEnqueueRequestsFromMapFunc(func(_ context.Context, operatorName string) []string {
+		return []string{operatorName}
+	}))
+
+	// TODO wire channelSource
+	if err = omCtrl.Watch(...); err != nil {
+		return err
+	}
 	return mgr.Add(inputResInitializer)
 }
