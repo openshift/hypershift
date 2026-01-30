5fa6ceacb34e0fc9ffd0c4b99897dfc7b885835c
Borja Clemente <bclement@redhat.com>
2026-01-30 10:08:06 +0100

    fix(conversion): give capi-provider access to CRDs
    For conversion to work, the CAPI provider needs to be able to access
    CRDs cluster-wide to list available versions.
    
    Signed-off-by: Borja Clemente <bclement@redhat.com>

--- hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
+++ hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
@@ -2930,6 +2930,11 @@ func reconcileCAPIManagerClusterRoleBinding(binding *rbacv1.ClusterRoleBinding, 
 			Name:      sa.Name,
 			Namespace: sa.Namespace,
 		},
+		{
+			Kind:      "ServiceAccount",
+			Name:      "capi-provider",
+			Namespace: sa.Namespace,
+		},
 	}
 	return nil
 }
