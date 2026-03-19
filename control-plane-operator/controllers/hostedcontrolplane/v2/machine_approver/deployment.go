c389600c9f1b812ff3c80955dedced907e2af34c
Damiano Donati <damiano.donati@gmail.com>
2026-03-19 22:13:19 +0100

    feat(machine-approver): add TLS configuration options to deployment manifests
    Added `--tls-min-version` and `--tls-cipher-suites` arguments to the machine approver deployment configurations across multiple environments to enhance security settings.

--- control-plane-operator/controllers/hostedcontrolplane/v2/machine_approver/deployment.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/machine_approver/deployment.go
@@ -2,7 +2,9 @@ package machineapprover
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/openshift/hypershift/support/config"
 	component "github.com/openshift/hypershift/support/controlplane-component"
 	"github.com/openshift/hypershift/support/util"
 
@@ -12,8 +14,16 @@ import (
 
 func adaptDeployment(cpContext component.WorkloadContext, deployment *appsv1.Deployment) error {
 	hcp := cpContext.HCP
+	configuration := hcp.Spec.Configuration
 	util.UpdateContainer(ComponentName, deployment.Spec.Template.Spec.Containers, func(c *corev1.Container) {
 		c.Args = append(c.Args, fmt.Sprintf("--machine-namespace=%s", hcp.Namespace))
+
+		if tlsMinVersion := config.MinTLSVersion(configuration.GetTLSSecurityProfile()); tlsMinVersion != "" {
+			c.Args = append(c.Args, fmt.Sprintf("--tls-min-version=%s", tlsMinVersion))
+		}
+		if cipherSuites := config.CipherSuites(configuration.GetTLSSecurityProfile()); len(cipherSuites) != 0 {
+			c.Args = append(c.Args, fmt.Sprintf("--tls-cipher-suites=%s", strings.Join(cipherSuites, ",")))
+		}
 	})
 
 	return nil
