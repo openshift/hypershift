abf747d7a0a311c768dd8677e9f73409f4c6fd82
John Kyros <jkyros@redhat.com>
2026-05-29 20:22:53 -0500

    fixup! fix(karpenter): Stop controllers fighting over HCP status

--- control-plane-operator/hostedclusterconfigoperator/controllers/resources/resources.go
+++ control-plane-operator/hostedclusterconfigoperator/controllers/resources/resources.go
@@ -1707,7 +1707,7 @@ func (r *reconciler) patchHCPStatusCondition(ctx context.Context, hcp *hyperv1.H
 		return nil // No status change; avoid unnecessary API call.
 	}
 	if err := r.cpClient.Status().Patch(ctx, hcp, client.MergeFromWithOptions(originalHCP, client.MergeFromWithOptimisticLock{})); err != nil {
-		return fmt.Errorf("failed to update HostedControlPlane status with %s condition: %w", condition.Type, err)
+		return fmt.Errorf("failed to patch HostedControlPlane status with %s condition: %w", condition.Type, err)
 	}
 	log.Info(string(condition.Type) + " condition updated")
 	return nil
