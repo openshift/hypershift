140d64d0f2163d67bec8cf4ac71b7119957427e9
Steve Kuznetsov <stekuznetsov@microsoft.com>
2026-06-22 14:47:03 -0600

    fix: update log statements for correctness
    Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>

--- cmd/infra/powervs/destroy.go
+++ cmd/infra/powervs/destroy.go
@@ -117,8 +117,8 @@ func NewDestroyCommand() *cobra.Command {
 	_ = cmd.Flags().MarkHidden("cloud-instance-id")
 	_ = cmd.Flags().MarkHidden("transit-gateway")
 
-	logger := cmdutil.NewLogger().WithName(opts.InfraID)
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		logger := cmdutil.NewLogger().WithName(opts.InfraID)
 		if err := opts.Run(cmd.Context(), logger); err != nil {
 			logger.Error(err, "Failed to destroy infrastructure")
 			return err
