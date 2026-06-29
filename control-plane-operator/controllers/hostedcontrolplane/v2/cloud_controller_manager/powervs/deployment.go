12fd88ef0f770bd19a62bf76dfe01b6b9cea490a
Jan Chaloupka <jchaloup@redhat.com>
2026-06-29 19:22:33 +0200

    feat(cloud-controller-manager): inject centralized TLS configuration
    Inject --tls-min-version and --tls-cipher-suites flags to all six cloud
    controller manager providers based on HCP TLS security profile.

--- control-plane-operator/controllers/hostedcontrolplane/v2/cloud_controller_manager/powervs/deployment.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/cloud_controller_manager/powervs/deployment.go
@@ -2,7 +2,9 @@ package powervs
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/openshift/hypershift/support/config"
 	component "github.com/openshift/hypershift/support/controlplane-component"
 	"github.com/openshift/hypershift/support/podspec"
 
@@ -19,6 +21,16 @@ func adaptDeployment(cpContext component.WorkloadContext, deployment *appsv1.Dep
 	if hcp.Spec.Platform.PowerVS == nil {
 		return fmt.Errorf(".spec.platform.powervs is not defined")
 	}
+
+	podspec.UpdateContainer("cloud-controller-manager", deployment.Spec.Template.Spec.Containers, func(c *corev1.Container) {
+		// Add TLS configuration based on cluster TLS security profile
+		if tlsMinVersion := config.MinTLSVersion(hcp.Spec.Configuration.GetTLSSecurityProfile()); tlsMinVersion != "" {
+			c.Args = append(c.Args, fmt.Sprintf("--tls-min-version=%s", tlsMinVersion))
+		}
+		if cipherSuites := config.CipherSuites(hcp.Spec.Configuration.GetTLSSecurityProfile()); len(cipherSuites) != 0 {
+			c.Args = append(c.Args, fmt.Sprintf("--tls-cipher-suites=%s", strings.Join(cipherSuites, ",")))
+		}
+	})
 
 	podspec.UpdateVolume(cloudCredsVolumeName, deployment.Spec.Template.Spec.Volumes, func(v *corev1.Volume) {
 		v.Secret.SecretName = hcp.Spec.Platform.PowerVS.KubeCloudControllerCreds.Name
