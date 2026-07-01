8d282e52e6c7136738a3c991bfaee292b802f3ea
Jan Chaloupka <jchaloup@redhat.com>
2026-07-01 15:51:22 +0200

    feat(csi-snapshot-controller-operator): inject centralized TLS configuration
    Mount a configmap with an operator config injected with the HCP TLS security
    profile.

---
+++ control-plane-operator/controllers/hostedcontrolplane/v2/snapshotcontroller/configmap.go
@@ -0,0 +1,26 @@
+package snapshotcontroller
+
+import (
+	"github.com/openshift/hypershift/support/config"
+	component "github.com/openshift/hypershift/support/controlplane-component"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+func adaptControllerConfig(cpContext component.WorkloadContext, cm *corev1.ConfigMap) error {
+	data, err := config.BuildGenericControllerConfigData(
+		"0.0.0.0:8443",
+		"",
+		cpContext.HCP.Spec.Configuration.GetTLSSecurityProfile(),
+	)
+	if err != nil {
+		return err
+	}
+
+	if cm.Data == nil {
+		cm.Data = map[string]string{}
+	}
+
+	cm.Data["config.yaml"] = data
+	return nil
+}
