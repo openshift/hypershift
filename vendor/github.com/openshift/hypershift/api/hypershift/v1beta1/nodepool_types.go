164d1489f5abe4ac6d1113df43335fd249ba529a
vismishr <vismishr@redhat.com>
2026-07-27 18:04:23 +0530

    fix: OCPBUGS-94251: improve CEL duration validation with overflow rejection
    Update NodePool nodeDrainTimeout and nodeVolumeDetachTimeout CEL
    validation to use duration() function for overflow detection and
    accept leading-dot decimals like .5s. Follows the HTTPKeepAliveTimeout
    two-rule pattern: regex for format + guarded duration() for range check.
    
    Add envtest coverage for .5s, us suffix, and overflow edge cases.

--- vendor/github.com/openshift/hypershift/api/hypershift/v1beta1/nodepool_types.go
+++ vendor/github.com/openshift/hypershift/api/hypershift/v1beta1/nodepool_types.go
@@ -185,7 +185,8 @@ type NodePoolSpec struct {
 	// Changing this field propagate inplace into existing Nodes.
 	// +optional
 	// +kubebuilder:validation:Type:=string
-	// +kubebuilder:validation:XValidation:rule="self.matches('^([0-9]+(\\\\.[0-9]+)?(ns|us|µs|ms|s|m|h))+$')",message="nodeDrainTimeout must be a valid duration string with a unit suffix (e.g. 30s, 5m, 1h)"
+	// +kubebuilder:validation:XValidation:rule="self.matches('^(([0-9]+(\\\\.[0-9]+)?|\\\\.[0-9]+)(ns|us|µs|ms|s|m|h))+$')",message="nodeDrainTimeout must be a valid duration string with a unit suffix (e.g. 30s, 5m, 1h)"
+	// +kubebuilder:validation:XValidation:rule="!self.matches('^(([0-9]+(\\\\.[0-9]+)?|\\\\.[0-9]+)(ns|us|µs|ms|s|m|h))+$') || duration(self) >= duration('0s')",message="nodeDrainTimeout must be a non-negative duration"
 	NodeDrainTimeout *metav1.Duration `json:"nodeDrainTimeout,omitempty"`
 
 	// nodeVolumeDetachTimeout is the maximum amount of time that the controller will spend on detaching volumes from a node.
@@ -194,7 +195,8 @@ type NodePoolSpec struct {
 	// Changing this field propagate inplace into existing Nodes.
 	// +optional
 	// +kubebuilder:validation:Type:=string
-	// +kubebuilder:validation:XValidation:rule="self.matches('^([0-9]+(\\\\.[0-9]+)?(ns|us|µs|ms|s|m|h))+$')",message="nodeVolumeDetachTimeout must be a valid duration string with a unit suffix (e.g. 30s, 5m, 1h)"
+	// +kubebuilder:validation:XValidation:rule="self.matches('^(([0-9]+(\\\\.[0-9]+)?|\\\\.[0-9]+)(ns|us|µs|ms|s|m|h))+$')",message="nodeVolumeDetachTimeout must be a valid duration string with a unit suffix (e.g. 30s, 5m, 1h)"
+	// +kubebuilder:validation:XValidation:rule="!self.matches('^(([0-9]+(\\\\.[0-9]+)?|\\\\.[0-9]+)(ns|us|µs|ms|s|m|h))+$') || duration(self) >= duration('0s')",message="nodeVolumeDetachTimeout must be a non-negative duration"
 	NodeVolumeDetachTimeout *metav1.Duration `json:"nodeVolumeDetachTimeout,omitempty"`
 
 	// nodeLabels propagates a list of labels to Nodes, only once on creation.
