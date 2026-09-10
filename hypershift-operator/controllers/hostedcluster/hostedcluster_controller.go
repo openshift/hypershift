a9c88a0a7ccb26b87e3a53c71f763e8f82660a85
Bryan Cox <brcox@redhat.com>
2026-09-10 14:19:10 -0400

    feat(hypershift-operator): propagate component configuration
    Copy HostedCluster control plane component configuration into the HostedControlPlane.
    
    Signed-off-by: Bryan Cox <brcox@redhat.com>

--- hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
+++ hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
@@ -2878,6 +2878,7 @@ func reconcileHostedControlPlane(hcp *hyperv1.HostedControlPlane, hcluster *hype
 	hcp.Spec.DNS = hcluster.Spec.DNS
 	hcp.Spec.Services = hcluster.Spec.Services
 	hcp.Spec.ControllerAvailabilityPolicy = hcluster.Spec.ControllerAvailabilityPolicy
+	hcluster.Spec.ControlPlaneComponentConfiguration.DeepCopyInto(&hcp.Spec.ControlPlaneComponentConfiguration)
 	hcp.Spec.InfrastructureAvailabilityPolicy = hcluster.Spec.InfrastructureAvailabilityPolicy
 	hcp.Spec.Etcd.ManagementType = hcluster.Spec.Etcd.ManagementType
 	if hcluster.Spec.Etcd.ManagementType == hyperv1.Unmanaged && hcluster.Spec.Etcd.Unmanaged != nil {
