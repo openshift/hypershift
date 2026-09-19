327dd910aa1886e4051f2a31647f0a31a35192e6
red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>
2026-09-19 01:38:34 +0000

    chore(deps): update module github.com/gabriel-vasile/mimetype to v1.4.15
    Signed-off-by: red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>

--- vendor/github.com/gabriel-vasile/mimetype/internal/scan/bytes.go
+++ vendor/github.com/gabriel-vasile/mimetype/internal/scan/bytes.go
@@ -35,6 +35,19 @@ func (b *Bytes) TrimRWS() {
 	}
 }
 
+// FirstNonWS returns the first non-whitespace character from b,
+// or 0x00 if no such character is found.
+func (b Bytes) FirstNonWS() byte {
+	for i := range b {
+		if ByteIsWS(b[i]) {
+			continue
+		}
+		return b[i]
+	}
+
+	return 0x00
+}
+
 // Peek one byte from b or 0x00 if b is empty.
 func (b *Bytes) Peek() byte {
 	if len(*b) > 0 {
@@ -63,8 +76,8 @@ func (b *Bytes) PopN(n int) []byte {
 	return nil
 }
 
-// PopUntil will advance b until, but not including, the first occurence of stopAt
-// character. If no occurence is found, then it will advance until the end of b.
+// PopUntil will advance b until, but not including, the first occurrence of stopAt
+// character. If no occurrence is found, then it will advance until the end of b.
 // The returned Bytes is a slice of all the bytes that we're advanced over.
 func (b *Bytes) PopUntil(stopAt ...byte) Bytes {
 	if len(*b) == 0 {
@@ -77,7 +90,7 @@ func (b *Bytes) PopUntil(stopAt ...byte) Bytes {
 
 	prefix := (*b)[:i]
 	*b = (*b)[i:]
-	return Bytes(prefix)
+	return prefix
 }
 
 // ReadSlice is the same as PopUntil, but the returned value includes stopAt as well.
@@ -94,7 +107,7 @@ func (b *Bytes) ReadSlice(stopAt byte) Bytes {
 
 	prefix := (*b)[:i]
 	*b = (*b)[i:]
-	return Bytes(prefix)
+	return prefix
 }
 
 // Line returns the first line from b and advances b with the length of the
@@ -109,32 +122,30 @@ func (b *Bytes) Line() Bytes {
 	return line
 }
 
-// DropLastLine drops the last incomplete line from b.
-//
-// mimetype limits itself to ReadLimit bytes when performing a detection.
-// This means, for file formats like CSV for NDJSON, the last line of the input
-// can be an incomplete line.
-// If b length is less than readLimit, it means we received an incomplete file
-// and proceed with dropping the last line.
-func (b *Bytes) DropLastLine(readLimit uint32) {
-	if readLimit == 0 || uint32(len(*b)) < readLimit {
-		return
-	}
-
-	for i := len(*b) - 1; i > 0; i-- {
-		if (*b)[i] == '\n' {
-			*b = (*b)[:i]
-			return
-		}
-	}
-}
-
 func (b *Bytes) Uint16() (uint16, bool) {
 	if len(*b) < 2 {
 		return 0, false
 	}
 	v := binary.LittleEndian.Uint16(*b)
 	*b = (*b)[2:]
+	return v, true
+}
+
+func (b *Bytes) Uint32() (uint32, bool) {
+	if len(*b) < 4 {
+		return 0, false
+	}
+	v := binary.LittleEndian.Uint32(*b)
+	*b = (*b)[4:]
+	return v, true
+}
+
+func (b *Bytes) Uint32be() (uint32, bool) {
+	if len(*b) < 4 {
+		return 0, false
+	}
+	v := binary.BigEndian.Uint32(*b)
+	*b = (*b)[4:]
 	return v, true
 }
 
@@ -151,7 +162,7 @@ const (
 	FullWord
 )
 
-// Search for occurences of pattern p inside b at any index.
+// Search for occurrences of pattern p inside b at any index.
 // It returns the index where p was found in b and how many bytes were needed
 // for matching the pattern.
 func (b Bytes) Search(p []byte, flags Flags) (i int, l int) {
@@ -192,10 +203,8 @@ func (b Bytes) Match(p []byte, flags Flags) int {
 	if l == 0 {
 		return -1
 	}
-	// If no flags, or scanning for full word at the end of pattern then
-	// do a fast HasPrefix check.
-	// For other flags it's not possible to use HasPrefix.
-	if flags == 0 || flags&FullWord > 0 {
+	// Some cases we can handle with a simple bytes.HasPrefix.
+	if flags == 0 || flags == FullWord {
 		if bytes.HasPrefix(b, p) {
 			b = b[len(p):]
 			p = p[len(p):]
@@ -219,7 +228,7 @@ func (b Bytes) Match(p []byte, flags Flags) int {
 				return -1
 			}
 			b = b[1:]
-			if !ByteIsWS(p[0]) {
+			if len(p) > 0 && !ByteIsWS(p[0]) {
 				b.TrimLWS()
 			}
 		} else {
